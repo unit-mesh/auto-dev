b8125c2efe5d3e2c5e2ae99015a2b7d3e0cd2032
Phodal Huang <h@phodal.com>
2023-04-15 19:48:36 +0800

    refactor: config for code

--- src/main/kotlin/cc/unitmesh/devti/language/DisplayStoryLineMarkerContributor.kt
+++ src/main/kotlin/cc/unitmesh/devti/language/DisplayStoryLineMarkerContributor.kt
@@ -1,7 +1,6 @@
 package cc.unitmesh.devti.language
 
 import cc.unitmesh.devti.DevtiIcons
-import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.psi.PsiComment
@@ -9,11 +8,9 @@ import com.intellij.psi.PsiElement
 
 class DisplayStoryLineMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
-        // a valid Devti property should be a comment, and the comment format should be like "devti://story/1102/{AC1,AC2}"
         if (element !is PsiComment) return null
 
         val commentText = element.text
-        // use regex to match devti://story/1102/{AC1,AC2}
         val regex = DevtiAnnotator.DEVTI_REGEX
         val matchResult = regex.find(commentText) ?: return null
 
