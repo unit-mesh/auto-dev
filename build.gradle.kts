8f1b760c8fab19c6be0db5a8fcc7787c3ba7663a
Phodal Huang <h@phodal.com>
2023-04-16 09:35:49 +0800

    docs: update lost configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,11 +40,6 @@ dependencies {
     implementation("com.aallam.openai:openai-client:3.2.0")
 }
 
-// Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
-kotlin {
-    jvmToolchain(11)
-}
-
 // Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     pluginName = properties("pluginName")
