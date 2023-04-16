8097b6932bcaabb2814ebc302069573f71208419
Phodal Huang <h@phodal.com>
2023-04-16 17:21:11 +0800

    refactor: extract for filters

--- src/main/kotlin/cc/unitmesh/devti/analysis/JavaEndpointFetcher.kt
+++ src/main/kotlin/cc/unitmesh/devti/analysis/JavaEndpointFetcher.kt
@@ -2,7 +2,7 @@ package cc.unitmesh.devti.analysis
 
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
@@ -18,11 +18,20 @@ class JavaEndpointFetcher(val project: Project) {
         val searchScope: GlobalSearchScope = ProjectScope.getContentScope(project)
         val javaFiles = FileTypeIndex.getFiles(JavaFileType.INSTANCE, searchScope)
 
-        return javaFiles
-            .mapNotNull { virtualFile -> psiManager.findFile(virtualFile) }
-            .filter { psiFile ->
-                val javaClasses = PsiTreeUtil.findChildrenOfType(psiFile, PsiClass::class.java)
-                javaClasses.any { clazz -> clazz.hasAnnotation("Controller") || clazz.hasAnnotation("RestController") }
-            }
+        return filterFiles(javaFiles, psiManager, ::controllerFilter)
     }
+
+    fun filterFiles(
+        javaFiles: Collection<VirtualFile>,
+        psiManager: PsiManager,
+        filter: (PsiClass) -> Boolean
+    ) = javaFiles
+        .mapNotNull { virtualFile -> psiManager.findFile(virtualFile) }
+        .filter { psiFile ->
+            val javaClasses = PsiTreeUtil.findChildrenOfType(psiFile, PsiClass::class.java)
+            javaClasses.any(filter)
+        }
+
+    private fun controllerFilter(clazz: PsiClass) =
+        clazz.hasAnnotation("Controller") || clazz.hasAnnotation("RestController")
 }
---
+++ src/test/kotlin/cc/unitmesh/devti/analysis/JavaEndpointFetcherTest.kt
@@ -0,0 +1,14 @@
+package cc.unitmesh.devti.analysis
+
+import org.junit.Test
+
+
+class JavaEndpointFetcherTest {
+
+    @Test
+    fun should_success_filter_all_controller() {
+//        val javaEndpointFetcher = JavaEndpointFetcher()
+//        val result = javaEndpointFetcher.fetch("src/test/resources/JavaEndpointFetcherTest.java")
+//        assert(result.size == 2)
+    }
+}
