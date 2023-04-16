3659098570779f02a661814b14f993289c9d9f39
Phodal Huang <h@phodal.com>
2023-04-16 20:03:46 +0800

    test: format coes

--- src/test/kotlin/cc/unitmesh/devti/analysis/JavaModifierTest.kt
+++ src/test/kotlin/cc/unitmesh/devti/analysis/JavaModifierTest.kt
@@ -9,16 +9,21 @@ class JavaModifierTest : LightPlatformTestCase() {
 
     fun testShould_fetch_java_endpoints() {
         val originCode = """
-public String hello() {
-    return "Greetings from Spring Boot!";
-}
+    public String hello() {
+        return "Greetings from Spring Boot!";
+    }
             """.trimIndent()
         val psiClass = javaFactory.createClassFromText(originCode, null)
         psiClass.setName("HelloController")
+        psiClass.addBefore(javaFactory.createAnnotationFromText("@Controller", null), psiClass.firstChild)
 
         val endpoints = JavaModifier(project)
         val newClasses =
-            endpoints.addMethodToClass(psiClass, "public String hello2() { return \"Greetings from Spring Boot!\"; }")
+            endpoints.addMethodToClass(psiClass, """
+    public String hello2() { 
+        return "Greetings from Spring Boot!"; 
+    }
+""".trimIndent())
 
         assertEquals("hello", newClasses.methods[0].name)
         assertEquals("hello2", newClasses.methods[1].name)
