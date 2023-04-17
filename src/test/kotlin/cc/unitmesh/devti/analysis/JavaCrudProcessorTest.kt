ee01bad0912ee688a670ea944d03eaaff7ce3331
Phodal Huang <h@phodal.com>
2023-04-17 15:35:03 +0800

    test: update testcases

--- src/test/kotlin/cc/unitmesh/devti/analysis/JavaCrudProcessorTest.kt
+++ src/test/kotlin/cc/unitmesh/devti/analysis/JavaCrudProcessorTest.kt
@@ -1,11 +1,15 @@
 package cc.unitmesh.devti.analysis
 
+import com.intellij.lang.java.JavaLanguage
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElementFactory
-import com.intellij.testFramework.LightPlatformTestCase
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.impl.file.PsiDirectoryFactory
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import junit.framework.TestCase
+import kotlin.math.log
 
-class JavaCrudProcessorTest : LightPlatformTestCase() {
+class JavaCrudProcessorTest : BasePlatformTestCase() {
     private val javaFactory: PsiElementFactory get() = JavaPsiFacade.getElementFactory(project)
 
     fun testShould_create_a_new_controller() {
@@ -17,5 +21,17 @@ class JavaCrudProcessorTest : LightPlatformTestCase() {
         TestCase.assertEquals(psiClass.text, """@Controller class HelloController {
 
 }""")
+    }
+
+    fun testShould_create_new_file() {
+        val newController = PsiFileFactory.getInstance(project).createFileFromText(
+            "HelloController.java", JavaLanguage.INSTANCE, """@Controller class HelloController {
+            |
+            |}""".trimMargin()
+        )
+
+//        val psiDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(myFixture.tempDirFixture.findOrCreateDir("src/main/java"))
+//        println(psiDirectory)
+//        val newFile = psiDirectory.add(newController)
     }
 }
