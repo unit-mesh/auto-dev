92cef79594508f5df151169bf21943c492690d1d
Phodal Huang <h@phodal.com>
2023-04-18 17:05:04 +0800

    refactor: clean old settigns barS

--- src/main/kotlin/cc/unitmesh/devti/runconfig/config/AiCopilotConfiguration.kt
+++ src/main/kotlin/cc/unitmesh/devti/runconfig/config/AiCopilotConfiguration.kt
@@ -1,6 +1,5 @@
 package cc.unitmesh.devti.runconfig.config
 
-import cc.unitmesh.devti.prompt.openai.DtOpenAIVersion
 import cc.unitmesh.devti.runconfig.options.OpenAIConfigureOptions
 import cc.unitmesh.devti.runconfig.ui.AiCopilotSettingsEditor
 import com.intellij.execution.Executor
@@ -14,7 +13,6 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
-import org.jdom.Element
 
 class AiCopilotConfiguration(project: Project, name: String, factory: ConfigurationFactory) :
     RunConfigurationBase<OpenAIConfigureOptions>(project, factory, name) {
@@ -37,33 +35,5 @@ class AiCopilotConfiguration(project: Project, name: String, factory: Configurat
         configurationPerRunnerSettings: ConfigurationPerRunnerSettings?
     ) {
         super.checkRunnerSettings(runner, runnerSettings, configurationPerRunnerSettings)
-    }
-
-    override fun writeExternal(element: Element) {
-        super.writeExternal(element)
-
-        element.writeString("openAiApiKey", options.openAiApiKey())
-        element.writeString("aiEngineVersion", options.aiEngineVersion().toString())
-        element.writeString("aiMaxTokens", options.aiMaxTokens().toString())
-    }
-
-    override fun readExternal(element: Element) {
-        super.readExternal(element)
-
-        element.readString("openAiApiKey")?.let { this.options.setOpenAiApiKey(it) }
-        element.readString("aiEngineVersion")?.let { this.options.setAiEngineVersion(it.toInt()) }
-        element.readString("aiMaxTokens")?.let { this.options.setAiMaxTokens(it.toInt()) }
-    }
-
-    fun setOpenAiApiKey(text: String) {
-        this.options.setOpenAiApiKey(text)
-    }
-
-    fun setAiVersion(fromIndex: DtOpenAIVersion) {
-        this.options.setAiEngineVersion(fromIndex.ordinal)
-    }
-
-    fun setAiMaxTokens(openAiMaxTokens: Int) {
-        this.options.setAiMaxTokens(openAiMaxTokens)
     }
 }
--- src/main/kotlin/cc/unitmesh/devti/runconfig/ui/DtSettingsEditor.kt
+++ src/main/kotlin/cc/unitmesh/devti/runconfig/ui/DtSettingsEditor.kt
@@ -2,64 +2,56 @@ package cc.unitmesh.devti.runconfig.ui
 
 import cc.unitmesh.devti.prompt.openai.DtOpenAIVersion
 import cc.unitmesh.devti.runconfig.config.AutoCRUDConfiguration
+import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
+import com.intellij.ui.dsl.builder.Cell
+import com.intellij.ui.dsl.builder.Row
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import javax.swing.JComponent
 
-class DtSettingsEditor(project: Project) : BaseSettingsEditor<AutoCRUDConfiguration>(project) {
+class DtSettingsEditor(project: Project) : SettingsEditor<AutoCRUDConfiguration>() {
+    protected var completionProvider = DtCommandCompletionProvider()
+
     private val githubToken = DtCommandLineEditor(project, completionProvider)
     private val githubRepo = DtCommandLineEditor(project, completionProvider)
     private val storyId = DtCommandLineEditor(project, completionProvider)
+
+    protected var panel: JComponent? = null
 
     override fun createEditor(): JComponent = panel {
         row("Github Token:") {
             fullWidthCell(githubToken)
         }
 
-        row ("GitHub Project (owner/repo)") {
+        row("GitHub Project (owner/repo)") {
             fullWidthCell(githubRepo)
         }
 
         // story id
-        row ("Story ID:") {
+        row("Story ID:") {
             fullWidthCell(storyId)
         }
 
         row("API Engine:") {
             comboBox(listOf("OpenAI"))
         }
-
-        row("AI API Key:") {
-            fullWidthCell(aiApiToken)
-        }
-
-        row("API Engine:") {
-            cell(aiEngineVersion)
-        }
-
-        row("Max Tokens") {
-            fullWidthCell(maxTokens)
-        }
     }.also {
         panel = it
     }
 
     override fun resetEditorFrom(configuration: AutoCRUDConfiguration) {
-        githubToken.text = configuration.options.githubToken()
         githubRepo.text = configuration.options.githubRepo()
-        aiApiToken.text = configuration.options.openAiApiKey()
-        aiEngineVersion.selectedIndex = configuration.options.aiEngineVersion()
-        openAiMaxTokens = configuration.options.aiMaxTokens()
-        maxTokens.text = openAiMaxTokens.toString()
         storyId.text = configuration.options.storyId()
     }
 
     override fun applyEditorTo(configuration: AutoCRUDConfiguration) {
-        configuration.setGithubToken(githubToken.text)
         configuration.setGithubRepo(githubRepo.text)
-        configuration.setOpenAiApiKey(aiApiToken.text)
-        configuration.setAiVersion(DtOpenAIVersion.fromIndex(aiEngineVersion.selectedIndex))
-        configuration.setAiMaxTokens(openAiMaxTokens)
         configuration.setStoryId(storyId.text)
     }
+}
+
+fun <T : JComponent> Row.fullWidthCell(component: T): Cell<T> {
+    return cell(component)
+        .horizontalAlign(HorizontalAlign.FILL)
 }
