8d1a41928d47a9664d0bf513f008712c68dd14e0
Phodal Huang <h@phodal.com>
2023-04-18 14:50:41 +0800

    docs: update for actions

--- src/main/kotlin/cc/unitmesh/devti/language/AiCopilotMarkerContributor.kt
+++ src/main/kotlin/cc/unitmesh/devti/language/AiCopilotMarkerContributor.kt
@@ -1,9 +1,15 @@
 package cc.unitmesh.devti.language
 
 import cc.unitmesh.devti.DevtiIcons
-import cc.unitmesh.devti.runconfig.command.AiCopilotConfigurationProducer
-import com.intellij.execution.lineMarker.ExecutorAction
+import cc.unitmesh.devti.runconfig.AutoCRUDState
+import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiMethod
@@ -14,12 +20,29 @@ class AiCopilotMarkerContributor : RunLineMarkerContributor() {
         val parent = element.parent
         if (parent !is PsiMethod) return null
 
-        val actions = ExecutorAction.getActions(0)
-        val state = AiCopilotConfigurationProducer().findConfig(listOf(element)) ?: return null
+        val runAction = object : AnAction({ "Auto Coding" }, DevtiIcons.AI_COPILOT) {
+            override fun actionPerformed(e: AnActionEvent) {
+                ApplicationManager.getApplication().invokeLater {
+                    execute("devti://ai-copilot", DefaultRunExecutor.EXECUTOR_ID)
+                }
+            }
+        }
+
         return Info(
             DevtiIcons.AI_COPILOT,
-            { state.configurationName },
-            *actions
+            { "Auto Coding" },
+            runAction
         )
+    }
+
+    private fun execute(command: String, executorId: String) {
+        log.warn("execute: $command, $executorId")
+        runReadAction {
+            // todo: modify files
+        }
+    }
+
+    companion object {
+        private val log: Logger = logger<AutoCRUDState>()
     }
 }
--- src/main/kotlin/cc/unitmesh/devti/language/CodeCommentsMarkerContributor.kt
+++ src/main/kotlin/cc/unitmesh/devti/language/CodeCommentsMarkerContributor.kt
@@ -1,8 +1,15 @@
 package cc.unitmesh.devti.language
 
 import cc.unitmesh.devti.DevtiIcons
-import com.intellij.execution.lineMarker.ExecutorAction
+import cc.unitmesh.devti.runconfig.AutoCRUDState
+import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiMethod
@@ -13,13 +20,29 @@ class CodeCommentsMarkerContributor : RunLineMarkerContributor() {
         val parent = element.parent
         if (parent !is PsiMethod) return null
 
-        val actions = ExecutorAction.getActions(0)
-//        val state = AiCopilotConfigurationProducer().findConfig(listOf(element)) ?: return null
+        val runAction = object : AnAction({ "Auto Comments" }, DevtiIcons.AI_COPILOT) {
+            override fun actionPerformed(e: AnActionEvent) {
+                ApplicationManager.getApplication().invokeLater {
+                    execute("devti://comments", DefaultRunExecutor.EXECUTOR_ID)
+                }
+            }
+        }
+
         return Info(
-            DevtiIcons.COMMENTS,
-//            { state.configurationName },
+            DevtiIcons.AI_COPILOT,
             { "Auto Comments" },
-            *actions
+            runAction
         )
+    }
+
+    private fun execute(command: String, executorId: String) {
+        log.warn("execute: $command, $executorId")
+        runReadAction {
+            // todo: modify files
+        }
+    }
+
+    companion object {
+        private val log: Logger = logger<AutoCRUDState>()
     }
 }
