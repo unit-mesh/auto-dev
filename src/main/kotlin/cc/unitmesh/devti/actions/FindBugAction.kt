aa66dc5a1f93fffd7435a8234a2cf8946c592aa7
Phodal Huang <h@phodal.com>
2023-04-20 15:50:52 +0800

    refactor: extract for FindbUG

---
+++ src/main/kotlin/cc/unitmesh/devti/actions/FindBugAction.kt
@@ -0,0 +1,14 @@
+package cc.unitmesh.devti.actions
+
+import cc.unitmesh.devti.DevtiIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.NlsSafe
+import com.intellij.psi.PsiMethod
+
+class FindBugAction(methodName: @NlsSafe String, method: PsiMethod) : AnAction({ "Find bug for $methodName" }, DevtiIcons.AI_COPILOT) {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+
+}
--- src/main/kotlin/cc/unitmesh/devti/language/FindBugMarkerContributor.kt
+++ src/main/kotlin/cc/unitmesh/devti/language/FindBugMarkerContributor.kt
@@ -1,12 +1,9 @@
 package cc.unitmesh.devti.language
 
 import cc.unitmesh.devti.DevtiIcons
+import cc.unitmesh.devti.actions.FindBugAction
 import cc.unitmesh.devti.runconfig.AutoCRUDState
-import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
@@ -17,30 +14,17 @@ import com.intellij.psi.PsiMethod
 class FindBugMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
         if (element !is PsiIdentifier) return null
-        val parent = element.parent
-        if (parent !is PsiMethod) return null
+        val method = element.parent
+        if (method !is PsiMethod) return null
 
-        val methodName = parent.name
-        val runAction = object : AnAction({ "Find Bug for $methodName" }, DevtiIcons.AI_COPILOT) {
-            override fun actionPerformed(e: AnActionEvent) {
-                ApplicationManager.getApplication().invokeLater {
-                    execute("devti://find-bug", DefaultRunExecutor.EXECUTOR_ID)
-                }
-            }
-        }
+        val methodName = method.name
+        val runAction = FindBugAction(methodName, method)
 
         return Info(
             DevtiIcons.AI_COPILOT,
             { "Find Bug" },
             runAction
         )
-    }
-
-    private fun execute(command: String, executorId: String) {
-        log.warn("execute: $command, $executorId")
-        runReadAction {
-            // todo: modify files
-        }
     }
 
     companion object {
