bf3c41d86aa865c94797237d38d17684b5d826ec
Phodal Huang <h@phodal.com>
2023-04-25 13:58:39 +0800

    build: add api server

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -12,9 +12,11 @@ dependencies {
     implementation(libs.chapi.java)
     implementation(libs.chapi.kotlin)
 
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.briandilley.jsonrpc4j:jsonrpc4j:1.6")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
