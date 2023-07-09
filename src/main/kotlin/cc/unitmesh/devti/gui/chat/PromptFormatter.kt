38aaa3511f80aa39ce3cb3b981eedcbc9eaf8a95
Phodal Huang <h@phodal.com>
2023-07-09 09:15:44 +0800

    feat: add for service

--- src/main/kotlin/cc/unitmesh/devti/gui/chat/PromptFormatter.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/chat/PromptFormatter.kt
@@ -47,12 +47,25 @@ class ActionPromptFormatter(
             ChatBotActionType.CODE_COMPLETE -> {
                 prompt = "补全如下的 $lang 代码"
 
-                if (fileName.endsWith("Controller.java")) {
-                    prompt = """代码补全 $lang 要求：
-                                |1. 在 Controller 中使用 BeanUtils 完成 DTO 的转换
-                                |2. 不允许把 json，map 这类对象传到 service 中
-                                |3. 不允许在 Controller 中使用 @Autowired
-                            """.trimMargin()
+                val isController = fileName.endsWith("Controller.java")
+                val isService = fileName.endsWith("Service.java") || fileName.endsWith("ServiceImpl.java")
+
+                when {
+                    isController -> {
+                        prompt = """代码补全 $lang 要求：
+                                            |1. 在 Controller 中使用 BeanUtils 完成 DTO 的转换
+                                            |2. 不允许把 json，map 这类对象传到 service 中
+                                            |3. 不允许在 Controller 中使用 @Autowired
+                                        """.trimMargin()
+                    }
+
+                    isService -> {
+                        prompt = """代码补其 $lang 要求：
+                                            |1. 不允许同时使用 BeanUtils 和 Map 转换 DTO
+                                            |2. 直接调用 repository 的方法时，使用 get, find, count, delete, save, update 这类方法
+                                            |3. Service 层应该捕获并处理可能出现的异常。通常情况下，应该将异常转换为应用程序自定义异常并抛出。
+                                            """.trimMargin()
+                    }
                 }
 
             }
