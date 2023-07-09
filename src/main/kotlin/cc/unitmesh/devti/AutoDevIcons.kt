57b9b090e8e6a74fa1cb40d8dc86657091419499
Phodal Huang <h@phodal.com>
2023-07-09 15:17:36 +0800

    refactor: remove unused feature

--- src/main/kotlin/cc/unitmesh/devti/AutoDevIcons.kt
+++ src/main/kotlin/cc/unitmesh/devti/AutoDevIcons.kt
@@ -6,8 +6,6 @@ import javax.swing.Icon
 class AutoDevIcons {
     companion object {
         val STORY: Icon = IconLoader.getIcon("/icons/story.svg", AutoDevIcons::class.java)
-        val REFACTOR: Icon = IconLoader.getIcon("/icons/find-bug.svg", AutoDevIcons::class.java)
         val AI_COPILOT: Icon = IconLoader.getIcon("/icons/ai-copilot.svg", AutoDevIcons::class.java)
-        val COMMENTS: Icon = IconLoader.getIcon("/icons/comments.svg", AutoDevIcons::class.java)
     }
 }
