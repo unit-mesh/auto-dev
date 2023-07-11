122bd9d30b129cac328e2683059dc4362421c250
Phodal Huang <h@phodal.com>
2023-07-11 13:36:16 +0800

    refactor: clean for docs

--- src/main/kotlin/cc/unitmesh/devti/language/DtLanguage.kt
+++
@@ -1,8 +0,0 @@
-package cc.unitmesh.devti.language
-
-import com.intellij.lang.Language
-
-object DtLanguage : Language("DevTi", "text/devti", "text/x-devti", "application/x-devti") {
-    override fun isCaseSensitive() = true
-    override fun getDisplayName() = "DevTi"
-}
