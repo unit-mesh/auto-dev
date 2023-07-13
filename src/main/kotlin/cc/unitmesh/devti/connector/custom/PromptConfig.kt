0b07662fe534f166832a49d70ed10d8cc4e5e143
Phodal Huang <h@phodal.com>
2023-07-13 09:21:06 +0800

    feat: add requirements to prompot item

--- src/main/kotlin/cc/unitmesh/devti/connector/custom/PromptConfig.kt
+++ src/main/kotlin/cc/unitmesh/devti/connector/custom/PromptConfig.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 @Serializable
-data class PromptItem(val instruction: String, val input: String)
+data class PromptItem(val instruction: String, val input: String, val requirements: String = "")
 
 @Serializable
 data class PromptConfig(
