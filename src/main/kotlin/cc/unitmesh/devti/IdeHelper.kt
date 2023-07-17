71d48b290be96b51e3d56093d2e31eba94803124
Phodal Huang <h@phodal.com>
2023-07-17 09:17:52 +0800

    docs: udpate format

--- src/main/kotlin/cc/unitmesh/devti/IdeHelper.kt
+++ src/main/kotlin/cc/unitmesh/devti/IdeHelper.kt
@@ -7,7 +7,8 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.codeStyle.CodeStyleManager
 
 object IdeHelper {
-    fun formatCode(project: Project, document: Document, textRange: TextRange, suggestion: String) {
+    fun updateDocument(project: Project, document: Document, textRange: TextRange, suggestion: String) {
+        document.replaceString(textRange.startOffset, textRange.endOffset, suggestion)
         PsiDocumentManager.getInstance(project).commitDocument(document)
         val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document) ?: return
         val reformatRange = TextRange(textRange.startOffset, textRange.startOffset + suggestion.length)
