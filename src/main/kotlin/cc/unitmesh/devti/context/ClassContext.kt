f18fe67ebc9199c12d37d01037a0206ab14e06ba
Phodal Huang <h@phodal.com>
2023-07-17 20:29:46 +0800

    refactor: init file contxt

--- src/main/kotlin/cc/unitmesh/devti/context/ClassContext.kt
+++ src/main/kotlin/cc/unitmesh/devti/context/ClassContext.kt
@@ -1,5 +1,17 @@
 package cc.unitmesh.devti.context
 
-class ClassContext {
+import cc.unitmesh.devti.context.base.NamedElementContext
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+
+class ClassContext(
+    override val root: PsiElement,
+    override val text: String?,
+    override val name: String?,
+    val methods: List<PsiElement>,
+    val fields: List<PsiElement>,
+    val superClasses: List<String>?,
+    val usages: List<PsiReference>
+) : NamedElementContext(root, text, name) {
 
 }
--- src/main/kotlin/cc/unitmesh/devti/context/FileContext.kt
+++ src/main/kotlin/cc/unitmesh/devti/context/FileContext.kt
@@ -1,4 +1,32 @@
 package cc.unitmesh.devti.context
 
-class FileContext {
+import cc.unitmesh.devti.context.base.LLMQueryContext
+import com.google.gson.Gson
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+
+class FileContext(
+    val root: PsiFile,
+    val name: String,
+    val path: String,
+    val packageString: String?,
+    val imports: List<PsiElement>,
+    val classes: List<PsiElement>,
+    val methods: List<PsiElement>
+) : LLMQueryContext {
+
+    override fun toQuery(): String {
+        return ""
+    }
+
+    override fun toJson(): String {
+        return Gson().toJson(
+            mapOf(
+                "name" to name,
+                "path" to path,
+                "package" to packageString
+            )
+        )
+    }
+
 }
