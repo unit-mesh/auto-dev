bf8ac64921474ec5f098e5229c3e7b3233544d89
Phodal Huang <h@phodal.com>
2023-07-17 11:57:04 +0800

    feat: make to extension point

---
+++ src/main/kotlin/cc/unitmesh/devti/intentions/AIAssistantIntention.kt
@@ -0,0 +1,88 @@
+package cc.unitmesh.devti.intentions
+
+import cc.unitmesh.devti.AutoDevBundle
+import cc.unitmesh.devti.AutoDevIcons
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.IntentionActionBean
+import com.intellij.icons.AllIcons
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.ListPopupStep
+import com.intellij.openapi.ui.popup.ListSeparator
+import com.intellij.openapi.ui.popup.PopupStep
+import com.intellij.openapi.ui.popup.util.BaseListPopupStep
+import com.intellij.openapi.util.Iconable
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import org.jetbrains.annotations.Nls
+import javax.swing.Icon
+import kotlin.jvm.internal.Intrinsics
+
+class AIAssistantIntention : IntentionAction, Iconable {
+    override fun startInWriteAction(): Boolean = false
+
+    override fun getText(): String = AutoDevBundle.message("intentions.assistant.name")
+    override fun getFamilyName(): String = AutoDevBundle.message("intentions.assistant.name")
+
+    override fun getIcon(flags: Int): Icon {
+        return AutoDevIcons.AI_COPILOT
+    }
+
+    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
+        if (file == null) return false
+
+        val topLevelFile = InjectedLanguageManager.getInstance(project).getTopLevelFile((file as PsiElement?)!!)
+        return topLevelFile?.virtualFile != null
+    }
+
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        val intentions = getAiAssistantIntentions(project, editor, file)
+
+        val title = AutoDevBundle.message("intentions.assistant.popup.title")
+        val popupStep = CustomPopupStep(intentions, project, editor, file, title)
+        val popup = JBPopupFactory.getInstance().createListPopup(popupStep)
+
+        popup.showInBestPositionFor(editor)
+    }
+
+    private fun getAiAssistantIntentions(project: Project, editor: Editor, file: PsiFile): List<IntentionAction> {
+        val intentions: MutableList<IntentionAction> = ArrayList()
+        val extensionList = EP_NAME.extensionList
+
+        for (bean in extensionList) {
+            val intentionActionBean = bean as IntentionActionBean
+            val intention = intentionActionBean.instance
+            if (intention.isAvailable(project, editor, file)) {
+                intentions.add(intention)
+            }
+        }
+
+        return intentions
+    }
+
+    companion object {
+        val EP_NAME: ExtensionPointName<IntentionActionBean> =
+            ExtensionPointName<IntentionActionBean>("cc.unitmesh.devti.aiAssistantIntention")
+    }
+}
+
+class CustomPopupStep(
+    val intentionAction: List<IntentionAction>,
+    val project: Project,
+    val editor: Editor,
+    val psiFile: PsiFile,
+    val popupTitle: @Nls String
+) : BaseListPopupStep<IntentionAction>(popupTitle, intentionAction) {
+    override fun getTextFor(value: IntentionAction): String = value.text
+
+    override fun getSeparatorAbove(value: IntentionAction?): ListSeparator? =
+        if (value != null && Intrinsics.areEqual(value, intentionAction)) ListSeparator() else null
+
+    override fun onChosen(selectedValue: IntentionAction?, finalChoice: Boolean): PopupStep<*>? {
+        val runnable = Runnable { selectedValue!!.invoke(project, editor, psiFile) }
+        return doFinalStep(runnable)
+    }
+}
