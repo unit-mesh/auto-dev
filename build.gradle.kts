71b4ec16a96fcf2cf0d22b2b114fd443cac86c9b
Phodal Huang <h@phodal.com>
2023-07-18 10:09:08 +0800

    docs: update for config

--- build.gradle.kts
+++ build.gradle.kts
@@ -120,6 +120,10 @@ allprojects {
 }
 
 project(":plugin") {
+    apply {
+        plugin("org.jetbrains.changelog")
+    }
+
     version = prop("pluginVersion")
     intellij {
         pluginName.set("autodev")
@@ -227,13 +231,51 @@ project(":plugin") {
             // jvmArgs("-Didea.l10n=true")
         }
 
-        withType<PatchPluginXmlTask> {
-            pluginDescription.set(provider { file("description.html").readText() })
+//        patchPluginXml {
+//            version = properties("pluginVersion")
+//            sinceBuild = properties("pluginSinceBuild")
+//            untilBuild = properties("pluginUntilBuild")
+//
+//            // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+//            pluginDescription = properties("pluginDescription")
+//
+//            val changelog = project.changelog // local variable for configuration cache compatibility
+//            // Get the latest available change notes from the changelog file
+//            changeNotes = properties("pluginVersion").map { pluginVersion ->
+//                with(changelog) {
+//                    renderItem(
+//                        (getOrNull(pluginVersion) ?: getUnreleased())
+//                            .withHeader(false)
+//                            .withEmptySections(false),
+//                        Changelog.OutputType.HTML,
+//                    )
+//                }
+//            }
+//        }
+
+        // Configure UI tests plugin
+        // Read more: https://github.com/JetBrains/intellij-ui-test-robot
+//        runIdeForUiTests {
+//            systemProperty("robot-server.port", "8082")
+//            systemProperty("ide.mac.message.dialogs.as.sheets", "false")
+//            systemProperty("jb.privacy.policy.text", "<!--999.999-->")
+//            systemProperty("jb.consents.confirmation.enabled", "false")
+//        }
+
+        signPlugin {
+            certificateChain = environment("CERTIFICATE_CHAIN")
+            privateKey = environment("PRIVATE_KEY")
+            password = environment("PRIVATE_KEY_PASSWORD")
         }
 
-        withType<PublishPluginTask> {
-            token.set("publishToken")
-//            channels.set(listOf(channel))
+        publishPlugin {
+            dependsOn("patchChangelog")
+            token = environment("PUBLISH_TOKEN")
+            // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+            // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+            // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+            channels =
+                properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
         }
     }
 }
@@ -289,30 +331,30 @@ project(":idea") {
     }
 }
 
-
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
-
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
-}
+//
+//// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+//intellij {
+//    pluginName = properties("pluginName")
+//    version = properties("platformVersion")
+//    type = properties("platformType")
+//
+//    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+//    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+//}
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    groups.empty()
-    repositoryUrl = properties("pluginRepositoryUrl")
-}
+//changelog {
+//    groups.empty()
+//    repositoryUrl = properties("pluginRepositoryUrl")
+//}
 
 // Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath = provider { file(".qodana").canonicalPath }
-    reportPath = provider { file("build/reports/inspections").canonicalPath }
-    saveReport = true
-    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
-}
+//qodana {
+//    cachePath = provider { file(".qodana").canonicalPath }
+//    reportPath = provider { file("build/reports/inspections").canonicalPath }
+//    saveReport = true
+//    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
+//}
 
 fun File.isPluginJar(): Boolean {
     if (!isFile) return false
@@ -333,75 +375,3 @@ fun File.isManifestFile(): Boolean {
 }
 
 
-// Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-kover.xmlReport {
-    onCheck = true
-}
-
-tasks {
-    wrapper {
-        gradleVersion = properties("gradleVersion").get()
-    }
-//
-//    configure<JavaPluginExtension> {
-//        sourceCompatibility = VERSION_17
-//        targetCompatibility = VERSION_17
-//    }
-//
-//    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-//        kotlinOptions {
-//            jvmTarget = VERSION_17.toString()
-//            languageVersion = "1.8"
-//            // see https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library
-//            apiVersion = "1.7"
-//            freeCompilerArgs = listOf("-Xjvm-default=all")
-//        }
-//    }
-
-    patchPluginXml {
-        version = properties("pluginVersion")
-        sinceBuild = properties("pluginSinceBuild")
-        untilBuild = properties("pluginUntilBuild")
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription = properties("pluginDescription")
-
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        }
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
-
-    signPlugin {
-        certificateChain = environment("CERTIFICATE_CHAIN")
-        privateKey = environment("PRIVATE_KEY")
-        password = environment("PRIVATE_KEY_PASSWORD")
-    }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token = environment("PUBLISH_TOKEN")
-        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels =
-            properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
-    }
-}
