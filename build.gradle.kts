59795e2a6531fa1dd628058720990bda3431a85e
Phodal Huang <h@phodal.com>
2023-07-18 12:58:14 +0800

    fix: fix some cofings

--- build.gradle.kts
+++ build.gradle.kts
@@ -252,16 +252,18 @@ project(":plugin") {
 //            privateKey = environment("PRIVATE_KEY")
 //            password = environment("PRIVATE_KEY_PASSWORD")
 //        }
-//
-//        publishPlugin {
-//            dependsOn("patchChangelog")
-//            token = environment("PUBLISH_TOKEN")
-//            // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-//            // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-//            // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-//            channels =
-//                properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+
+//        withType<SignedPluginTask> {
 //        }
+
+        withType<PatchPluginXmlTask> {
+            pluginDescription.set(provider { file("description.html").readText() })
+        }
+
+        withType<PublishPluginTask> {
+            token.set("publishToken")
+            channels.set(properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) })
+        }
     }
 }
 
