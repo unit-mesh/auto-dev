2c8fe386cc747b2d4a34c716c010dfa34d429783
Phodal Huang <h@phodal.com>
2023-07-19 23:41:21 +0800

    refactor: inine code

--- src/main/kotlin/cc/unitmesh/devti/intentions/editor/CodeCompletionIntention.kt
+++ src/main/kotlin/cc/unitmesh/devti/intentions/editor/CodeCompletionIntention.kt
@@ -24,31 +24,10 @@ import kotlin.math.min
 
 
 class CodeCompletionIntention : AbstractChatIntention() {
-    private val documentationTargetPointer: SmartPsiElementPointer<PsiElement>? = null
-
-    companion object {
-        val logger = Logger.getInstance(CodeCompletionIntention::class.java)
-    }
-
-    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
-        return (editor != null) && (file != null)
-    }
-
     override fun getText(): String = AutoDevBundle.message("intentions.chat.code.complete.name")
-
     override fun getFamilyName(): String = AutoDevBundle.message("intentions.chat.code.complete.family.name")
+    override fun getPrompt(project: Project, elementToExplain: PsiElement?): String = "Complete code:"
 
-    override fun getPrompt(project: Project, elementToExplain: PsiElement?): String {
-        return "Complete code for "
-    }
-
-    /**
-     * Invokes the method and performs auto-completion based on the current caret position in the editor.
-     *
-     * @param project the current project
-     * @param editor the editor in which the completion is performed
-     * @param file the PSI file in the editor
-     */
     override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
         if (editor == null || file == null) return
 
@@ -71,7 +50,7 @@ class CodeCompletionIntention : AbstractChatIntention() {
         while (suffixEnd > offset && !EditorActionUtil.isWordBoundary(editor.document.text, suffixEnd, false, false)) {
             suffixEnd--
         }
-//
+
 //        val element = PsiUtilBase.getElementAtCaret(editor) ?: file
 //        val chunksWithPaths = SimilarChunksWithPaths().similarChunksWithPaths(element)
 //        val size = chunksWithPaths.chunks?.size ?: 0
@@ -116,7 +95,7 @@ class CodeCompletionIntention : AbstractChatIntention() {
     ) {
         val flow: Flow<String> = connectorFactory.connector().stream(prompt)
 
-        runBlockingCancellable(indicator) {
+        runBlocking {
             launch {
                 val currentOffset = Ref.IntRef()
                 currentOffset.element = offset
@@ -145,21 +124,25 @@ class CodeCompletionIntention : AbstractChatIntention() {
 
     private val connectorFactory = ConnectorFactory.getInstance()
 
-    fun insertStringAndSaveChange(
-        project: Project,
-        suggestion: String,
-        document: Document,
-        startOffset: Int,
-        withReformat: Boolean
-    ) {
-        document.insertString(startOffset, suggestion)
-        PsiDocumentManager.getInstance(project).commitDocument(document)
+    companion object {
+        val logger = Logger.getInstance(CodeCompletionIntention::class.java)
 
-        if (withReformat) {
-            val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
-            psiFile?.let { file ->
-                val reformatRange = TextRange(startOffset, startOffset + suggestion.length)
-                CodeStyleManager.getInstance(project).reformatText(file, listOf(reformatRange))
+        fun insertStringAndSaveChange(
+            project: Project,
+            suggestion: String,
+            document: Document,
+            startOffset: Int,
+            withReformat: Boolean
+        ) {
+            document.insertString(startOffset, suggestion)
+            PsiDocumentManager.getInstance(project).commitDocument(document)
+
+            if (withReformat) {
+                val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
+                psiFile?.let { file ->
+                    val reformatRange = TextRange(startOffset, startOffset + suggestion.length)
+                    CodeStyleManager.getInstance(project).reformatText(file, listOf(reformatRange))
+                }
             }
         }
     }
