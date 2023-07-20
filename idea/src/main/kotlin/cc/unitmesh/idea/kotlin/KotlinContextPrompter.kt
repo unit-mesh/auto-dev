b3493b102dca3e98584ad4d930bfc4a51b6be0e5
Phodal Huang <h@phodal.com>
2023-07-20 13:50:49 +0800

    feat(kotlin): init kotlin context prompter

---
+++ idea/src/main/kotlin/cc/unitmesh/idea/kotlin/KotlinContextPrompter.kt
@@ -0,0 +1,50 @@
+package cc.unitmesh.idea.kotlin
+
+import cc.unitmesh.devti.context.chunks.SimilarChunksWithPaths
+import cc.unitmesh.devti.gui.chat.ChatBotActionType
+import cc.unitmesh.devti.provider.ContextPrompter
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+class KotlinContextPrompter : ContextPrompter() {
+    private var action: ChatBotActionType? = null
+    private var selectedText: String = ""
+    private var file: PsiFile? = null
+    private var project: Project? = null
+    private var lang: String = ""
+
+    override fun initContext(actionType: ChatBotActionType, prefixText: String, file: PsiFile?, project: Project) {
+        this.action = actionType
+        this.selectedText = prefixText
+        this.file = file
+        this.project = project
+        this.lang = file?.language?.displayName ?: ""
+    }
+
+    override fun getUIPrompt(): String {
+        val chunkContext = SimilarChunksWithPaths().similarChunksWithPaths(file!!).toQuery()
+
+        return """$action for the code:
+            ```${lang}
+            $chunkContext
+            $selectedText
+            ```
+            """.trimIndent()
+    }
+
+    override fun getRequestPrompt(): String {
+        val chunkContext = SimilarChunksWithPaths().similarChunksWithPaths(file!!).toQuery()
+
+        return """$action for the code:
+            ```${lang}
+            $chunkContext
+            $selectedText
+            ```
+            """.trimIndent()
+    }
+
+    companion object {
+        val logger = Logger.getInstance(KotlinContextPrompter::class.java)
+    }
+}
