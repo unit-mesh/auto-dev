4cb47f035da554e30fc44b2e42e878922d43b69f
Phodal Huang <h@phodal.com>
2023-07-20 20:45:34 +0800

    feat: update for tasks

--- src/main/kotlin/cc/unitmesh/devti/intentions/editor/CodeCompletionIntention.kt
+++ src/main/kotlin/cc/unitmesh/devti/intentions/editor/CodeCompletionIntention.kt
@@ -65,121 +65,12 @@ class CodeCompletionIntention : AbstractChatIntention() {
         // TODO: use suffix to improve the completion
 //        val suffix = document.getText(TextRange(offset, suffixEnd))
 
-        logger.warn("Prompt: $prompt")
-
-        updateCodeCompletion(prompt, editor, offset)
-
-        val task = object : Task.Backgroundable(project, "Code completion", true) {
-            override fun run(indicator: ProgressIndicator) {
-                updateCodeCompletion(prompt, editor, offset)
-            }
-        }
-
+        val task = CodeCompletionTask(editor, prompt, offset)
         ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
 
     }
 
-    private val writeActionGroupId = "code.complete.intention.write.action"
-
-    /**
-     * Renders an inlay with the given prompt in the specified editor at the specified offset.
-     *
-     * @param prompt The prompt to display in the inlay.
-     * @param editor The editor in which to render the inlay.
-     * @param offset The offset at which to render the inlay.
-     */
-    private fun updateCodeCompletion(
-        prompt: @NlsSafe String,
-        editor: Editor,
-        offset: Int,
-    ) {
-        val flow: Flow<String> = connectorFactory.connector().stream(prompt)
-        LLMCoroutineScopeService.scope(editor.project!!).launch {
-            val currentOffset = Ref.IntRef()
-            currentOffset.element = offset
-
-            val project = editor.project!!
-            val suggestion = StringBuilder()
-            flow.collect {
-                invokeLater {
-                    WriteCommandAction.runWriteCommandAction(
-                        project,
-                        AutoDevBundle.message("intentions.chat.code.complete.name"),
-                        writeActionGroupId,
-                        {
-                            insertStringAndSaveChange(project, it, editor.document, currentOffset.element, false)
-                        }
-                    )
-
-                    currentOffset.element += it.length
-                    editor.caretModel.moveToOffset(currentOffset.element)
-                }
-            }
-
-            logger.warn("Suggestion: $suggestion")
-        }
-    }
-
-    private fun insertLine(
-        project: Project,
-        suggestionLine: String,
-        editor: Editor,
-        currentOffset: Ref.IntRef
-    ) {
-        WriteCommandAction.runWriteCommandAction(
-            project,
-            AutoDevBundle.message("intentions.chat.code.complete.name"),
-            writeActionGroupId,
-            {
-                insertStringAndSaveChange(
-                    project,
-                    suggestionLine,
-                    editor.document,
-                    currentOffset.element,
-                    false
-                )
-            }
-        )
-
-        currentOffset.element += suggestionLine.length
-        editor.caretModel.moveToOffset(currentOffset.element)
-    }
-
-    private val connectorFactory = ConnectorFactory.getInstance()
-
     companion object {
         val logger = Logger.getInstance(CodeCompletionIntention::class.java)
-
-        fun updateDocument(
-            project: Project,
-            suggestion: String,
-            document: Document,
-            textRange: TextRange
-        ) {
-            document.replaceString(textRange.startOffset, textRange.endOffset, suggestion)
-            PsiDocumentManager.getInstance(project).commitDocument(document)
-            val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
-            val reformatRange = TextRange(textRange.startOffset, textRange.startOffset + suggestion.length)
-            CodeStyleManager.getInstance(project).reformatText(psiFile!!, listOf(reformatRange))
-        }
-
-        fun insertStringAndSaveChange(
-            project: Project,
-            suggestion: String,
-            document: Document,
-            startOffset: Int,
-            withReformat: Boolean
-        ) {
-            document.insertString(startOffset, suggestion)
-            PsiDocumentManager.getInstance(project).commitDocument(document)
-
-            if (withReformat) {
-                val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
-                psiFile?.let { file ->
-                    val reformatRange = TextRange(startOffset, startOffset + suggestion.length)
-                    CodeStyleManager.getInstance(project).reformatText(file, listOf(reformatRange))
-                }
-            }
-        }
     }
 }
---
+++ src/main/kotlin/cc/unitmesh/devti/intentions/editor/CodeCompletionTask.kt
@@ -0,0 +1,83 @@
+package cc.unitmesh.devti.intentions.editor
+
+import cc.unitmesh.devti.AutoDevBundle
+import cc.unitmesh.devti.models.ConnectorFactory
+import cc.unitmesh.devti.models.LLMCoroutineScopeService
+import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.codeStyle.CodeStyleManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlin.jvm.internal.Ref
+
+class CodeCompletionTask(
+    val editor: Editor,
+    val prompt: String,
+    val offset: Int,
+) : Task.Backgroundable(editor.project, AutoDevBundle.message("intentions.chat.code.complete.name")) {
+
+    private val connectorFactory = ConnectorFactory.getInstance()
+
+    private val writeActionGroupId = "code.complete.intention.write.action"
+    override fun run(indicator: ProgressIndicator) {
+        val flow: Flow<String> = connectorFactory.connector().stream(prompt)
+        LLMCoroutineScopeService.scope(editor.project!!).launch {
+            val currentOffset = Ref.IntRef()
+            currentOffset.element = offset
+
+            val project = editor.project!!
+            val suggestion = StringBuilder()
+            flow.collect {
+                suggestion.append(it)
+                invokeLater {
+                    WriteCommandAction.runWriteCommandAction(
+                        project,
+                        AutoDevBundle.message("intentions.chat.code.complete.name"),
+                        writeActionGroupId,
+                        {
+                            insertStringAndSaveChange(project, it, editor.document, currentOffset.element, false)
+                        }
+                    )
+
+                    currentOffset.element += it.length
+                    editor.caretModel.moveToOffset(currentOffset.element)
+                }
+            }
+
+            CodeCompletionIntention.logger.warn("Suggestion: $suggestion")
+        }
+
+    }
+
+    companion object {
+        val logger = Logger.getInstance(CodeCompletionIntention::class.java)
+
+        fun insertStringAndSaveChange(
+            project: Project,
+            suggestion: String,
+            document: Document,
+            startOffset: Int,
+            withReformat: Boolean
+        ) {
+            document.insertString(startOffset, suggestion)
+            PsiDocumentManager.getInstance(project).commitDocument(document)
+
+            if (withReformat) {
+                val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
+                psiFile?.let { file ->
+                    val reformatRange = TextRange(startOffset, startOffset + suggestion.length)
+                    CodeStyleManager.getInstance(project).reformatText(file, listOf(reformatRange))
+                }
+            }
+        }
+    }
+}
