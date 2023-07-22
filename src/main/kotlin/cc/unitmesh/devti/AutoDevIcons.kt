9b20afb7d444d796d63a071c8cfd93dc069be02b
Phodal Huang <h@phodal.com>
2023-07-22 09:42:59 +0800

    feat: update for tool Window icon

--- src/main/kotlin/cc/unitmesh/devti/AutoDevIcons.kt
+++ src/main/kotlin/cc/unitmesh/devti/AutoDevIcons.kt
@@ -3,9 +3,10 @@ package cc.unitmesh.devti
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
-class AutoDevIcons {
-    companion object {
-        val STORY: Icon = IconLoader.getIcon("/icons/story.svg", AutoDevIcons::class.java)
-        val AI_COPILOT: Icon = IconLoader.getIcon("/icons/ai-copilot.svg", AutoDevIcons::class.java)
-    }
+object AutoDevIcons {
+    @JvmField
+    val STORY: Icon = IconLoader.getIcon("/icons/story.svg", AutoDevIcons::class.java)
+
+    @JvmField
+    val AI_COPILOT: Icon = IconLoader.getIcon("/icons/ai-copilot.svg", AutoDevIcons::class.java)
 }
