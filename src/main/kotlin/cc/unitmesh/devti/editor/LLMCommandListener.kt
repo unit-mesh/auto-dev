e5c42bd9d007345b5c41969857e3348807a460d1
Phodal Huang <h@phodal.com>
2023-07-22 11:48:35 +0800

    feat(inlay): init command listener

---
+++ src/main/kotlin/cc/unitmesh/devti/editor/LLMCommandListener.kt
@@ -0,0 +1,8 @@
+package cc.unitmesh.devti.editor
+
+import com.intellij.openapi.command.CommandListener
+import com.intellij.openapi.project.Project
+
+class LLMCommandListener(private val project: Project) : CommandListener {
+
+}
