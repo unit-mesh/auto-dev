ae53342d2ee4dce9f4a257f5691942c4f9b71ab2
Phodal Huang <h@phodal.com>
2023-07-23 09:59:58 +0800

    docs: update todo

--- src/main/kotlin/cc/unitmesh/devti/actions/chat/FixThisBotAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/actions/chat/FixThisBotAction.kt
@@ -178,7 +178,7 @@ class FixThisBotAction : ChatBaseAction() {
 }
 
 private fun ProjectFileIndex.isInProject(virtualFile: VirtualFile, project: Project): Boolean {
-    // https://github.com/JetBrains/intellij-community/blob/master/platform/core-impl/src/com/intellij/psi/search/ProjectScopeImpl.java#L25
+    // https://github.com/JetBrains/intellij-community/blob/master/platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ProjectFileIndexImpl.java#L32
     // new version has better method
     if (virtualFile.path.startsWith(project.basePath ?: return false)) {
         return true
