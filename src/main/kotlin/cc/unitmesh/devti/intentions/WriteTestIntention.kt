b345b5e08e042b93194c10d06a2c29aa863ef72d
Phodal Huang <h@phodal.com>
2023-07-28 09:09:29 +0800

    refactor(test): extract test task

--- src/main/kotlin/cc/unitmesh/devti/intentions/WriteTestIntention.kt
+++ src/main/kotlin/cc/unitmesh/devti/intentions/WriteTestIntention.kt
@@ -11,15 +11,15 @@ import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
 import cc.unitmesh.devti.provider.context.ChatOrigin
 import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -35,72 +35,77 @@ class WriteTestIntention : AbstractChatIntention() {
         val element = getElementToAction(project, editor) ?: return
         selectElement(element, editor)
 
-        val selectedText = element.text
-
-        val actionType = ChatActionType.WRITE_TEST
-
-        val lang = file.language.displayName
-
         LLMCoroutineScopeService.scope(project).launch {
-            WriteAction.runAndWait<Throwable> {
-                val testContextProvider = TestContextProvider.context(lang)
-                val testContext = testContextProvider?.findOrCreateTestFile(file, project, element)
-                if (testContext == null) {
-                    logger<WriteTestIntention>().error("Failed to create test file for: $file")
-                    return@runAndWait
+            writeTestTask(file, element, project, editor)
+        }
+    }
+
+    private fun writeTestTask(file: PsiFile, element: PsiElement, project: Project, editor: Editor) {
+        WriteAction.runAndWait<Throwable> {
+            val lang = file.language.displayName
+
+            val selectedText = element.text
+            val actionType = ChatActionType.WRITE_TEST
+
+            val testContextProvider = TestContextProvider.context(lang)
+            val testContext = testContextProvider?.findOrCreateTestFile(file, project, element)
+            if (testContext == null) {
+                logger<WriteTestIntention>().error("Failed to create test file for: $file")
+                return@runAndWait
+            }
+
+            runBlocking {
+                var prompter = if (testContext.isNewFile) {
+                    """Write unit test for following code. 
+                                | You MUST return code only, not explain.
+                                | You MUST use given-when-then style.
+                                | You MUST use should_xx style for test method name.
+                                | When testing controller, you MUST use MockMvc and test API only.
+                                | """.trimMargin()
+                } else {
+                    """Write unit test for following code. 
+                                | You MUST return method code only, not java class, no explain.
+                                | You MUST use given-when-then style.
+                                | You MUST use should_xx style for test method name.
+                                | When testing controller, you MUST use MockMvc and test API only.
+                                | You MUST return start with @Test annotation.
+                                | """.trimMargin()
                 }
 
-                runBlocking {
-                    var prompter = if (testContext.isNewFile) {
-                        """Write unit test for following code. 
-                            | You MUST return code only, not explain.
-                            | You MUST use given-when-then style.
-                            | You MUST use should_xx style for test method name.
-                            | When testing controller, you MUST use MockMvc and test API only.
-                            | """.trimMargin()
+                val creationContext = ChatCreationContext(ChatOrigin.Intention, actionType, file)
+                val chatContextItems = ChatContextProvider.collectChatContextList(project, creationContext)
+                chatContextItems.forEach {
+                    prompter += it.text
+                }
+
+                prompter += "\n"
+
+                // sometimes, we need to wait for the project to be smart or create test package,
+                // so we need to run this in a smart mode, it will wait for the project to be smart
+                DumbService.getInstance(project).runWhenSmart {
+                    val additionContext = testContext.relatedClass.joinToString("\n") {
+                        it.toQuery()
+                    }.lines().joinToString("\n") {
+                        "// $it"
+                    }
+
+
+                    prompter += additionContext
+
+                    prompter += "\n```${lang.lowercase()}\n$selectedText\n```\n"
+
+                    prompter += if (!testContext.isNewFile) {
+                        "Start writing test method code here:  \n"
                     } else {
-                        """Write unit test for following code. 
-                            | You MUST return method code only, not java class, no explain.
-                            | You MUST use given-when-then style.
-                            | You MUST use should_xx style for test method name.
-                            | When testing controller, you MUST use MockMvc and test API only.
-                            | You MUST return start with @Test annotation.
-                            | """.trimMargin()
+                        "Start with `import` syntax here:  \n"
                     }
 
-                    val creationContext = ChatCreationContext(ChatOrigin.Intention, actionType, file)
-                    val chatContextItems = ChatContextProvider.collectChatContextList(project, creationContext)
-                    chatContextItems.forEach {
-                        prompter += it.text
-                    }
+                    val flow: Flow<String> = ConnectorFactory.getInstance().connector(project).stream(prompter, "")
+                    logger<WriteTestIntention>().warn("Prompt: $prompter")
+                    writeTestToFile(project, flow, testContext, testContextProvider)
 
-                    prompter += "\n"
-
-                    DumbService.getInstance(project).runWhenSmart {
-                        val additionContext = testContext.relatedClass.joinToString("\n") {
-                            it.toQuery()
-                        }.lines().joinToString("\n") {
-                            "// $it"
-                        }
-
-
-                        prompter += additionContext
-
-                        prompter += "\n```${lang.lowercase()}\n$selectedText\n```\n"
-
-                        prompter += if (!testContext.isNewFile) {
-                            "Start writing test method code here:  \n"
-                        } else {
-                            "Start with `import` syntax here:  \n"
-                        }
-
-                        val flow: Flow<String> = ConnectorFactory.getInstance().connector(project).stream(prompter, "")
-                        logger<WriteTestIntention>().warn("Prompt: $prompter")
-                        writeTestToFile(project, flow, testContext, testContextProvider)
-
-                        // navigate to the test file
-                        navigateTestFile(testContext.file, editor, project)
-                    }
+                    // navigate to the test file
+                    navigateTestFile(testContext.file, editor, project)
                 }
             }
         }
