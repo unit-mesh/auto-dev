b9450daa6bab4932926c3771c4009833ee33adac
Phodal Huang <h@phodal.com>
2023-07-29 15:29:54 +0800

    refactor: align code util for parse code

---
+++ src/main/kotlin/cc/unitmesh/devti/parser/CodeUtil.kt
@@ -0,0 +1,70 @@
+package cc.unitmesh.devti.parser
+
+import com.intellij.lang.Language
+
+class Code(val language: Language, val text: String, val isComplete: Boolean) {
+    companion object {
+        fun parse(content: String): Code {
+            val regex = Regex("```([\\w#+]*)")
+            val lines = content.lines()
+
+            var codeStarted = false
+            var codeClosed = false
+            var languageId: String? = null
+            val codeBuilder = StringBuilder()
+
+            for (line in lines) {
+                if (!codeStarted) {
+                    val matchResult: MatchResult? = regex.find(line.trimStart())
+                    if (matchResult != null) {
+                        val substring = matchResult.groups[1]?.value
+                        languageId = substring
+                        codeStarted = true
+                    }
+                } else if (line.startsWith("```")) {
+                    codeClosed = true
+                    break
+                } else {
+                    codeBuilder.append(line).append("\n")
+                }
+            }
+
+            var startIndex = 0
+            var endIndex = codeBuilder.length - 1
+
+            while (startIndex <= endIndex) {
+                if (!codeBuilder[startIndex].isWhitespace()) {
+                    break
+                }
+                startIndex++
+            }
+
+            while (endIndex >= startIndex) {
+                if (!codeBuilder[endIndex].isWhitespace()) {
+                    break
+                }
+                endIndex--
+            }
+
+            val trimmedCode = codeBuilder.substring(startIndex, endIndex + 1).toString()
+            val language = findLanguage(languageId!!)
+            return Code(language, trimmedCode, codeClosed)
+        }
+
+        private fun findLanguage(languageName: String): Language {
+            val fixedLanguage = when (languageName) {
+                "csharp" -> "c#"
+                "fsharp" -> "f#"
+                "cpp" -> "c++"
+                else -> languageName
+            }
+
+            val languages = Language.getRegisteredLanguages()
+            val registeredLanguages = languages
+                .filter { it.displayName.isNotEmpty() }
+
+            return registeredLanguages.find { it.displayName.equals(fixedLanguage, ignoreCase = true) }
+                ?: Language.ANY
+        }
+    }
+}
---
+++ src/test/kotlin/cc/unitmesh/devti/parser/CodeUtilTest.kt
@@ -0,0 +1,50 @@
+package cc.unitmesh.devti.parser
+
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class CodeUtilTest {
+    @Test
+    fun should_parse_code_from_markdown_java_hello_world() {
+        val markdown = """
+            |```java
+            |public class HelloWorld {
+            |    public static void main(String[] args) {
+            |        System.out.println("Hello, World");
+            |    }
+            |}
+            |```
+        """.trimMargin()
+
+        val code = Code.parse(markdown)
+
+//        assertEquals(code.language.id, "java")
+        assertEquals(code.text, """
+            |public class HelloWorld {
+            |    public static void main(String[] args) {
+            |        System.out.println("Hello, World");
+            |    }
+            |}
+        """.trimMargin())
+        assertTrue(code.isComplete)
+    }
+
+    @Test
+    fun should_handle_code_not_complete_from_markdown() {
+        val markdown = """
+            |```java
+            |public class HelloWorld {
+            |    public static void main(String[] args) {
+            |        System.out.println("Hello, World");
+        """.trimMargin()
+
+        val code = Code.parse(markdown)
+        assertEquals(code.text, """
+            |public class HelloWorld {
+            |    public static void main(String[] args) {
+            |        System.out.println("Hello, World");
+        """.trimMargin())
+        assertTrue(!code.isComplete)
+    }
+}
