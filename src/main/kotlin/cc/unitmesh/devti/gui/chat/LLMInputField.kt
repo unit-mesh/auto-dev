61d822d6b080e62c1e10b2d3f1d06b3aeff81d39
Phodal Huang <h@phodal.com>
2023-07-29 23:06:38 +0800

    feat(ui): make chat works

--- src/main/kotlin/cc/unitmesh/devti/gui/chat/LLMInputField.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/chat/LLMInputField.kt
@@ -2,21 +2,23 @@ package cc.unitmesh.devti.gui.chat
 
 import cc.unitmesh.devti.AutoDevBundle
 import cc.unitmesh.devti.provider.ContextPrompter
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CustomShortcutSet
-import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.editor.EditorModificationUtil
+import com.intellij.openapi.editor.actions.EnterAction
+import com.intellij.openapi.editor.actions.IncrementalFindAction
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.fileTypes.FileTypes
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.ui.EditorTextField
+import com.intellij.util.messages.MessageBusConnection
 import com.intellij.util.ui.JBUI
 import java.awt.Color
-import java.awt.event.KeyAdapter
+import java.awt.event.ActionEvent
 import java.awt.event.KeyEvent
 import javax.swing.KeyStroke
 
@@ -25,11 +27,13 @@ class LLMInputField(
     chatCodingService: ChatCodingService,
     component: ChatCodingComponent,
     private val listeners: List<DocumentListener>,
-) : EditorTextField(project, FileTypes.PLAIN_TEXT) {
+) : EditorTextField(project, FileTypes.PLAIN_TEXT), Disposable {
     init {
         isOneLineMode = false
         updatePlaceholderText()
+        setFontInheritedFromLAF(true)
         addSettingsProvider {
+            it.putUserData(IncrementalFindAction.SEARCH_DISABLED, true)
             it.colorsScheme.lineSpacing = 1.0f
             it.settings.isUseSoftWraps = true
             it.settings.isPaintSoftWraps = false
@@ -37,7 +41,7 @@ class LLMInputField(
             it.contentComponent.setOpaque(false)
         }
 
-        DumbAwareAction.create { e: AnActionEvent? ->
+        DumbAwareAction.create {
             object : AnAction() {
                 override fun actionPerformed(e1: AnActionEvent) {
                     val editor = this@LLMInputField.editor ?: return
@@ -57,6 +61,31 @@ class LLMInputField(
                 KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, KeyEvent.META_DOWN_MASK), null)
             ), this
         )
+
+
+        val connect: MessageBusConnection = project.messageBus.connect(this)
+        val topic = AnActionListener.TOPIC
+
+        connect.subscribe(topic, object : AnActionListener {
+            override fun afterActionPerformed(action: AnAction, event: AnActionEvent, result: AnActionResult) {
+                if (event.dataContext.getData(CommonDataKeys.EDITOR) === this@LLMInputField.editor && action is EnterAction) {
+                    // todo: move this to a service
+                    val listener: (ActionEvent) -> Unit = {
+                        val prompt = this@LLMInputField.text
+                        this@LLMInputField.text = ""
+
+                        val context = ChatContext(null, "", "")
+                        chatCodingService.actionType = ChatActionType.REFACTOR
+                        chatCodingService.handlePromptAndResponse(component, object : ContextPrompter() {
+                            override fun displayPrompt() = prompt
+                            override fun requestPrompt() = prompt
+                        }, context)
+                    }
+
+                    listener.invoke(ActionEvent(this@LLMInputField, 0, ""))
+                }
+            }
+        })
     }
 
     private fun updatePlaceholderText() {
@@ -82,5 +111,9 @@ class LLMInputField(
             return colorsScheme.defaultBackground
         }
         return super.getBackground()
+    }
+
+    override fun dispose() {
+        listeners.forEach { editor?.document?.removeDocumentListener(it) }
     }
 }
