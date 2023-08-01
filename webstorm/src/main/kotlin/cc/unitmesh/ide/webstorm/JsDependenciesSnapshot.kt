a9825af5768f8f30847debe0d8e7485be530f665
Phodal Huang <h@phodal.com>
2023-08-01 21:20:48 +0800

    refactor: clean code

--- webstorm/src/main/kotlin/cc/unitmesh/ide/webstorm/JsDependenciesSnapshot.kt
+++ webstorm/src/main/kotlin/cc/unitmesh/ide/webstorm/JsDependenciesSnapshot.kt
@@ -1,6 +1,7 @@
 package cc.unitmesh.ide.webstorm
 
 import cc.unitmesh.devti.provider.context.ChatCreationContext
+import cc.unitmesh.ide.webstorm.provider.MOST_POPULAR_PACKAGES
 import com.intellij.javascript.nodejs.PackageJsonData
 import com.intellij.javascript.nodejs.packageJson.PackageJsonFileManager
 import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
@@ -15,91 +16,14 @@ class JsDependenciesSnapshot(
     val packages: Map<String, PackageJsonData.PackageJsonDependencyEntry>
 ) {
     companion object {
-        private const val TYPESCRIPT_PACKAGE = "typescript"
-        val mostPopularPackages = setOf(
-            "lodash",
-            "request",
-            "commander",
-            "react",
-            "express",
-            "async",
-            "moment",
-            "prop-types",
-            "react-dom",
-            "bluebird",
-            "underscore",
-            "vue",
-            "axios",
-            "tslib",
-            "glob",
-            "yargs",
-            "colors",
-            "webpack",
-            "uuid",
-            "classnames",
-            "minimist",
-            "body-parser",
-            "rxjs",
-            "babel-runtime",
-            "jquery",
-            "babel-core",
-            "core-js",
-            "babel-loader",
-            "cheerio",
-            "rimraf",
-            "eslint",
-            "dotenv",
-            TYPESCRIPT_PACKAGE,
-            "@types/node",
-            "@angular/core",
-            "@angular/common",
-            "redux",
-            "gulp",
-            "node-fetch",
-            "@angular/platform-browser",
-            "@babel/runtime",
-            "handlebars",
-            "@angular/compiler",
-            "aws-sdk",
-            "@angular/forms",
-            "webpack-dev-server",
-            "@angular/platform-browser-dynamic",
-            "mocha",
-            "socket.io",
-            "ws",
-            "node-sass",
-            "@angular/router",
-            "ramda",
-            "react-redux",
-            "@babel/core",
-            "@angular/http",
-            "ejs",
-            "coffee-script",
-            "mongodb",
-            "chai",
-            "mongoose",
-            "xml2js",
-            "bootstrap",
-            "jest",
-            "redis",
-            "vue-router",
-            "optimist",
-            "promise",
-            "@angular/animations",
-            "postcss",
-            "morgan",
-            "less",
-            "immutable"
-        )
-
         fun create(
             project: Project,
             creationContext: ChatCreationContext
         ): JsDependenciesSnapshot {
             var packageJsonFiles = emptySet<VirtualFile>()
             var resolvedPackageJson = false
-            val sourceFile = creationContext.sourceFile
-            val virtualFile = sourceFile?.virtualFile
+
+            val virtualFile = creationContext.sourceFile?.virtualFile
             if (virtualFile != null) {
                 val packageJson = PackageJsonUtil.findUpPackageJson(virtualFile)
                 if (packageJson != null) {
@@ -125,12 +49,13 @@ class JsDependenciesSnapshot(
         }
 
         private fun findTsConfigs(project: Project, set: Set<VirtualFile>): Set<VirtualFile> {
-            val mapNotNull = set.asSequence().mapNotNull { it ->
+            val mapNotNull = set.asSequence().mapNotNull {
                 it.parent?.findChild("tsconfig.json")
             }
 
-            return mapNotNull.plus(project.guessProjectDir()?.findChild("tsconfig.json"))
-                .filterNotNull().toSet()
+            val rootConfig = project.guessProjectDir()?.findChild("tsconfig.json")
+
+            return mapNotNull.plus(rootConfig).filterNotNull().toSet()
         }
     }
 }
