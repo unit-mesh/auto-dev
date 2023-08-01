02f905b445356fbc6b6c2ec66b6b60a1f5cf499e
Phodal Huang <h@phodal.com>
2023-08-01 18:47:27 +0800

    fix: fix error level issues

--- src/main/kotlin/cc/unitmesh/devti/gui/error/CodeBlockHighlightingFilter.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/error/CodeBlockHighlightingFilter.kt
@@ -1,17 +1,25 @@
 package cc.unitmesh.devti.gui.error
 
-import cc.unitmesh.devti.gui.block.AutoDevSnippetFile.isSnippet
+import cc.unitmesh.devti.gui.block.AutoDevSnippetFile
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 
 class CodeBlockHighlightingFilter : HighlightInfoFilter {
     override fun accept(highlightInfo: HighlightInfo, file: PsiFile?): Boolean {
-        val virtualFile = file?.virtualFile ?: return true
+        val hasError = highlightInfo.getSeverity().compareTo(HighlightSeverity.GENERIC_SERVER_ERROR_OR_WARNING) >= 0;
 
-        return highlightInfo.severity >= HighlightSeverity.GENERIC_SERVER_ERROR_OR_WARNING && isSnippet(
-            virtualFile
-        )
+        if (file == null || !hasError) {
+            return true;
+        }
+
+        val virtualFile = file.getVirtualFile();
+        if (virtualFile != null && AutoDevSnippetFile.isSnippet(virtualFile)) {
+            return false;
+        }
+
+        return true;
     }
 }
