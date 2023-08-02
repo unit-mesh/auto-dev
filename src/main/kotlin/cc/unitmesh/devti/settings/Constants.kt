b6354b6a1fcfec416eb2d5e767c6c061a71d4493
温州程序员劝退师 <adaihahaha@hotmail.com>
2023-08-02 09:31:09 +0800

    feat(kotlin): add model gpt-3.5-turbo-16k this is an enhanced model with an increased token limit of 16k, which is more advantageous for handling large document texts

--- src/main/kotlin/cc/unitmesh/devti/settings/Constants.kt
+++ src/main/kotlin/cc/unitmesh/devti/settings/Constants.kt
@@ -1,6 +1,6 @@
 package cc.unitmesh.devti.settings
 
-val OPENAI_MODEL = arrayOf("gpt-3.5-turbo", "gpt-4")
+val OPENAI_MODEL = arrayOf("gpt-3.5-turbo","gpt-3.5-turbo-16k", "gpt-4")
 val AI_ENGINES = arrayOf("OpenAI", "Custom", "Azure")
 val DEFAULT_AI_ENGINE = AI_ENGINES[0]
 
