483d64794b20e76d2515b7cedbbe4975618fb576
Phodal Huang <h@phodal.com>
2023-08-04 13:02:23 +0800

    fix: fix not file issues

--- src/main/kotlin/cc/unitmesh/devti/provider/builtin/DefaultContextPrompter.kt
+++ src/main/kotlin/cc/unitmesh/devti/provider/builtin/DefaultContextPrompter.kt
@@ -5,11 +5,19 @@ import cc.unitmesh.devti.provider.ContextPrompter
 
 class DefaultContextPrompter : ContextPrompter() {
     override fun displayPrompt(): String {
+        if (file == null) {
+            return "$action\n```${lang}\n$selectedText\n```"
+        }
+
         val chunkContext = SimilarChunksWithPaths.createQuery(file!!)
         return "$action\n```${lang}\n$chunkContext\n$selectedText\n```"
     }
 
     override fun requestPrompt(): String {
+        if (file == null) {
+            return "$action\n```${lang}\n$selectedText\n```"
+        }
+
         val chunkContext = SimilarChunksWithPaths.createQuery(file!!)
         return "$action\n```${lang}\n$chunkContext\n$selectedText\n```"
     }
