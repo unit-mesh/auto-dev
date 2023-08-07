c18071142f33bc6ccde02fc94632f28920fbe2a6
Phodal Huang <h@phodal.com>
2023-08-07 21:59:39 +0800

    feat(co): split to new module for plugin size

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "intellij-autodev"
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+include("companion")
 include("plugin")
 
 include(
