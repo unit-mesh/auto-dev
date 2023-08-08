3bd2387d3824889d21dafe07c1afc2554063acc3
Phodal Huang <h@phodal.com>
2023-08-08 10:34:01 +0800

    feat: make api works

--- companion/src/main/kotlin/cc/unitmesh/comp/actions/TalkWithApiAction.kt
+++ companion/src/main/kotlin/cc/unitmesh/comp/actions/TalkWithApiAction.kt
@@ -1,8 +1,11 @@
 package cc.unitmesh.comp.actions
 
 import cc.unitmesh.devti.AutoDevBundle
+import cc.unitmesh.devti.gui.chat.ChatActionType
+import cc.unitmesh.devti.gui.sendToChatWindow
 import cc.unitmesh.devti.intentions.AbstractChatIntention
 import chapi.ast.javaast.JavaAnalyser
+import chapi.domain.core.CodeDataStruct
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.editor.Editor
@@ -17,6 +20,8 @@ import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import org.archguard.scanner.analyser.backend.JavaApiAnalyser
 import org.archguard.scanner.core.sourcecode.ContainerService
+import org.archguard.scanner.core.sourcecode.ContainerSupply
+import java.awt.EventQueue.invokeLater
 
 class TalkWithApiAction : AbstractChatIntention() {
     override fun getText(): String = AutoDevBundle.message("intentions.companion.api.name")
@@ -34,22 +39,44 @@ class TalkWithApiAction : AbstractChatIntention() {
                     }
                 }
 
-                val javaApiAnalyser = JavaApiAnalyser()
-
-                val controllerFiles: List<ContainerService> = psiFiles.asSequence()
+                val apiList: List<String> = psiFiles.asSequence()
                     .filter { it.name.endsWith("Controller.java") }.map {
-                        JavaAnalyser().analysis(it.text, it.virtualFile.path).DataStructures
+                        ReadAction.compute<List<CodeDataStruct>, Throwable> {
+                            JavaAnalyser().analysis(it.text, it.virtualFile.path).DataStructures
+                        }
                     }.flatten().map {
+                        val javaApiAnalyser = JavaApiAnalyser()
                         javaApiAnalyser.analysisByNode(it, "")
                         javaApiAnalyser.toContainerServices()
-                    }.flatten().toList()
+                    }.flatten()
+                    .map {
+                        it.resources
+                    }.flatten()
+                    .map {
+                        val methodInfo = it.packageName + "." + it.className + "." + it.methodName
+                        it.sourceHttpMethod + " " + it.sourceUrl + " (" + methodInfo + ")"
+                    }
+                    .toList()
 
-                println(controllerFiles)
+                invokeLater {
+                    sendToChatWindow(project, ChatActionType.CHAT) { contentPanel, _ ->
+                        contentPanel.setInput(
+                            """You are an experienced software developer who can help me understand this system.
+
+Here is all this system's APIs:
+${apiList.joinToString("\n")}
+
+If you are understand, reply me with "I understand".
+"""
+                        )
+                    }
+                }
             }
         }
 
         ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
     }
+
 
 
 }
