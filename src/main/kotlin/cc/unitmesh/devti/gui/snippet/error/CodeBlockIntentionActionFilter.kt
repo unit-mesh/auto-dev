6c5f295de5eea6c62242376f707a781fe1a43e30
Phodal Huang <h@phodal.com>
2023-08-09 18:19:05 +0800

    refactor: reorg code

--- src/main/kotlin/cc/unitmesh/devti/gui/snippet/error/CodeBlockIntentionActionFilter.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/snippet/error/CodeBlockIntentionActionFilter.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.temporary.gui.block.error
+package cc.unitmesh.devti.gui.snippet.error
 
 import com.intellij.temporary.gui.block.AutoDevSnippetFile.isSnippet
 import com.intellij.codeInsight.daemon.impl.IntentionActionFilter
