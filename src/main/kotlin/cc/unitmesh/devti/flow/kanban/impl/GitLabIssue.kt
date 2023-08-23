ab0d91d1d7084b3f90f3bad56451d6e4af4eaec6
温州程序员劝退师 <adaihahaha@hotmail.com>
2023-08-23 18:34:19 +0800

    feat(kanban): add GitLabIssue implementation This commit adds the implementation for the GitLabIssue class, which is responsible for interacting with GitLab's issue tracking system. It includes methods for retrieving project information, getting stories, getting a story by ID, and updating story details. The implementation uses the GitLab4J library to communicate with the GitLab

---
+++ src/main/kotlin/cc/unitmesh/devti/flow/kanban/impl/GitLabIssue.kt
@@ -0,0 +1,43 @@
+package cc.unitmesh.devti.flow.kanban.impl
+
+import cc.unitmesh.devti.flow.kanban.Kanban
+import cc.unitmesh.devti.flow.model.SimpleProjectInfo
+import cc.unitmesh.devti.flow.model.SimpleStory
+import org.gitlab4j.api.GitLabApi
+import org.gitlab4j.api.models.Issue
+
+class GitLabIssue(private val apiUrl: String, private val personalAccessToken: String, gitlabUrl: String) : Kanban {
+    private lateinit var gitLabApi: GitLabApi
+
+    // Initialize the GitLabApi instance
+    init {
+        initializeGitLabApi(gitlabUrl, personalAccessToken)
+    }
+
+    private fun initializeGitLabApi(url: String, userToken: String) {
+        gitLabApi = GitLabApi(url, userToken)
+    }
+
+    override fun getProjectInfo(): SimpleProjectInfo {
+        val project = gitLabApi.projectApi.getProject(apiUrl)
+        return SimpleProjectInfo(project.nameWithNamespace, project.name, project.description ?: "")
+    }
+
+    override fun getStories(): List<SimpleStory> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getStoryById(storyId: String): SimpleStory {
+        val issue: Issue = gitLabApi.issuesApi.getIssue(apiUrl, storyId.toInt())
+        return SimpleStory(issue.iid.toString(), issue.title, issue.description)
+    }
+
+
+    override fun updateStoryDetail(simpleStory: SimpleStory) {
+        // Create a note as a comment on the issue
+        val issue: Issue = gitLabApi.issuesApi.getIssue(apiUrl, simpleStory.id.toInt())
+        gitLabApi.notesApi.createIssueNote(apiUrl, issue.iid, simpleStory.description)
+    }
+
+    // Other methods like getStories() and isValidStory() can be implemented as needed
+}
