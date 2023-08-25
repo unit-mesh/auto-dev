cbc6bfb007f626fe4c1928e7581020c9bb37ba48
Phodal Huang <h@phodal.com>
2023-08-25 14:06:57 +0800

    docs(counit): update display texT

--- src/main/kotlin/cc/unitmesh/devti/counit/CoUnitPreProcessor.kt
+++ src/main/kotlin/cc/unitmesh/devti/counit/CoUnitPreProcessor.kt
@@ -2,6 +2,7 @@ package cc.unitmesh.devti.counit
 
 import cc.unitmesh.devti.LLMCoroutineScope
 import cc.unitmesh.devti.counit.dto.ExplainQuery
+import cc.unitmesh.devti.counit.dto.QueryResponse
 import cc.unitmesh.devti.gui.chat.ChatCodingPanel
 import cc.unitmesh.devti.gui.chat.ChatContext
 import cc.unitmesh.devti.gui.chat.ChatRole
@@ -35,17 +36,15 @@ class CoUnitPreProcessor(val project: Project) {
         val originRequest = prompter.requestPrompt()
         ui.addMessage(originRequest, true, originRequest)
 
-        // originRequest is starsWith /counit, should remove it
         val request = originRequest.removePrefix("/counit").trim()
 
         val response = coUnitPromptGenerator.findIntention(request)
             ?: throw Exception("CoUnit response is null, please check your CoUnit server address")
 
         ui.addMessage(response, true, response)
-        // add for laoding
+
+        // loading
         ui.addMessage("start to identify intention", false, "start to identify intention")
-
-
         LLMCoroutineScope.scope(project).launch {
             llmProvider.appendLocalMessage(response, ChatRole.User)
 
@@ -62,33 +61,39 @@ class CoUnitPreProcessor(val project: Project) {
                 throw Exception("parse result error: $e")
             }
 
-            val searchTip = "search by query and hyde doc"
-
+            val searchTip = "search API by query and hypothetical document"
             llmProvider.appendLocalMessage(searchTip, ChatRole.User)
             ui.addMessage(searchTip, true, searchTip)
 
             val queryResult = coUnitPromptGenerator.queryTool(explain.query, explain.hypotheticalDocument)
 
-            val sb = StringBuilder()
-            if (queryResult.first != null) {
-                sb.append("query result: \n```json\n")
-                sb.append(Json.encodeToString(queryResult.first))
-                sb.append("\n```\n")
-            }
-
-            if (queryResult.second != null) {
-                sb.append("hyde doc result: \n```json\n")
-                sb.append(Json.encodeToString(queryResult.second))
-                sb.append("\n```\n")
-            }
-
-            val related = sb.toString()
+            val related = buildDocAsContext(queryResult)
             llmProvider.appendLocalMessage(related, ChatRole.User)
 
             ApplicationManager.getApplication().invokeLater {
                 ui.addMessage(related, true, related)
             }
         }
+    }
+
+    private fun buildDocAsContext(queryResult: Pair<QueryResponse?, QueryResponse?>): String {
+        val sb = StringBuilder()
+        val normalDoc = queryResult.first
+        if (normalDoc != null && normalDoc.data.isNotEmpty()) {
+            sb.append("here is related API to origin query's result: \n```json\n")
+            sb.append(Json.encodeToString(normalDoc.data[0].displayText))
+            sb.append("\n```\n")
+        }
+
+        val hypoDoc = queryResult.second
+        if (hypoDoc != null && hypoDoc.data.isNotEmpty()) {
+            sb.append("here is hypothetical document which related to origin query's result: \n```json\n")
+            sb.append(Json.encodeToString(hypoDoc))
+            sb.append("\n```\n")
+        }
+
+        val related = sb.toString()
+        return related
     }
 
     private fun fix(result: String): String {
