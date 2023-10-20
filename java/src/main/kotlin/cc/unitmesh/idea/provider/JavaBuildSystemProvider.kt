ce31c9eddf640ae74d8ad2b993724a6de7353d0f
Phodal Huang <h@phodal.com>
2023-10-20 14:31:13 +0800

    feat(sre): init java build system provider

---
+++ java/src/main/kotlin/cc/unitmesh/idea/provider/JavaBuildSystemProvider.kt
@@ -0,0 +1,33 @@
+package cc.unitmesh.idea.provider
+
+import cc.unitmesh.devti.provider.BuildSystemProvider
+import cc.unitmesh.devti.template.DockerfileContext
+import com.intellij.openapi.externalSystem.model.ProjectSystemId
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
+import com.intellij.openapi.project.Project
+import com.intellij.util.lang.JavaVersion
+import org.jetbrains.plugins.gradle.util.GradleConstants
+
+
+class JavaBuildSystemProvider : BuildSystemProvider {
+    override fun collect(project: Project): DockerfileContext {
+        val projectDataManager = ProjectDataManager.getInstance()
+        var buildToolName = ""
+        val gradleInfo = projectDataManager.getExternalProjectsData(project, GradleConstants.SYSTEM_ID)
+        if (gradleInfo.isNotEmpty()) {
+            buildToolName = "Gradle"
+        } else {
+            buildToolName = "Maven"
+        }
+
+        val javaVersion = JavaVersion.current()
+        val javaVersionStr = "${javaVersion.feature}"
+
+        return DockerfileContext(
+            buildToolVersion = "",
+            buildToolName = buildToolName,
+            languageName = "Java",
+            languageVersion = javaVersionStr
+        )
+    }
+}
