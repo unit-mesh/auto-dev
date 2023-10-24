f7fcb86316c1d5608f1d94409f04e53152621c37
Phodal Huang <h@phodal.com>
2023-10-24 15:45:42 +0800

    feat(sre): init github actions support

---
+++ src/main/kotlin/cc/unitmesh/genius/actions/GenerateDockerfileAction.kt
@@ -0,0 +1,37 @@
+package cc.unitmesh.genius.actions
+
+import cc.unitmesh.devti.AutoDevBundle
+import cc.unitmesh.devti.custom.tasks.FileGenerateTask
+import cc.unitmesh.devti.provider.BuildSystemProvider
+import cc.unitmesh.devti.template.TemplateRender
+import cc.unitmesh.genius.actions.context.DevOpsContext
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
+
+
+const val DOCKERFILE = "Dockerfile"
+
+class GenerateDockerfileAction : AnAction(AutoDevBundle.message("action.new.genius.dockerfile")) {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+
+        // first, we need to guess language
+        val dockerContexts = BuildSystemProvider.guess(project);
+        val templateRender = TemplateRender("genius/sre")
+        templateRender.context = DevOpsContext.from(dockerContexts)
+
+        val template = templateRender
+            .getTemplate("create-dockerfile.vm")
+
+        val msgs = templateRender.create(template)
+
+        val task: Task.Backgroundable = FileGenerateTask(project, msgs, DOCKERFILE)
+        ProgressManager.getInstance()
+            .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
+    }
+}
+
+
--- src/main/kotlin/cc/unitmesh/genius/actions/GenerateGitHubActionsAction.kt
+++ src/main/kotlin/cc/unitmesh/genius/actions/GenerateGitHubActionsAction.kt
@@ -3,8 +3,8 @@ package cc.unitmesh.genius.actions
 import cc.unitmesh.devti.AutoDevBundle
 import cc.unitmesh.devti.custom.tasks.FileGenerateTask
 import cc.unitmesh.devti.provider.BuildSystemProvider
-import cc.unitmesh.devti.template.DockerfileContext
 import cc.unitmesh.devti.template.TemplateRender
+import cc.unitmesh.genius.actions.context.DevOpsContext
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.ProgressManager
@@ -12,16 +12,14 @@ import com.intellij.openapi.progress.Task
 import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 
 
-private const val DOCKERFILE = "Dockerfile"
-
-class CreateGeniusDockerfileAction : AnAction(AutoDevBundle.message("action.new.genius.dockerfile")) {
+class GenerateGitHubActionsAction : AnAction(AutoDevBundle.message("action.new.genius.cicd.github")) {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
 
         // first, we need to guess language
         val dockerContexts = BuildSystemProvider.guess(project);
-        val templateRender = TemplateRender("genius/sre")
-        templateRender.context = DockerContext.from(dockerContexts)
+        val templateRender = TemplateRender("genius/cicd")
+        templateRender.context = DevOpsContext.from(dockerContexts)
 
         val template = templateRender
             .getTemplate("create-dockerfile.vm")
@@ -31,22 +29,6 @@ class CreateGeniusDockerfileAction : AnAction(AutoDevBundle.message("action.new.
         val task: Task.Backgroundable = FileGenerateTask(project, msgs, DOCKERFILE)
         ProgressManager.getInstance()
             .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
-    }
-}
-
-data class DockerContext(
-    val buildContext: String,
-) {
-    companion object {
-        fun from(dockerContexts: List<DockerfileContext>): DockerContext {
-            val string = dockerContexts.joinToString("\n") {
-                val build = "- Build tool name: ${it.buildToolName}, Build tool version: ${it.buildToolVersion}\n"
-                val language = "- Language name: ${it.languageName}, Language version: ${it.languageVersion}\n"
-                val task = "- Build tool Task list: ${it.taskString}\n"
-                build + language + task
-            }
-            return DockerContext(string)
-        }
     }
 }
 
