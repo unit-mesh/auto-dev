36c18dbc31f7e5026a18f867fcae6dd81bf1d2e3
Phodal Huang <h@phodal.com>
2023-10-26 11:02:00 +0800

    chore: init quick insight action

---
+++ src/main/kotlin/cc/unitmesh/devti/actions/quick/QuickInsightAction.kt
@@ -0,0 +1,14 @@
+package cc.unitmesh.devti.actions.quick
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * A quick insight action is an action that can be triggered by a user,
+ * user can input custom text to call with LLM.
+ */
+class QuickInsightAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
