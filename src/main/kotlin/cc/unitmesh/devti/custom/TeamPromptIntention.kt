68c360f65c853e34aaab228578e421b163448e3b
Phodal Huang <h@phodal.com>
2023-10-28 14:13:54 +0800

    refactor: extract task

--- src/main/kotlin/cc/unitmesh/devti/custom/TeamPromptIntention.kt
+++ src/main/kotlin/cc/unitmesh/devti/custom/TeamPromptIntention.kt
@@ -1,25 +1,15 @@
 package cc.unitmesh.devti.custom
 
-import cc.unitmesh.cf.core.llms.LlmMsg
-import cc.unitmesh.devti.AutoDevBundle
-import cc.unitmesh.devti.custom.tasks.FileGenerateTask
-import cc.unitmesh.devti.custom.team.InteractionType
 import cc.unitmesh.devti.custom.team.TeamPromptAction
+import cc.unitmesh.devti.custom.team.TeamPromptExecTask
 import cc.unitmesh.devti.custom.team.TeamPromptTemplateCompiler
 import cc.unitmesh.devti.gui.chat.ChatActionType
-import cc.unitmesh.devti.gui.sendToChatPanel
 import cc.unitmesh.devti.intentions.action.base.AbstractChatIntention
-import cc.unitmesh.devti.intentions.action.task.BaseCompletionTask
-import cc.unitmesh.devti.intentions.action.task.CodeCompletionRequest
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.guessProjectDir
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.temporary.calculateFrontendElementToExplain
 
@@ -71,58 +61,4 @@ class TeamPromptIntention(private val intentionConfig: TeamPromptAction) : Abstr
         ProgressManager.getInstance()
             .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
     }
-}
-
-class TeamPromptExecTask(
-    @JvmField val project: Project,
-    val msgs: List<LlmMsg.ChatMessage>,
-    val editor: Editor,
-    val intentionConfig: TeamPromptAction,
-    val element: PsiElement?,
-) :
-    Task.Backgroundable(project, AutoDevBundle.message("intentions.request.background.process.title")) {
-    override fun run(indicator: ProgressIndicator) {
-        val offset = runReadAction { editor.caretModel.offset }
-
-        val userPrompt = msgs.filter { it.role == LlmMsg.ChatRole.User }.joinToString("\n") { it.content }
-        val systemPrompt = msgs.filter { it.role == LlmMsg.ChatRole.System }.joinToString("\n") { it.content }
-
-        when (intentionConfig.actionPrompt.interaction) {
-            InteractionType.ChatPanel -> {
-                sendToChatPanel(project) { panel, service ->
-                    service.handleMsgsAndResponse(panel, msgs)
-                }
-            }
-
-            InteractionType.AppendCursor,
-            InteractionType.AppendCursorStream,
-            -> {
-                val msgString = systemPrompt + "\n" + userPrompt
-                val request = runReadAction {
-                    CodeCompletionRequest.create(editor, offset, element!!, null, msgString)
-                } ?: return
-
-                val task = object : BaseCompletionTask(request) {
-                    override fun keepHistory(): Boolean = false
-                    override fun promptText(): String = msgString
-                }
-
-                ProgressManager.getInstance()
-                    .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
-            }
-
-            InteractionType.OutputFile -> {
-                val fileName = intentionConfig.actionPrompt.other.getOrDefault("fileName", "output.txt") as String
-                val filename = project.guessProjectDir()!!.toNioPath().resolve(fileName).toFile()
-                if (!filename.exists()) {
-                    filename.createNewFile()
-                }
-
-                val task = FileGenerateTask(project, msgs, filename)
-                ProgressManager.getInstance()
-                    .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
-            }
-        }
-    }
-
 }
--- src/main/kotlin/cc/unitmesh/devti/custom/variable/VariableResolver.kt
+++ src/main/kotlin/cc/unitmesh/devti/custom/variable/VariableResolver.kt
@@ -3,6 +3,5 @@ package cc.unitmesh.devti.custom.variable
 interface VariableResolver {
     val type: CustomIntentionVariableType
     fun resolve(): String
-
     fun variableName() = type.name
 }
