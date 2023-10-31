cc6823029b32acad7c08afb56add33541d1a60f0
Phodal Huang <h@phodal.com>
2023-10-31 09:20:53 +0800

    chore: update chocolate factory version to 0.4.2 for support Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -360,8 +360,8 @@ project(":") {
         implementation("org.jetbrains:markdown:0.5.1")
         implementation(libs.kotlinx.serialization.json)
 
-        implementation("cc.unitmesh:cocoa-core:0.4.1")
-        implementation("cc.unitmesh:git-commit-message:0.4.1")
+        implementation("cc.unitmesh:cocoa-core:0.4.2")
+        implementation("cc.unitmesh:git-commit-message:0.4.2")
 
         // kanban
         implementation(libs.github.api)
