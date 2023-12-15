821e7b0b998d0c14f54f6964dc2f0a8118802156
Phodal Huang <h@phodal.com>
2023-12-15 09:40:43 +0800

    chore: remove 213 code

--- src/213/main/kotlin/com/intellij/temporary/gui/block/Util.kt
+++
@@ -1,9 +0,0 @@
-package com.intellij.temporary.gui.block
-
-import com.intellij.openapi.observable.properties.GraphProperty
-import com.intellij.openapi.observable.properties.GraphPropertyImpl
-import com.intellij.openapi.observable.properties.PropertyGraph
-
-fun <T> PropertyGraph.property(initial: T): GraphProperty<T> {
-    return GraphPropertyImpl(this) { initial }
-}
