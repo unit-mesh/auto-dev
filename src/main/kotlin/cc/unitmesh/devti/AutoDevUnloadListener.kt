11f7c287cba32b4d43157a5c84b23e22f9731789
Phodal Huang <h@phodal.com>
2023-12-18 21:38:51 +0800

    feat: try to add unload listener

---
+++ src/main/kotlin/cc/unitmesh/devti/AutoDevUnloadListener.kt
@@ -0,0 +1,10 @@
+package cc.unitmesh.devti
+
+import com.intellij.ide.plugins.DynamicPluginListener
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+
+class AutoDevUnloadListener: DynamicPluginListener {
+    override fun beforePluginUnload(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
+
+    }
+}
