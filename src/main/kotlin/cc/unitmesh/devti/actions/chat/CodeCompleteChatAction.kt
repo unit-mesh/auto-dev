efe9462ea24d58d0df83fc8ff49f511837f776f4
Phodal Huang <h@phodal.com>
2023-12-20 14:44:19 +0800

    refactor: split chat code completion action

--- src/main/kotlin/cc/unitmesh/devti/actions/chat/CodeCompleteChatAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/actions/chat/CodeCompleteChatAction.kt
@@ -1,31 +1,46 @@
 package cc.unitmesh.devti.actions.chat
 
-import cc.unitmesh.devti.actions.chat.base.ChatBaseAction
+import cc.unitmesh.devti.AutoDevBundle
 import cc.unitmesh.devti.gui.chat.ChatActionType
-import com.intellij.openapi.actionSystem.ActionUpdateThread
+import cc.unitmesh.devti.gui.chat.ChatContext
+import cc.unitmesh.devti.gui.sendToChatPanel
+import cc.unitmesh.devti.provider.ContextPrompter
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.application.runReadAction
+import com.intellij.temporary.getElementToAction
 
-class CodeCompleteChatAction : ChatBaseAction() {
-    override fun getActionType(): ChatActionType = ChatActionType.CODE_COMPLETE
+class CodeCompleteChatAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val document = e.getData(CommonDataKeys.EDITOR)?.document
+        val caretModel = e.getData(CommonDataKeys.EDITOR)?.caretModel
 
-    override fun getReplaceableAction(event: AnActionEvent): (response: String) -> Unit {
-        val editor = event.getRequiredData(CommonDataKeys.EDITOR)
-        val project = event.getRequiredData(CommonDataKeys.PROJECT)
-        val document = editor.document
+        var prefixText = caretModel?.currentCaret?.selectedText ?: ""
 
-        val primaryCaret = editor.caretModel.primaryCaret;
-        val start = primaryCaret.selectionStart
-        val end = primaryCaret.selectionEnd
+        val file = e.getData(CommonDataKeys.PSI_FILE)
 
-        return { response ->
-            WriteCommandAction.runWriteCommandAction(project) {
-                document.insertString(start, response)
-                primaryCaret.removeSelection()
-                primaryCaret.moveToOffset(end + response.length)
-            }
+        val lineEndOffset = document?.getLineEndOffset(document.getLineNumber(caretModel?.offset ?: 0)) ?: 0
+        if (prefixText.isEmpty()) {
+            prefixText = document?.text?.substring(0, lineEndOffset) ?: ""
+        }
+        val suffixText = document?.text?.substring(lineEndOffset) ?: ""
+
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+
+        val prompter = ContextPrompter.prompter(file?.language?.displayName ?: "")
+
+        val element = runReadAction { getElementToAction(project, editor) }
+        prompter.initContext(ChatActionType.CODE_COMPLETE, prefixText, file, project, caretModel?.offset ?: 0, element)
+
+        sendToChatPanel(project) { panel, service ->
+            val chatContext = ChatContext(
+                null,
+                prefixText,
+                suffixText
+            )
+            service.handlePromptAndResponse(panel, prompter, chatContext)
         }
     }
 }
-
