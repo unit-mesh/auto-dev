b0f9fcb5ebdc5a4df1089916b5f2315ce04aecda
Phodal Huang <h@phodal.com>
2024-01-10 15:46:10 +0800

    feat(context): update ClassContext to include package information
    Update the ClassContext file to include the package information in the generated string representation. This will provide more context when debugging and analyzing the code.

--- src/main/kotlin/cc/unitmesh/devti/context/ClassContext.kt
+++ src/main/kotlin/cc/unitmesh/devti/context/ClassContext.kt
@@ -38,7 +38,7 @@ class ClassContext(
         val filePath = displayName ?: runReadAction { root.containingFile?.virtualFile?.path }
 
         return """
-        |'path: $filePath
+        |'package: $filePath
         |class $className$superClasses {
         |  $classFields
         |  $methodSignatures
