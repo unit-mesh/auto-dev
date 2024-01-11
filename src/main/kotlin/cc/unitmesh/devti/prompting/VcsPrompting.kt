ddf4ad02f5ef8ba70b94fcca2feee598b2f49b26
Phodal Huang <h@phodal.com>
2024-01-11 20:16:32 +0800

    refactor(prompting): move diff simplification logic to DiffSimplifier
    This commit moves the logic for simplifying the diff in the VcsPrompting class to a new DiffSimplifier class. The DiffSimplifier class takes a list of changes and a list of ignore file patterns, and simplifies the diff by filtering out binary or too large changes and removing unnecessary information from the diff output. This refactoring improves code organization and separation of concerns.

--- src/main/kotlin/cc/unitmesh/devti/prompting/VcsPrompting.kt
+++ src/main/kotlin/cc/unitmesh/devti/prompting/VcsPrompting.kt
@@ -22,75 +22,19 @@
 
 package cc.unitmesh.devti.prompting
 
+import cc.unitmesh.devti.prompting.diff.DiffSimplifier
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.diff.impl.patch.IdeaTextPatchBuilder
-import com.intellij.openapi.diff.impl.patch.UnifiedDiffWriter
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.changes.*
-import com.intellij.project.stateStore
 import com.intellij.vcs.log.VcsFullCommitDetails
-import git4idea.repo.GitRepositoryManager
-import org.jetbrains.annotations.NotNull
 import java.io.IOException
 import java.io.StringWriter
-import java.nio.file.Path
 import java.nio.file.PathMatcher
-import java.util.stream.Collectors
-import kotlin.math.min
 
 @Service(Service.Level.PROJECT)
 class VcsPrompting(private val project: Project) {
-    private val gitRepositoryManager = GitRepositoryManager.getInstance(project)
-
-    fun calculateDiff(changes: List<Change>, project: Project, ignoreFilePatterns: List<PathMatcher>): String {
-        try {
-            val writer = StringWriter()
-            val basePath = project.basePath ?: throw RuntimeException("Project base path is null.")
-
-            val filteredChanges = changes.stream()
-                .filter { change -> !isBinaryOrTooLarge(change!!) }
-                .filter {
-                    val filePath = it.afterRevision?.file
-                    if (filePath != null) {
-                        ignoreFilePatterns.none { pattern ->
-                            pattern.matches(Path.of(it.afterRevision!!.file.path))
-                        }
-                    } else {
-                        true
-                    }
-                }
-                .toList()
-
-            if (filteredChanges.isEmpty()) {
-                return ""
-            }
-
-            val patches = IdeaTextPatchBuilder.buildPatch(
-                project,
-                filteredChanges.subList(0, min(filteredChanges.size, 500)),
-                Path.of(basePath),
-                false,
-                true
-            )
-
-
-            UnifiedDiffWriter.write(
-                project,
-                project.stateStore.projectBasePath,
-                patches,
-                writer,
-                "\n",
-                null as CommitContext?,
-                emptyList()
-            )
-            val diffString = writer.toString()
-            return trimDiff(diffString)
-        } catch (e: VcsException) {
-            throw RuntimeException("Error calculating diff: ${e.message}", e)
-        }
-    }
 
     fun prepareContext(): String {
         val changeListManager = ChangeListManagerImpl.getInstance(project)
@@ -98,7 +42,8 @@ class VcsPrompting(private val project: Project) {
             it.changes
         }
 
-        return this.calculateDiff(changes, project, listOf())
+        val context = project.service<DiffSimplifier>().simplify(changes, listOf())
+        return context
     }
 
     /**
@@ -126,57 +71,21 @@ class VcsPrompting(private val project: Project) {
         }
 
         writer.write("Changes:\n\n")
-        val changeText = calculateDiff(selectList, project, ignoreFilePatterns)
+        val changeText = project.service<DiffSimplifier>().simplify(selectList, ignoreFilePatterns)
 
         if (changeText.isEmpty()) {
             return null
         }
 
+
+
         writer.write("```patch\n\n")
-        writer.write(trimDiff(changeText))
+        writer.write(DiffSimplifier.postProcess(changeText))
         writer.write("\n\n```\n\n")
 
         return writer.toString()
     }
 
-    private val revisionRegex = Regex("\\(revision [^)]+\\)")
-    private val lineTip = "\\ No newline at end of file"
-
-    @NotNull
-    fun trimDiff(@NotNull diffString: String): String {
-        val lines = diffString.lines()
-        val destination = ArrayList<String>()
-        lines.forEach { line ->
-            if (line.startsWith("diff --git ") || line.startsWith("index ") || line.startsWith("Index ")) return@forEach
-
-            if (line == "===================================================================") return@forEach
-
-            // if a patch includes `\ No newline at the end of file` remove it
-            if (line.contains(lineTip)) {
-                return@forEach
-            }
-
-            if (line.startsWith("---") || line.startsWith("+++")) {
-                // remove revision number with regex
-                val result = revisionRegex.replace(line, "")
-                destination.add(result)
-            } else {
-                destination.add(line)
-            }
-        }
-        return destination.joinToString("\n")
-    }
-
-    private fun isBinaryOrTooLarge(@NotNull change: Change): Boolean {
-        return isBinaryOrTooLarge(change.beforeRevision) || isBinaryOrTooLarge(change.afterRevision)
-    }
-
-    private fun isBinaryOrTooLarge(revision: ContentRevision?): Boolean {
-        val virtualFile = (revision as? CurrentContentRevision)?.virtualFile
-        return revision != null && (isBinaryRevision(revision) || (virtualFile != null && FileUtilRt.isTooLarge(
-            virtualFile.length
-        )))
-    }
 
     fun hasChanges(): List<Change> {
         val changeListManager = ChangeListManagerImpl.getInstance(project)
@@ -186,9 +95,4 @@ class VcsPrompting(private val project: Project) {
 
         return changes
     }
-}
-
-fun isBinaryRevision(cr: ContentRevision?): Boolean {
-    if (cr == null) return false
-    return if (cr is BinaryContentRevision) true else cr.file.fileType.isBinary
 }
---
+++ src/main/kotlin/cc/unitmesh/devti/prompting/diff/DiffSimplifier.kt
@@ -0,0 +1,113 @@
+package cc.unitmesh.devti.prompting.diff
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.diff.impl.patch.IdeaTextPatchBuilder
+import com.intellij.openapi.diff.impl.patch.UnifiedDiffWriter
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.changes.*
+import com.intellij.project.stateStore
+import org.jetbrains.annotations.NotNull
+import java.io.StringWriter
+import java.nio.file.Path
+import java.nio.file.PathMatcher
+import kotlin.math.min
+
+@Service(Service.Level.PROJECT)
+class DiffSimplifier(val project: Project) {
+    fun simplify(changes: List<Change>, ignoreFilePatterns: List<PathMatcher>): String {
+        try {
+            val writer = StringWriter()
+            val basePath = project.basePath ?: throw RuntimeException("Project base path is null.")
+
+            val filteredChanges = changes.stream()
+                .filter { change -> !isBinaryOrTooLarge(change!!) }
+                .filter {
+                    val filePath = it.afterRevision?.file
+                    if (filePath != null) {
+                        ignoreFilePatterns.none { pattern ->
+                            pattern.matches(Path.of(it.afterRevision!!.file.path))
+                        }
+                    } else {
+                        true
+                    }
+                }
+                .toList()
+
+            if (filteredChanges.isEmpty()) {
+                return ""
+            }
+
+            val patches = IdeaTextPatchBuilder.buildPatch(
+                project,
+                filteredChanges.subList(0, min(filteredChanges.size, 500)),
+                Path.of(basePath),
+                false,
+                true
+            )
+
+
+            UnifiedDiffWriter.write(
+                project,
+                project.stateStore.projectBasePath,
+                patches,
+                writer,
+                "\n",
+                null as CommitContext?,
+                emptyList()
+            )
+            val diffString = writer.toString()
+            return postProcess(diffString)
+        } catch (e: VcsException) {
+            throw RuntimeException("Error calculating diff: ${e.message}", e)
+        }
+    }
+
+    companion object {
+        private val revisionRegex = Regex("\\(revision [^)]+\\)")
+        private val lineTip = "\\ No newline at end of file"
+
+        @NotNull
+        fun postProcess(@NotNull diffString: String): String {
+            val lines = diffString.lines()
+            val destination = ArrayList<String>()
+            lines.forEach { line ->
+                if (line.startsWith("diff --git ") || line.startsWith("index ") || line.startsWith("Index ")) return@forEach
+
+                if (line == "===================================================================") return@forEach
+
+                // if a patch includes `\ No newline at the end of file` remove it
+                if (line.contains(lineTip)) {
+                    return@forEach
+                }
+
+                if (line.startsWith("---") || line.startsWith("+++")) {
+                    // remove revision number with regex
+                    val result = revisionRegex.replace(line, "")
+                    destination.add(result)
+                } else {
+                    destination.add(line)
+                }
+            }
+
+            return destination.joinToString("\n")
+        }
+
+        private fun isBinaryOrTooLarge(@NotNull change: Change): Boolean {
+            return isBinaryOrTooLarge(change.beforeRevision) || isBinaryOrTooLarge(change.afterRevision)
+        }
+
+        private fun isBinaryOrTooLarge(revision: ContentRevision?): Boolean {
+            val virtualFile = (revision as? CurrentContentRevision)?.virtualFile
+            return revision != null && (isBinaryRevision(revision) || (virtualFile != null && FileUtilRt.isTooLarge(
+                virtualFile.length
+            )))
+        }
+
+        private fun isBinaryRevision(cr: ContentRevision?): Boolean {
+            if (cr == null) return false
+            return if (cr is BinaryContentRevision) true else cr.file.fileType.isBinary
+        }
+    }
+}
