f4ffd665d228d6e29ac488712eb9ddd1f024eb29
Phodal Huang <h@phodal.com>
2024-01-12 16:38:22 +0800

    refactor(test): improve current class information prompt
    The current class information prompt in the TestCodeGenTask.kt file has been improved. The format of the current class information is now joined with "//" at the beginning of each line. This change provides better readability and clarity for the prompt.

--- src/main/kotlin/cc/unitmesh/devti/custom/test/TemplatedTestPrompt.kt
+++ src/main/kotlin/cc/unitmesh/devti/custom/test/TemplatedTestPrompt.kt
@@ -9,12 +9,36 @@ import com.intellij.openapi.project.guessProjectDir
 class TemplatedTestPrompt(private val project: Project) {
     val settings = project.teamPromptsSettings
 
-    fun lookup(fileName: String): String? {
+    /**
+     * Looks up a template file by its name and returns its content as a string.
+     *
+     * @param templateFileName the name of the template file to look up
+     * @return the content of the template file as a string, or null if the file is not found
+     *
+     * 1. Find the prompts directory
+     */
+    fun lookup(templateFileName: String): String? {
         val path = settings.state.teamPromptsDir
         val promptsDir = project.guessProjectDir()?.findChild(path) ?: return null
-        val templateFile = promptsDir.findChild(fileName) ?: return null
+        val templateFile = promptsDir.findChild("templates") ?: return null
 
-        val templateCode = templateFile.children.firstOrNull { it.name.endsWith(".vm") } ?: return null
-        return templateCode.inputStream.readBytes().toString(Charsets.UTF_8)
+        val templateCode = templateFile.children.firstOrNull { it.name == templateFileName }
+        if (templateCode != null) {
+            return templateCode.inputStream.readBytes().toString(Charsets.UTF_8)
+        }
+
+        // second lookup same name with .vm
+        val vmTemplate = templateFile.children.firstOrNull { it.name == "$templateFileName.vm" }
+        if (vmTemplate != null) {
+            return vmTemplate.inputStream.readBytes().toString(Charsets.UTF_8)
+        }
+
+        // final search end with filename
+        val endSuffix = templateFile.children.firstOrNull { it.name.endsWith(templateFileName) }
+        if (endSuffix != null) {
+            return endSuffix.inputStream.readBytes().toString(Charsets.UTF_8)
+        }
+
+        return null
     }
 }
