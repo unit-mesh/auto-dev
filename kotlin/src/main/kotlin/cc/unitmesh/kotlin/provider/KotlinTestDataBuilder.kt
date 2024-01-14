1e3c5654ab5cd573d9e5021ce316af7390eefa8d
Phodal Huang <h@phodal.com>
2024-01-14 09:34:40 +0800

    refactor(kotlin): modify KotlinTestDataBuilder to return empty string instead of "null"
    The KotlinTestDataBuilder class was modified to return an empty string instead of the string "null" when the value is null. This change improves the consistency and readability of the code.

--- kotlin/src/main/kotlin/cc/unitmesh/kotlin/provider/KotlinTestDataBuilder.kt
+++ kotlin/src/main/kotlin/cc/unitmesh/kotlin/provider/KotlinTestDataBuilder.kt
@@ -30,7 +30,7 @@ class KotlinTestDataBuilder : TestDataBuilder {
                     }
 
                     else -> {
-                        "null"
+                        ""
                     }
                 }
             }
