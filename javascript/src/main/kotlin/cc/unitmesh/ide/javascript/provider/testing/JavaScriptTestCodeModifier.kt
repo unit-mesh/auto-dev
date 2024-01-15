030c50ec79c810c097ceb546d188efac4d74f3cf
Phodal Huang <h@phodal.com>
2024-01-15 21:23:40 +0800

    refactor(javascript): modify JavaScriptTestCodeModifier and JestCodeModifier
    - Refactored the `JavaScriptTestCodeModifier` class to improve code readability and maintainability.
    - Removed unnecessary imports and unused methods.
    - Extracted common functionality to a separate utility class `LanguageApplicableUtil`.
    - Updated the `isApplicable` method to use the utility class.
    - Modified the `insertMethod` method to add the new code element after the last child element.
    - Refactored the `JestCodeModifier` class to remove duplicate code and unnecessary methods.

--- javascript/src/main/kotlin/cc/unitmesh/ide/javascript/provider/testing/JavaScriptTestCodeModifier.kt
+++ javascript/src/main/kotlin/cc/unitmesh/ide/javascript/provider/testing/JavaScriptTestCodeModifier.kt
@@ -1,7 +1,50 @@
 package cc.unitmesh.ide.javascript.provider.testing
 
 import cc.unitmesh.devti.context.builder.CodeModifier
+import cc.unitmesh.ide.javascript.util.LanguageApplicableUtil
+import com.intellij.lang.Language
+import com.intellij.lang.javascript.psi.JSFile
+import com.intellij.lang.javascript.psi.impl.JSPsiElementFactory
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiManager
 
-abstract class JavaScriptTestCodeModifier(val framework: String): CodeModifier {
+open class JavaScriptTestCodeModifier : CodeModifier {
+    override fun isApplicable(language: Language): Boolean {
+        return LanguageApplicableUtil.isJavaScriptApplicable(language)
+    }
+
+    override fun insertTestCode(sourceFile: VirtualFile, project: Project, code: String): Boolean {
+        insertMethod(sourceFile, project, code)
+        return true
+    }
+
+    override fun insertMethod(sourceFile: VirtualFile, project: Project, code: String): Boolean {
+        val file = sourceFile as JSFile
+        val psiElement = file.lastChild
+
+        val language = sourceFile.language
+        val element = PsiFileFactory.getInstance(project).createFileFromText(language, "")
+        val codeElement = JSPsiElementFactory.createJSStatement(code, element)
+
+        runReadAction {
+            psiElement?.parent?.addAfter(codeElement, psiElement)
+        }
+
+        return true
+    }
+
+    override fun insertClass(sourceFile: VirtualFile, project: Project, code: String): Boolean {
+        WriteCommandAction.runWriteCommandAction(project) {
+            val psiFile = PsiManager.getInstance(project).findFile(sourceFile) as JSFile
+            val document = psiFile.viewProvider.document!!
+            document.insertString(document.textLength, code)
+        }
+
+        return true
+    }
 
 }
--- javascript/src/main/kotlin/cc/unitmesh/ide/javascript/provider/testing/JestCodeModifier.kt
+++ javascript/src/main/kotlin/cc/unitmesh/ide/javascript/provider/testing/JestCodeModifier.kt
@@ -8,32 +8,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 
-class JestCodeModifier : JavaScriptTestCodeModifier("jest") {
+class JestCodeModifier : JavaScriptTestCodeModifier() {
     override fun isApplicable(language: Language): Boolean {
         return LanguageApplicableUtil.isJavaScriptApplicable(language)
     }
-
-    override fun insertTestCode(sourceFile: VirtualFile, project: Project, code: String): Boolean {
-        if (code.startsWith("import") && code.contains("test")) {
-            return insertClass(sourceFile, project, code)
-        }
-
-        insertClass(sourceFile, project, code)
-        return true
-    }
-
-    override fun insertMethod(sourceFile: VirtualFile, project: Project, code: String): Boolean {
-        return true
-    }
-
-    override fun insertClass(sourceFile: VirtualFile, project: Project, code: String): Boolean {
-        WriteCommandAction.runWriteCommandAction(project) {
-            val psiFile = PsiManager.getInstance(project).findFile(sourceFile) as JSFile
-            val document = psiFile.viewProvider.document!!
-            document.insertString(document.textLength, code)
-        }
-
-        return true
-    }
-
 }
