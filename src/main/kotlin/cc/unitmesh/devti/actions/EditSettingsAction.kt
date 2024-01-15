6ad002974e77f0020b5e4309aff559b370b3a589
Phodal Huang <h@phodal.com>
2024-01-15 08:26:42 +0800

    refactor(actions): rename AutoDevOpenSettingsAction to EditSettingsAction
    Rename the file `AutoDevOpenSettingsAction.kt` to `EditSettingsAction.kt` in the `cc.unitmesh.devti.actions` package. Also modify the `META-INF/autodev-core.xml` file to update the action ID, class, text, and description for the `Open Settings` action to `Edit Settings`.

--- src/main/kotlin/cc/unitmesh/devti/actions/EditSettingsAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/actions/EditSettingsAction.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.ShowSettingsUtil
 
-class AutoDevOpenSettingsAction : AnAction() {
+class EditSettingsAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
         ShowSettingsUtil.getInstance().showSettingsDialog(project, AutoDevSettingsConfigurable::class.java)
