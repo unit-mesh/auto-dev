09718139bd162e8e9414264d0ca0a541c1bb5002
Phodal Huang <h@phodal.com>
2024-01-15 11:07:12 +0800

    feat(webstorm): add support for web chat creation context
    This commit adds support for web chat creation context in the LanguageApplicableUtil class. It includes a new function `isWebChatCreationContextSupported` that checks if the given `ChatCreationContext` is supported in WebStorm. If the context is not supported in WebStorm, it falls back to checking if the source file is a web-compatible file or a package.json file.

--- webstorm/src/main/kotlin/cc/unitmesh/ide/webstorm/LanguageApplicableUtil.kt
+++ webstorm/src/main/kotlin/cc/unitmesh/ide/webstorm/LanguageApplicableUtil.kt
@@ -1,14 +1,40 @@
 package cc.unitmesh.ide.webstorm
 
+import cc.unitmesh.devti.provider.context.ChatCreationContext
 import com.intellij.json.JsonLanguage
 import com.intellij.lang.Language
 import com.intellij.lang.html.HTMLLanguage
+import com.intellij.lang.html.HtmlCompatibleFile
 import com.intellij.lang.javascript.JavaScriptSupportLoader
 import com.intellij.lang.javascript.JavascriptLanguage
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
 import com.intellij.lang.javascript.dialects.TypeScriptJSXLanguageDialect
+import com.intellij.psi.PsiFile
+import com.intellij.util.PlatformUtils
+import com.intellij.util.concurrency.annotations.RequiresReadLock
+import org.jetbrains.annotations.ApiStatus
 
 object LanguageApplicableUtil {
     private val supportedLanguages = setOf(JavascriptLanguage.INSTANCE.id, JavaScriptSupportLoader.TYPESCRIPT.id)
     fun isJavaScriptApplicable(language: Language) =
         supportedLanguages.contains(language.id) || language is HTMLLanguage || language is JsonLanguage || language is TypeScriptJSXLanguageDialect
+
+    @RequiresReadLock
+    @ApiStatus.Internal
+    fun isWebChatCreationContextSupported(creationContext: ChatCreationContext): Boolean {
+        if (PlatformUtils.isWebStorm()) {
+            return true
+        }
+        return isWebLLMContext(creationContext.sourceFile)
+    }
+
+    @ApiStatus.Internal
+    private fun isWebLLMContext(psiFile: PsiFile?): Boolean {
+        if (psiFile == null) return false
+        if ((psiFile is HtmlCompatibleFile) || PackageJsonUtil.isPackageJsonFile(psiFile)) {
+            return true
+        }
+
+        return psiFile.language.isKindOf(JavascriptLanguage.INSTANCE) || psiFile.language.isKindOf(HTMLLanguage.INSTANCE)
+    }
 }
