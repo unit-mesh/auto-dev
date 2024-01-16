7f1c1c3f8bc298481e9f9fe72d3ed3eda87d14dc
Phodal Huang <h@phodal.com>
2024-01-16 23:17:36 +0800

    chore(build): remove unnecessary experiment
    This commit removes unnecessary code in the build.gradle.kts file. The removed code was related to the "experiment" project, which is no longer needed. This cleanup improves the readability and maintainability of the build script.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "AutoDev-Intellij"
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-include("experiment")
 include("plugin")
 
 include(
