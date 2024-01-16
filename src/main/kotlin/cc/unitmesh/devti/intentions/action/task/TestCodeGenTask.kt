50c277070f3b52f859a1d09e634b7740a7419623
Phodal Huang <h@phodal.com>
2024-01-16 13:14:52 +0800

    chore: update IntelliJ plugin version
    Update the IntelliJ plugin version in gradle.properties and gradle-222.properties files. The plugin version is changed from 221.* to 222.* to align with the IntelliJ version IU-2022.2.4.

--- src/main/kotlin/cc/unitmesh/devti/intentions/action/task/TestCodeGenTask.kt
+++ src/main/kotlin/cc/unitmesh/devti/intentions/action/task/TestCodeGenTask.kt
@@ -24,6 +24,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 
 data class TestGenPromptContext(
