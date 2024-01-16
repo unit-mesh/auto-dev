a5d96cfb7a6c44b34331fea95c9d64484e70de02
Phodal Huang <h@phodal.com>
2024-01-16 23:07:19 +0800

    refactor(rust): rename RustContextProvider to RustVersionContextProvider
    This commit renames the file `RustContextProvider.kt` to `RustVersionContextProvider.kt` in the `rust/src/main/kotlin/cc/unitmesh/rust/provider/` directory. The class `RustContextProvider` is also renamed to `RustVersionContextProvider`. The `isApplicable` method now checks if the source file language is `RsLanguage`. The `collect` method retrieves the Rust version using `CargoProjectsService` and returns a `ChatContextItem` with the Rust version information. The corresponding XML configuration file is also modified to use `RustVersionContextProvider` instead of `RustContextProvider`.

---
+++ rust/src/main/kotlin/cc/unitmesh/rust/provider/RustVersionContextProvider.kt
@@ -0,0 +1,27 @@
+package cc.unitmesh.rust.provider
+
+import cc.unitmesh.devti.provider.context.ChatContextItem
+import cc.unitmesh.devti.provider.context.ChatContextProvider
+import cc.unitmesh.devti.provider.context.ChatCreationContext
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import org.rust.cargo.project.model.CargoProjectsService
+import org.rust.lang.RsLanguage
+
+class RustVersionContextProvider : ChatContextProvider {
+    override fun isApplicable(project: Project, creationContext: ChatCreationContext): Boolean {
+        return creationContext.sourceFile?.language is RsLanguage
+    }
+
+    override suspend fun collect(project: Project, creationContext: ChatCreationContext): List<ChatContextItem> {
+        val cargoProjectsService = project.service<CargoProjectsService>()
+        val rustcInfo = cargoProjectsService.allProjects.firstOrNull()?.rustcInfo ?: return emptyList()
+
+        return listOf(
+            ChatContextItem(
+                RustVersionContextProvider::class,
+                "Rust Version: ${rustcInfo.version}"
+            )
+        )
+    }
+}
