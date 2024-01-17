961e59f8e223ba4c2fbd87bc397937b66be2308f
Phodal Huang <h@phodal.com>
2024-01-17 10:06:16 +0800

    refactor(cpp): improve CppClassContextBuilder
    - Replaced `com.jetbrains.cidr.execution.debugger.evaluation.renderers.CxxNameParser` with `com.intellij.psi.util.PsiTreeUtil` and other classes for better functionality.
    - Updated the `getClassContext` method to handle C++ class definitions more effectively.
    - Added support for gathering usages.
    - Updated the return type to `ClassContext`.
    - Added detailed comments and example code for better understanding.
    - Added a new test case in `CppClassContextBuilderTest` to verify the functionality of the updated code.
    - Modified the XML configuration file to use the updated `CppClassContextBuilder` implementation for Objective-C language.

--- cpp/src/main/kotlin/cc/unitmesh/cpp/context/CppClassContextBuilder.kt
+++ cpp/src/main/kotlin/cc/unitmesh/cpp/context/CppClassContextBuilder.kt
@@ -3,14 +3,57 @@ package cc.unitmesh.cpp.context
 import cc.unitmesh.devti.context.ClassContext
 import cc.unitmesh.devti.context.builder.ClassContextBuilder
 import com.intellij.psi.PsiElement
-import com.jetbrains.cidr.execution.debugger.evaluation.renderers.CxxNameParser
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.cidr.lang.psi.OCDeclaration
+import com.jetbrains.cidr.lang.psi.OCFunctionDeclaration
+import com.jetbrains.cidr.lang.psi.OCStructLike
 
+/**
+ * Builds a [ClassContext] for a C++ class.
+ *
+ * example:
+ * ```
+ * class Car {
+ *   public:
+ *
+ *     // class data
+ *     string brand, model;
+ *     int mileage = 0;
+ *
+ *     // class function
+ *     void drive(int distance) {
+ *         mileage += distance;
+ *     }
+ * };
+ * ```
+ *
+ * will be converted to: [ClassContext]
+ */
 class CppClassContextBuilder : ClassContextBuilder {
     override fun getClassContext(psiElement: PsiElement, gatherUsages: Boolean): ClassContext? {
-        if (psiElement !is CxxNameParser) {
+        if (psiElement !is OCStructLike) {
             return null
         }
 
-        return null
+        val definition = psiElement.symbol?.locateDefinition(psiElement.project) ?: return null
+        if (definition !is OCStructLike) return null
+
+        val definitionSymbol = definition.symbol ?: return null
+
+        val methods = PsiTreeUtil.getChildrenOfTypeAsList(definition, OCFunctionDeclaration::class.java)
+        val declarations = PsiTreeUtil.getChildrenOfTypeAsList(definition, OCDeclaration::class.java)
+        val fields = declarations.filter { it.declarators.isNotEmpty() }
+
+        val supers = definitionSymbol.getBaseCppClasses(definition).map { it.name }
+
+        return ClassContext(
+            definition,
+            definition.text,
+            definition.name,
+            methods,
+            fields,
+            supers,
+            emptyList()
+        )
     }
 }
---
+++ cpp/src/test/kotlin/cc/unitmesh/cpp/context/CppClassContextBuilderTest.kt
@@ -0,0 +1,46 @@
+package cc.unitmesh.cpp.context;
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.jetbrains.cidr.lang.psi.OCDeclaration
+import com.jetbrains.cidr.lang.psi.OCStructLike
+import com.jetbrains.cidr.lang.psi.OCTypeElement
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class CppClassContextBuilderTest : BasePlatformTestCase() {
+
+    fun testShouldGetFunctionNameOfCarsMethod() {
+        // given
+        val psiElement = myFixture.configureByText(
+            "car.cpp", """
+            #include <iostream>
+            
+            class Car {
+            public:
+                // class data
+                std::string brand, model;
+                int mileage = 0;
+            
+                // class function
+                void drive(int distance) {
+                    mileage += distance;
+                }
+            };
+            """.trimIndent()
+        )
+
+        val decl = PsiTreeUtil.getChildrenOfTypeAsList(psiElement, OCDeclaration::class.java).first()
+        val type = PsiTreeUtil.getChildrenOfTypeAsList(decl, OCTypeElement::class.java).first()
+        val clz = PsiTreeUtil.getChildrenOfTypeAsList(type, OCStructLike::class.java).first()
+
+        // when
+        val result = CppClassContextBuilder().getClassContext(clz, true)!!
+
+        // then
+        assertEquals("Car", result.name)
+        assertEquals(1, result.methods.size)
+        assertEquals(3, result.fields.size)
+    }
+}
