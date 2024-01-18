fb3caaca0d3b31163cb08688938fd462f04ef639
Phodal Huang <h@phodal.com>
2024-01-18 11:09:12 +0800

    feat(vcs): add commit message suggestion functionality
    This commit adds functionality to suggest commit messages based on the changes made. It includes the ability to retrieve the changes and unversioned files from the commit workflow UI. The commit message is then generated using the diff context and the project. Additionally, this commit introduces the ability to find example commit messages from the project's VCS log history. The examples are displayed in the commit message suggestion.

--- src/main/kotlin/cc/unitmesh/devti/actions/vcs/CommitMessageSuggestionAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/actions/vcs/CommitMessageSuggestionAction.kt
@@ -12,10 +12,19 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.CurrentContentRevision
 import com.intellij.openapi.vcs.ui.CommitMessage
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcs.commit.CommitWorkflowUi
+import com.intellij.vcs.log.TimedVcsCommit
+import com.intellij.vcs.log.VcsCommitMetadata
+import com.intellij.vcs.log.VcsLogFilterCollection
+import com.intellij.vcs.log.VcsLogProvider
+import com.intellij.vcs.log.impl.VcsProjectLog
+import com.intellij.vcs.log.visible.filters.VcsLogFilterObject
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.flow.*
 
@@ -44,17 +53,35 @@ class CommitMessageSuggestionAction : ChatBaseAction() {
         e.presentation.isEnabled = changes.isNotEmpty()
     }
 
+    private fun getChanges(e: AnActionEvent): List<Change>? {
+        val commitWorkflowUi = e.getData(VcsDataKeys.COMMIT_WORKFLOW_UI) ?: return null
+
+        val changes = commitWorkflowUi.getIncludedChanges()
+        val unversionedFiles = commitWorkflowUi.getIncludedUnversionedFiles()
+
+        val unversionedFileChanges = unversionedFiles.map {
+            Change(null, CurrentContentRevision(it))
+        }
+
+        if (changes.isNotEmpty() || unversionedFileChanges.isNotEmpty()) {
+            return changes + unversionedFileChanges
+        }
+
+        return null
+    }
+
     override fun executeAction(event: AnActionEvent) {
         val project = event.project ?: return
-        val diffContext = project.service<VcsPrompting>().prepareContext()
+        val changes = getChanges(event) ?: return
+        val diffContext = project.service<VcsPrompting>().prepareContext(changes)
+
         if (diffContext.isEmpty() || diffContext == "\n") {
             logger.warn("Diff context is empty or cannot get enough useful context.")
             AutoDevNotifications.notify(project, "Diff context is empty or cannot get enough useful context.")
             return
         }
 
-        val prompt = generateCommitMessage(diffContext)
-
+        val prompt = generateCommitMessage(diffContext, project)
         val commitMessageUi = event.getData(VcsDataKeys.COMMIT_MESSAGE_CONTROL) as CommitMessage
 
         // empty commit message before generating
@@ -79,17 +106,59 @@ class CommitMessageSuggestionAction : ChatBaseAction() {
         }
     }
 
-    private fun generateCommitMessage(diff: String): String {
+    private fun findExampleCommitMessages(project: Project): String? {
+        val logProviders = VcsProjectLog.getLogProviders(project)
+        val entry = logProviders.entries.firstOrNull() ?: return null
+
+        val logProvider = entry.value
+        val branch = logProvider.getCurrentBranch(entry.key) ?: return null
+        val user = logProvider.getCurrentUser(entry.key)
+        val fromBranch = VcsLogFilterObject.fromBranch(branch)
+
+        val filter = if (user != null) {
+            val fromUser = VcsLogFilterObject.fromUser(user, setOf())
+            VcsLogFilterObject.collection(fromUser)
+        } else {
+            VcsLogFilterObject.collection(fromBranch)
+        }
+
+        return collectExamples(logProvider, entry.key, filter)
+    }
+
+    private fun collectExamples(
+        logProvider: VcsLogProvider,
+        root: VirtualFile,
+        filter: VcsLogFilterCollection
+    ): String? {
+        val commits = logProvider.getCommitsMatchingFilter(root, filter, 3)
+
+        if (commits.isEmpty()) return null
+
+        val builder = StringBuilder("")
+        val commitIds = commits.map { (it as TimedVcsCommit).id.asString() }
+
+        val metadataCallback: (VcsCommitMetadata) -> Unit = {
+            builder.append(it.fullMessage).append("\n")
+        }
+
+        logProvider.readMetadata(root, commitIds, metadataCallback)
+
+        return builder.toString()
+    }
+
+    private fun generateCommitMessage(diff: String, project: Project): String {
         return """Write a cohesive yet descriptive commit message for a given diff. 
 Make sure to include both information What was changed and Why.
 Start with a short sentence in imperative form, no more than 50 characters long.
 Then leave an empty line and continue with a more detailed explanation, if necessary.
 Explanation should have less than 200 characters.
 
-examples:
+Examples:
 - fix(authentication): add password regex pattern
 - feat(storage): add new test cases
 - test(java): fix test case for user controller
+History Examples:
+${findExampleCommitMessages(project) ?: "No example found."}
 
 Diff:
 
