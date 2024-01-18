34e3964a08b4e2be60790396a57376ebfe56707b
Phodal Huang <h@phodal.com>
2024-01-18 08:40:50 +0800

    refactor(cpp): remove unused imports and code
    This commit removes unused imports and code from the `CppWriteTestService.kt` file in the `cpp` module. The removed code includes unused imports and an unused extension function `nameWithoutExtension`. This refactor improves code cleanliness and reduces unnecessary clutter.

--- cpp/src/main/kotlin/cc/unitmesh/cpp/provider/testing/CppWriteTestService.kt
+++ cpp/src/main/kotlin/cc/unitmesh/cpp/provider/testing/CppWriteTestService.kt
@@ -5,38 +5,28 @@ import cc.unitmesh.devti.context.ClassContext
 import cc.unitmesh.devti.provider.WriteTestService
 import cc.unitmesh.devti.provider.context.TestFileContext
 import com.intellij.execution.configurations.RunProfile
-import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.application.runReadAction
-import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.jetbrains.cidr.cpp.execution.testing.CMakeTestRunConfiguration
+import com.jetbrains.cidr.lang.OCLanguage
 import com.jetbrains.cidr.lang.psi.OCFunctionDeclaration
 
-private val String.nameWithoutExtension: String
-    get() {
-        val lastDot = lastIndexOf('.')
-        return if (lastDot == -1) this else substring(0, lastDot)
-    }
-
-// use Google Test or CATCH?
 class CppWriteTestService : WriteTestService() {
     override fun runConfigurationClass(project: Project): Class<out RunProfile> = CMakeTestRunConfiguration::class.java
 
     override fun isApplicable(element: PsiElement): Boolean {
-        return true
+        return element.language is OCLanguage
     }
 
     override fun findOrCreateTestFile(sourceFile: PsiFile, project: Project, element: PsiElement): TestFileContext? {
         // 1. check project root test folder, if not exist, create it
         val baseDir = project.guessProjectDir() ?: return null
 
-        val sourceFilePath = sourceFile.virtualFile.name
+        val sourceFilePath = sourceFile.virtualFile
 
         val testFilePath = sourceFilePath.nameWithoutExtension + "_test" + "." + sourceFile.virtualFile.extension
         val testFile = WriteAction.computeAndWait<VirtualFile?, Throwable> {
