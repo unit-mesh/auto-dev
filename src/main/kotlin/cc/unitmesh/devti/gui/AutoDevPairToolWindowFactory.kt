936cd3d488943587f0b93b510241739a9da81301
Phodal Huang <h@phodal.com>
2024-01-22 21:17:29 +0800

    feat(gui): add AutoDevPairToolWindow class
    This commit adds the `AutoDevPairToolWindow` class to the `cc.unitmesh.devti.gui.pair` package. The class is responsible for creating the tool window content and displaying a layered architecture code block. It also includes a text field for user input and a submit button.

--- src/main/kotlin/cc/unitmesh/devti/gui/AutoDevPairToolWindowFactory.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/AutoDevPairToolWindowFactory.kt
@@ -1,18 +1,9 @@
 package cc.unitmesh.devti.gui
 
-import cc.unitmesh.devti.fullWidthCell
-import cc.unitmesh.devti.gui.chat.ChatRole
-import cc.unitmesh.devti.provider.architecture.LayeredArchProvider
-import cc.unitmesh.devti.util.parser.Code
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.fileTypes.PlainTextLanguage
+import cc.unitmesh.devti.gui.pair.AutoDevPairToolWindow
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.NullableComponent
-import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.temporary.gui.block.*
-import com.intellij.ui.dsl.builder.panel
 
 class AutoDevPairToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
@@ -23,31 +14,5 @@ class AutoDevPairToolWindowFactory : ToolWindowFactory {
 
         val content = contentManager.factory.createContent(panel, "", false)
         contentManager.addContent(content)
-    }
-}
-
-class AutoDevPairToolWindow(val project: Project, val disposable: Disposable) : SimpleToolWindowPanel(true, true),
-    NullableComponent {
-    init {
-        val layeredArch = LayeredArchProvider.find(project)?.getLayeredArch(project)
-        val panel = panel {
-            row {
-                label("Layered Architecture")
-            }
-            row {
-                val text = layeredArch?.print() ?: "No Layered Arch"
-                val block = CodeBlock(SimpleMessage(text, text, ChatRole.User))
-                block.code = Code(PlainTextLanguage.INSTANCE, text, true)
-
-                val codeBlockView = CodeBlockView(block, project, disposable)
-                fullWidthCell(codeBlockView.getComponent())
-            }
-        }
-
-        setContent(panel)
-    }
-
-    override fun isNull(): Boolean {
-        return false
     }
 }
---
+++ src/main/kotlin/cc/unitmesh/devti/gui/pair/AutoDevPairToolWindow.kt
@@ -0,0 +1,71 @@
+package cc.unitmesh.devti.gui.pair
+
+import cc.unitmesh.devti.fullWidthCell
+import cc.unitmesh.devti.gui.chat.AutoDevInputListener
+import cc.unitmesh.devti.gui.chat.AutoDevInputSection
+import cc.unitmesh.devti.gui.chat.AutoDevInputTrigger
+import cc.unitmesh.devti.gui.chat.ChatRole
+import cc.unitmesh.devti.pair.arch.ProjectPackageTree
+import cc.unitmesh.devti.pair.tasking.Tasking
+import cc.unitmesh.devti.provider.architecture.LayeredArchProvider
+import cc.unitmesh.devti.util.parser.Code
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.NullableComponent
+import com.intellij.openapi.ui.SimpleToolWindowPanel
+import com.intellij.temporary.gui.block.CodeBlock
+import com.intellij.temporary.gui.block.CodeBlockView
+import com.intellij.temporary.gui.block.SimpleMessage
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.panel
+
+data class PairContext(
+    var input: String = "",
+    var tasks: List<Tasking> = listOf(),
+    var arch: ProjectPackageTree? = null,
+)
+
+class AutoDevPairToolWindow(val project: Project, val disposable: Disposable) : SimpleToolWindowPanel(true, true),
+    NullableComponent {
+    init {
+        val layeredArch = LayeredArchProvider.find(project)?.getLayeredArch(project)
+        val model = PairContext(arch = layeredArch)
+
+        // https://plugins.jetbrains.com/docs/intellij/kotlin-ui-dsl-version-2.html
+        val panel = panel {
+            row("Your Task") {
+                text("Your context-aware pair for help you!")
+                textField()
+                    .comment("Your task for asking help!")
+                    .bindText(model::input)
+            }
+
+            row {
+                val inputSection = AutoDevInputSection(project, disposable)
+                inputSection.addListener(object : AutoDevInputListener {
+                    override fun onSubmit(component: AutoDevInputSection, trigger: AutoDevInputTrigger) {
+                        val prompt = component.text
+                        println("prompt: $prompt")
+                    }
+                })
+            }
+            row {
+                val text = layeredArch?.print() ?: "No Layered Arch"
+                val block = CodeBlock(SimpleMessage(text, text, ChatRole.User))
+                block.code = Code(PlainTextLanguage.INSTANCE, text, true)
+
+                val codeBlockView = CodeBlockView(block, project, disposable)
+                val component = codeBlockView.getComponent()
+
+                fullWidthCell(component)
+            }
+        }
+
+        setContent(panel)
+    }
+
+    override fun isNull(): Boolean {
+        return false
+    }
+}
