dfa73cc8073cce0f44e36f94c5a9ad26617be37b
Phodal Huang <h@phodal.com>
2024-01-23 08:47:11 +0800

    fix(ui): add lost support for idea 222 vertical alignment in grid layout
    This commit adds support for vertical alignment in the grid layout of the UI. The `VerticalAlign` class is imported and can now be used to specify the vertical alignment of components in the grid layout.

--- src/222/main/kotlin/cc/unitmesh/devti/ui.kt
+++ src/222/main/kotlin/cc/unitmesh/devti/ui.kt
@@ -3,6 +3,7 @@ package cc.unitmesh.devti
 import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.Row
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.ui.dsl.gridLayout.VerticalAlign
 import javax.swing.JComponent
 
 fun <T : JComponent> Row.fullWidthCell(component: T): Cell<T> {
