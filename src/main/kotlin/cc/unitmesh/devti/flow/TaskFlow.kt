ecf891e97660786e092b4a01964f6bd9ae705b67
Phodal Huang <h@phodal.com>
2024-01-24 20:32:56 +0800

    feat(flow): add documentation and comments to TaskFlow interface #81
    The TaskFlow interface in `TaskFlow.kt` has been updated to include documentation and comments for each method. This will improve code readability and provide better understanding of the purpose and functionality of each method. The `clarify()` method now returns a string representing the documentation for the class. The `design()` method now takes a context parameter and returns a string specifying the format for designing the task flow. The `execute()` method now includes a comment explaining its purpose and the expected result of the execution.

--- src/main/kotlin/cc/unitmesh/devti/flow/TaskFlow.kt
+++ src/main/kotlin/cc/unitmesh/devti/flow/TaskFlow.kt
@@ -1,10 +1,40 @@
 package cc.unitmesh.devti.flow
 
+/**
+ * AutoDev TaskFlow is the built-in Workflow for AutoDev, see in [#81](https://github.com/unit-mesh/auto-dev/issues/81)
+ */
 interface TaskFlow {
+    /**
+     * This method is used to clarify the purpose of user's requirement.
+     * It returns a string will specify format, will parse in design()
+     * For example, tableNames in SQL:
+     * ```markdown
+     * [item1, item2, item3]
+     * ```
+     * @return A string representing the documentation for the class.
+     */
     fun clarify(): String
+
+    /**
+     * This method is used to ask LLM to design the tasking flow based on the given context.
+     * It returns a string will specify format, will parse in execute()
+     *
+     * @param context The context for designing the task flow.
+     */
     fun design(context: Any): String {
         return ""
     }
+
+    /**
+     * Executes the last step of the task flow.
+     *
+     * Like:
+     * - Execute SQL in [GenSqlFlow]
+     * - Execute HTTP request in [GenHttpFlow]
+     * - Execute shell command in [GenShellFlow]
+     *
+     * @return A string representing the result of the execution.
+     */
     fun execute(): String {
         return ""
     }
