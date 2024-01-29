0e1edf155353a1aad73c92e26939d0d49308c333
iptton <iptton@gmail.com>
2024-01-29 15:25:04 +0800

    fix(util): LLMCoroutineScope use SupervisorJob to avoid coroutines affected each other.

--- src/main/kotlin/cc/unitmesh/devti/util/LLMCoroutineScope.kt
+++ src/main/kotlin/cc/unitmesh/devti/util/LLMCoroutineScope.kt
@@ -3,11 +3,16 @@ package cc.unitmesh.devti.util
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.SupervisorJob
+
+val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    throwable.printStackTrace()
+}
 
 @Service(Service.Level.PROJECT)
-class LLMCoroutineScope(val coroutineScope: CoroutineScope = CoroutineScope(EmptyCoroutineContext)) {
+class LLMCoroutineScope(val coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + coroutineExceptionHandler)){
     companion object {
         fun scope(project: Project): CoroutineScope = project.service<LLMCoroutineScope>().coroutineScope
     }
