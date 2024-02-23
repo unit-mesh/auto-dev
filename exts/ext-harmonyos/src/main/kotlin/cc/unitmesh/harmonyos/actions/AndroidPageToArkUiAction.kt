e175992779ce93974c223568c9daa0b30f340639
Phodal Huang <h@phodal.com>
2024-02-23 15:49:53 +0800

    feat(harmonyos): add support for sending UI to chat panel
    This commit adds support for sending UI elements to the chat panel in the HarmonyOS extension. The functionality is implemented in the `AndroidPageToArkUiAction` class by importing the required classes `cc.unitmesh.devti.gui.sendToChatPanel` and `cc.unitmesh.devti.llms.LlmFactory`. The `isAvailable` method has also been updated to check for compatibility with DevEcoStudio and AndroidStudio.

--- exts/ext-harmonyos/src/main/kotlin/cc/unitmesh/harmonyos/actions/AndroidPageToArkUiAction.kt
+++ exts/ext-harmonyos/src/main/kotlin/cc/unitmesh/harmonyos/actions/AndroidPageToArkUiAction.kt
@@ -1,19 +1,40 @@
 package cc.unitmesh.harmonyos.actions
 
+import cc.unitmesh.devti.gui.sendToChatPanel
 import cc.unitmesh.devti.intentions.action.base.ChatBaseIntention
+import cc.unitmesh.devti.llms.LlmFactory
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.psi.PsiFile
 
 class AndroidPageToArkUiAction : ChatBaseIntention() {
+    override fun priority(): Int = 900
+    override fun getText(): String = "Android Page to Ark UI"
+    override fun getFamilyName(): String = "Android Page to Ark UI"
+
     override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
         return System.getProperty("idea.platform.prefix", "idea") == "DevEcoStudio"
                 || System.getProperty("idea.platform.prefix", "idea") == "AndroidStudio"
     }
 
-    override fun priority(): Int = 900
+    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+        if (editor == null || file == null) return
+        val selectedText = editor.selectionModel.selectedText ?: return
 
-    override fun getText(): String = "Android Page to Ark UI"
+        val autoUi = AutoArkUi(project, selectedText, editor)
 
-    override fun getFamilyName(): String = "Android Page to Ark UI"
+        sendToChatPanel(project) { contentPanel, _ ->
+            val llmProvider = LlmFactory().create(project)
+//            val context = AutoPageContext.build(reactAutoPage,  language, frameworks)
+//            val prompter = AutoPageFlow(context, contentPanel, llmProvider)
+//
+//            ProgressManager.getInstance()
+//                .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
+        }
+    }
+}
+
+class AutoArkUi(project: Project, selectedText: @NlsSafe String, editor: Editor) {
+
 }
