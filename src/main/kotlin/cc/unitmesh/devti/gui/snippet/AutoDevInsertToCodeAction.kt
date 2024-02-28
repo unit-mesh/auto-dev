66c2e964b7b9a948c0f73cc281c1854237419ae1
Phodal Huang <h@phodal.com>
2024-02-28 09:26:53 +0800

    feat(icon): add support for auto dev insert file action
    This commit modifies the `AutoDevInsertToCodeAction.kt` file in order to add support for the auto dev insert file action in the GUI. It imports `PlatformDataKeys` to access editor data and performs a command action to insert selected text or the entire text into the editor.

--- src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevInsertToCodeAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevInsertToCodeAction.kt
@@ -2,24 +2,22 @@ package cc.unitmesh.devti.gui.snippet
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.util.TextRange
 
 class AutoDevInsertFileAction : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val editor = e.getData(com.intellij.openapi.actionSystem.PlatformDataKeys.EDITOR) ?: return
+        val project = e.project ?: return
+        val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
         val selectionModel = if (editor.selectionModel.hasSelection()) editor.selectionModel else null
         val textToPaste = selectionModel?.selectedText ?: editor.document.text.trimEnd()
 
-        val project = e.project ?: return
-
         val selectedTextEditor = FileEditorManager.getInstance(project).selectedTextEditor
-        WriteCommandAction.writeCommandAction(project).compute<TextRange, RuntimeException> {
+        WriteCommandAction.writeCommandAction(project).compute<Any, RuntimeException> {
             val offset = selectionModel?.selectionStart ?: selectedTextEditor?.document?.textLength ?: return@compute null
             selectedTextEditor?.document?.insertString(offset, textToPaste)
-            TextRange.from(offset, textToPaste.length)
         }
     }
 
