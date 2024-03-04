4a0a7a4451a66f471d1bb462a39ef843f9109979
Phodal Huang <h@phodal.com>
2024-03-04 22:25:43 +0800

    fix: fix import

--- src/main/kotlin/cc/unitmesh/devti/gui/chat/ChatCodingPanel.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/chat/ChatCodingPanel.kt
@@ -21,7 +21,6 @@ import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.util.preferredHeight
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
