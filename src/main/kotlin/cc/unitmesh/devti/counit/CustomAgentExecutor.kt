4bd68404aaf79a4b2dca0e79689a90c783b3a169
Phodal Huang <h@phodal.com>
2024-03-05 17:32:58 +0800

    feat(custom-agent): add support for custom request format and improve serialization #%1
    The `CustomAgentExecutor` class has been updated to support a custom request format and to use Kotlinx serialization for improved performance and readability. The request body is now serialized using `Json.encodeToString`, and the `CustomRequest` class has been added to represent the custom request structure. This change also includes the import of necessary Kotlinx serialization and OkHttp libraries.

--- src/main/kotlin/cc/unitmesh/devti/counit/CustomAgentExecutor.kt
+++ src/main/kotlin/cc/unitmesh/devti/counit/CustomAgentExecutor.kt
@@ -3,12 +3,16 @@ package cc.unitmesh.devti.counit
 import cc.unitmesh.devti.counit.configurable.customAgentSetting
 import cc.unitmesh.devti.counit.model.AuthType
 import cc.unitmesh.devti.counit.model.CustomAgentConfig
+import cc.unitmesh.devti.llms.custom.CustomRequest
+import cc.unitmesh.devti.llms.custom.Message
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 
 @Service(Service.Level.PROJECT)
 class CustomAgentExecutor(val project: Project) {
@@ -16,7 +20,11 @@ class CustomAgentExecutor(val project: Project) {
 
     fun execute(input: String, agent: CustomAgentConfig): String? {
         val serverAddress = project.customAgentSetting.serverAddress ?: return null
-        val body = RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), input)
+
+        val customRequest = CustomRequest(listOf(Message("user", input)))
+        val request = Json.encodeToString<CustomRequest>(customRequest)
+
+        val body = request.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
         val builder = Request.Builder()
 
         val auth = agent.auth
