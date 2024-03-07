a4e751c79c53583786942507f8619988475e0322
Phodal Huang <h@phodal.com>
2024-03-07 14:37:52 +0800

    fix(snippet): fix UpdateThread issu && add logging to AutoDevInsertCodeAction #51
    Add logging to the AutoDevInsertCodeAction class to provide better visibility of actions performed by the code snippet. This will help in debugging and tracking the execution flow.

--- src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevInsertCodeAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevInsertCodeAction.kt
@@ -3,7 +3,9 @@ package cc.unitmesh.devti.gui.snippet
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.psi.PsiDocumentManager
@@ -11,6 +13,8 @@ import com.intellij.psi.codeStyle.CodeStyleManager
 
 
 class AutoDevInsertCodeAction : DumbAwareAction() {
+    private val logger = logger<AutoDevInsertCodeAction>()
+
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
@@ -53,5 +57,5 @@ class AutoDevInsertCodeAction : DumbAwareAction() {
         }
     }
 
-    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 }
