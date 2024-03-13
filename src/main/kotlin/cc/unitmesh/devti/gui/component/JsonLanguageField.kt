490524806a2811686b985cd407fd1ee3543fa006
Phodal Huang <h@phodal.com>
2024-03-13 19:50:30 +0800

    refactor(schema): refactor custom agent schema provider and file names #101
    Renamed CUSTOM_AGENT_FILE_NAME and AutoDevCustomAgentFile.json to match the new naming convention. Refactored AutoDevJsonJsonSchemaProviderFactory and AutoDevJsonSchemaFileProvider classes into CustomAgentSchemaFileProvider and CustomAgentJsonSchemaProviderFactory to improve code organization and readability.

--- src/main/kotlin/cc/unitmesh/devti/gui/component/JsonLanguageField.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/component/JsonLanguageField.kt
@@ -1,22 +1,16 @@
 package cc.unitmesh.devti.gui.component
 
+import cc.unitmesh.devti.custom.schema.CUSTOM_AGENT_FILE_NAME
 import com.intellij.json.JsonLanguage
 import com.intellij.lang.Language
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.colors.EditorColorsUtil
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.ui.LanguageTextField
-import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
-import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
-import com.jetbrains.jsonSchema.extension.SchemaType
 import java.awt.Dimension
 import java.awt.FontMetrics
-
-private const val CUSTOM_AGENT_NAME = "AutoDevCustomAgentFile.json"
 
 class JsonLanguageField(private val myProject: Project, val value: String, private val placeholder: String) :
     LanguageTextField(JsonLanguage.INSTANCE, myProject, value,
@@ -26,7 +20,7 @@ class JsonLanguageField(private val myProject: Project, val value: String, priva
             }
 
             override fun customizePsiFile(file: PsiFile?) {
-                file?.name = CUSTOM_AGENT_NAME
+                file?.name = CUSTOM_AGENT_FILE_NAME
             }
         }
     ) {
@@ -49,36 +43,3 @@ class JsonLanguageField(private val myProject: Project, val value: String, priva
     }
 }
 
-internal class AutoDevJsonJsonSchemaProviderFactory: JsonSchemaProviderFactory {
-    override fun getProviders(project: Project): MutableList<JsonSchemaFileProvider> {
-        return mutableListOf(AutoDevJsonSchemaFileProvider(project))
-    }
-}
-
-class AutoDevJsonSchemaFileProvider(val project: Project): JsonSchemaFileProvider {
-    override fun isAvailable(file: VirtualFile): Boolean {
-        return runReadAction { isAutoDevCustomAgentFile(file) }
-    }
-
-    private fun isAutoDevCustomAgentFile(file: VirtualFile): Boolean {
-        if (!file.isValid) {
-            return false
-        }
-
-        return file.name == CUSTOM_AGENT_NAME
-    }
-
-    override fun getName(): String = "AutoDevCustomAgentFile"
-
-    override fun getSchemaFile(): VirtualFile? {
-        return JsonSchemaProviderFactory.getResourceFile(this::class.java, schemaFileName)
-    }
-
-    override fun getSchemaType(): SchemaType {
-        return SchemaType.schema
-    }
-
-    companion object {
-        private const val schemaFileName = "autodev-custom-agent.json"
-    }
-}
