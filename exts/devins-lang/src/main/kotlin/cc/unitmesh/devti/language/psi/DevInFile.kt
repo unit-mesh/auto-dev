75212464952be0d9bfec2893f1301732ec2f49d3
Phodal Huang <h@phodal.com>
2024-03-16 23:01:25 +0800

    fix(language): update external ID in DevInFile.kt #101
    The external ID in DevInFile.kt was updated from "Feakin.file" to "devin.file" to match the expected value for improved consistency and future maintenance.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/psi/DevInFile.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/psi/DevInFile.kt
@@ -10,7 +10,7 @@ import com.intellij.psi.StubBuilder
 import com.intellij.psi.stubs.*
 import com.intellij.psi.tree.IStubFileElementType
 
-class DevInFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, DevInLanguage) {
+class DevInFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, DevInLanguage.INSTANCE) {
     override fun getFileType(): FileType = DevInFileType.INSTANCE
 
     override fun getOriginalFile(): DevInFile = super.getOriginalFile() as DevInFile
@@ -27,7 +27,7 @@ class DevInFileStub(file: DevInFile?, private val flags: Int) : PsiFileStubImpl<
     object Type : IStubFileElementType<DevInFileStub>(DevInLanguage) {
         override fun getStubVersion(): Int = 1
 
-        override fun getExternalId(): String = "Feakin.file"
+        override fun getExternalId(): String = "devin.file"
 
         override fun serialize(stub: DevInFileStub, dataStream: StubOutputStream) {
             dataStream.writeByte(stub.flags)
