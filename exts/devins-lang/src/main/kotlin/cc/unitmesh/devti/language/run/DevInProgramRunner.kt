91acc81947ca73ab0cb5c87cd8d0acc3383a5138
Phodal Huang <h@phodal.com>
2024-03-16 21:04:43 +0800

    fix(devins-lang): use GenericProgramRunner for DevInProgramRunner #101
    Use GenericProgramRunner instead of ProgramRunner in DevInProgramRunner for improved compatibility and flexibility. Update execute method to match new interface signature.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/run/DevInProgramRunner.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/run/DevInProgramRunner.kt
@@ -1,30 +1,29 @@
 package cc.unitmesh.devti.language.run
 
-import com.intellij.execution.ExecutionManager
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.ProgramRunner
+import com.intellij.execution.runners.GenericProgramRunner
 import com.intellij.execution.runners.showRunContent
+import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import org.jetbrains.annotations.NonNls
 
-class DevInProgramRunner : ProgramRunner<RunnerSettings> {
+open class DevInProgramRunner : GenericProgramRunner<RunnerSettings>() {
     companion object {
         const val RUNNER_ID: String = "DevInCommandRunner"
     }
 
-    override fun getRunnerId(): @NonNls String = RUNNER_ID
+    override fun getRunnerId(): String = RUNNER_ID
 
     override fun canRun(executorId: String, profile: RunProfile): Boolean {
         return profile is AutoDevConfiguration
     }
 
-    override fun execute(environment: ExecutionEnvironment) {
-        ExecutionManager.getInstance(environment.project).startRunProfile(environment) { state: RunProfileState ->
-            FileDocumentManager.getInstance().saveAllDocuments()
-            showRunContent(state.execute(environment.executor, this), environment)
-        }
+    override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
+        if (environment.runProfile !is AutoDevConfiguration) return null
+
+        FileDocumentManager.getInstance().saveAllDocuments()
+        return showRunContent(state.execute(environment.executor, this), environment)
     }
 }
