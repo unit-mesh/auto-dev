ad41f5b5314a747c81218cfdaacf2a83342d7e84
Phodal Huang <h@phodal.com>
2024-03-16 19:45:12 +0800

    fix(devins-lang): modify PatchInsCommand.kt and InsCommand.kt #101
    - Remove unused imports in PatchInsCommand.kt
    - Replace deprecated classes and methods in PatchInsCommand.kt
    - Modify logic in PatchInsCommand.kt to use MultiMap and ApplyPatchDefaultExecutor
    - Add comment in InsCommand.kt

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/PatchInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/PatchInsCommand.kt
@@ -1,58 +1,40 @@
 package cc.unitmesh.devti.language.compiler.exec
 
-import cc.unitmesh.devti.AutoDevBundle
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.diff.impl.patch.FilePatch
 import com.intellij.openapi.diff.impl.patch.PatchReader
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.StreamUtil
+import com.intellij.openapi.vcs.changes.patch.AbstractFilePatchInProgress
+import com.intellij.openapi.vcs.changes.patch.ApplyPatchDefaultExecutor
 import com.intellij.openapi.vcs.changes.patch.MatchPatchPaths
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.testFramework.LightVirtualFile
-import java.io.IOException
-import java.io.InputStreamReader
+import com.intellij.util.containers.MultiMap
 
 class PatchInsCommand(val myProject: Project, val prop: String, val codeContent: String) : InsCommand {
     override fun execute(): String? {
         FileDocumentManager.getInstance().saveAllDocuments()
 
-        val patchFile = LightVirtualFile(prop, codeContent)
+        val shelfExecutor = ApplyPatchDefaultExecutor(myProject)
 
-        val myReader: PatchReader = loadPatches(patchFile) ?: return "<DevliError>: Cannot read patch file."
+        val myReader = PatchReader(codeContent)
         myReader.parseAllPatches()
 
-        val filePatches: MutableList<FilePatch> = ArrayList()
-        filePatches.addAll(myReader.allPatches)
-
-
+        val filePatches: MutableList<FilePatch> = myReader.allPatches
         val matchedPatches =
             MatchPatchPaths(myProject).execute(filePatches, true)
 
+        val patchGroups = MultiMap<VirtualFile, AbstractFilePatchInProgress<*>>()
+        for (patchInProgress in matchedPatches) {
+            patchGroups.putValue(patchInProgress.base, patchInProgress)
+        }
+
         ApplicationManager.getApplication().invokeLater {
-            matchedPatches
+            val additionalInfo = myReader.getAdditionalInfo(ApplyPatchDefaultExecutor.pathsFromGroups(patchGroups))
+            shelfExecutor.apply(filePatches, patchGroups, null, prop, additionalInfo)
         }
 
         return "Patch applied"
-    }
-
-    private fun loadPatches(patchFile: VirtualFile): PatchReader? {
-        try {
-            val text = ReadAction.compute<String, IOException> {
-                InputStreamReader(patchFile.inputStream, patchFile.charset)
-                    .use { inputStreamReader ->
-                        return@compute StreamUtil.readText(inputStreamReader)
-                    }
-            }
-
-            val reader = PatchReader(text)
-            reader.parseAllPatches()
-            return reader
-        } catch (e: Exception) {
-            AutoDevBundle.message("devin.patch.cannot.read.patch", patchFile.presentableName)
-            return null
-        }
     }
 
 }
