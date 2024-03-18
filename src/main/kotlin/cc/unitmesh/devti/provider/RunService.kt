f4c33bcccc05217c4d336992669b8b46bcb2644f
Phodal Huang <h@phodal.com>
2024-03-18 21:43:54 +0800

    feat(devins-lang): improve dynamic run configuration creation #101

--- src/main/kotlin/cc/unitmesh/devti/provider/RunService.kt
+++ src/main/kotlin/cc/unitmesh/devti/provider/RunService.kt
@@ -5,12 +5,17 @@ import com.intellij.execution.ExecutorRegistryImpl
 import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
-import com.intellij.ide.actions.runAnything.RunAnythingPopupUI
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
+import java.util.function.Consumer
+
+val RW_MAIN_CONFIGURATION_ID: Key<String> = Key<String>("RunWidgetMainRunConfigurationId")
 
 interface RunService {
     private val logger: Logger get() = logger<RunService>()
@@ -34,7 +39,11 @@ interface RunService {
             it.name == virtualFile.nameWithoutExtension && (it.javaClass == runConfigureClass)
         }
 
+        var isTemporary = false
+
+        // try to create config if not founds
         if (testConfig == null) {
+            isTemporary = true
             testConfig = createConfiguration(project, virtualFile)
         }
 
@@ -43,24 +52,30 @@ interface RunService {
             return
         }
 
-        val configurationSettings =
-            runManager.findConfigurationByTypeAndName(testConfig.type, testConfig.name)
-
-        if (configurationSettings == null) {
+        val settings = runManager.findConfigurationByTypeAndName(testConfig.type, testConfig.name)
+        if (settings == null) {
             logger.warn("Failed to find test configuration for: ${virtualFile.nameWithoutExtension}")
             return
         }
 
-        logger.info("configurationSettings: $configurationSettings")
-        runManager.selectedConfiguration = configurationSettings
+        if (isTemporary) {
+            settings.isTemporary = true
+        }
 
-        val executor: Executor = RunAnythingPopupUI.getExecutor()
-        ExecutorRegistryImpl.RunnerHelper.run(
+        logger.info("configurationSettings: $settings")
+        runManager.selectedConfiguration = settings
+
+        val executor: Executor = DefaultRunExecutor()
+        val dataContext = DataContext.EMPTY_CONTEXT
+
+        ExecutorRegistryImpl.RunnerHelper.runSubProcess(
             project,
             testConfig,
-            configurationSettings,
-            DataContext.EMPTY_CONTEXT,
+            settings,
+            dataContext,
             executor
-        )
+        ) { ee: ExecutionEnvironment ->
+            // TODO: Add customizations for process data
+        }
     }
 }
