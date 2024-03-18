3580aa31ddb839e1e2066fd23dadc6f1edada486
Phodal Huang <h@phodal.com>
2024-03-18 22:05:15 +0800

    feat(devins-lang): add support for execution environment customization #101
    Add support for customizing the process data and reading output text when running a file.
    The `ExecutionEnvironmentBuilder` is created to customize the executor settings.

--- src/main/kotlin/cc/unitmesh/devti/provider/RunService.kt
+++ src/main/kotlin/cc/unitmesh/devti/provider/RunService.kt
@@ -1,19 +1,21 @@
 package cc.unitmesh.devti.provider
 
+import com.intellij.execution.ExecutionManager
 import com.intellij.execution.Executor
-import com.intellij.execution.ExecutorRegistryImpl
 import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.executors.DefaultRunExecutor
-import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.impl.ConsoleViewImpl
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder
+import com.intellij.execution.ui.RunContentManager
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.vfs.VirtualFile
-import java.util.function.Consumer
 
 val RW_MAIN_CONFIGURATION_ID: Key<String> = Key<String>("RunWidgetMainRunConfigurationId")
 
@@ -32,7 +34,7 @@ interface RunService {
 
     fun createConfiguration(project: Project, path: String): RunConfiguration? = null
 
-    fun runFile(project: Project, virtualFile: VirtualFile) {
+    fun runFile(project: Project, virtualFile: VirtualFile): @NlsSafe String? {
         val runManager = RunManager.getInstance(project)
         var testConfig = runManager.allConfigurationsList.firstOrNull {
             val runConfigureClass = runConfigurationClass(project)
@@ -49,13 +51,13 @@ interface RunService {
 
         if (testConfig == null) {
             logger.warn("Failed to find test configuration for: ${virtualFile.nameWithoutExtension}")
-            return
+            return null
         }
 
         val settings = runManager.findConfigurationByTypeAndName(testConfig.type, testConfig.name)
         if (settings == null) {
             logger.warn("Failed to find test configuration for: ${virtualFile.nameWithoutExtension}")
-            return
+            return null
         }
 
         if (isTemporary) {
@@ -68,14 +70,37 @@ interface RunService {
         val executor: Executor = DefaultRunExecutor()
         val dataContext = DataContext.EMPTY_CONTEXT
 
-        ExecutorRegistryImpl.RunnerHelper.runSubProcess(
-            project,
-            testConfig,
-            settings,
-            dataContext,
-            executor
-        ) { ee: ExecutionEnvironment ->
-            // TODO: Add customizations for process data
-        }
+//        ExecutorRegistryImpl.RunnerHelper.runSubProcess(
+//            project,
+//            testConfig,
+//            settings,
+//            dataContext,
+//            executor
+//        ) { ee: ExecutionEnvironment ->
+//            // TODO: Add customizations for process data
+//            val selectedContent = RunContentManager.getInstance(project).selectedContent
+//            val executionConsole = selectedContent?.executionConsole
+//                    as? ConsoleViewImpl ?: return@runSubProcess
+//
+//            val document = executionConsole.editor.document
+//            val text = document.text
+//        }
+//
+//        RunManager.getInstance(project)
+//
+//        return ""
+
+        var builder = ExecutionEnvironmentBuilder.createOrNull(executor, settings) ?: return null
+        builder = builder.activeTarget()
+        val environment = builder.dataContext(dataContext).build()
+        ExecutionManager.getInstance(project).restartRunProfile(environment)
+
+        val selectedContent = RunContentManager.getInstance(project).selectedContent
+        val executionConsole = selectedContent?.executionConsole as? ConsoleViewImpl ?: return null
+
+        val document = executionConsole.editor.document
+        val text = document.text
+
+        return text
     }
 }
