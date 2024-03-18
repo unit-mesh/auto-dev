79397f73f8188c44ea6e3c7fbc5e6933af413960
Phodal Huang <h@phodal.com>
2024-03-18 16:39:07 +0800

    chore(devins-lang): remove unused imports and functions #101
    Remove unused imports and functions in the `DevInCompletionContributor` class to improve code cleanliness.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/DevInCompletionContributor.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/DevInCompletionContributor.kt
@@ -1,21 +1,16 @@
 package cc.unitmesh.devti.language
 
 import cc.unitmesh.devti.language.completion.*
-import cc.unitmesh.devti.language.psi.DevInFile
 import cc.unitmesh.devti.language.psi.DevInTypes
 import cc.unitmesh.devti.language.psi.DevInUsed
 import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionInitializationContext
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.codeInsight.completion.CompletionUtilCore
 import com.intellij.patterns.ElementPattern
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PsiElementPattern
 import com.intellij.psi.PsiElement
 
 class DevInCompletionContributor : CompletionContributor() {
-    private val INPUT_DUMMY_IDENTIFIER = CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED
-
     init {
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(DevInTypes.LANGUAGE_ID), CodeFenceLanguageProvider())
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(DevInTypes.VARIABLE_ID), CustomVariableProvider())
@@ -35,12 +30,6 @@ class DevInCompletionContributor : CompletionContributor() {
             valuePattern(BuiltinCommand.SYMBOL.commandName),
             SymbolReferenceLanguageProvider()
         )
-    }
-
-    override fun beforeCompletion(context: CompletionInitializationContext) {
-        if (context.file is DevInFile) {
-            context.dummyIdentifier = INPUT_DUMMY_IDENTIFIER
-        }
     }
 
     private inline fun <reified I : PsiElement> psiElement(): PsiElementPattern.Capture<I> {
