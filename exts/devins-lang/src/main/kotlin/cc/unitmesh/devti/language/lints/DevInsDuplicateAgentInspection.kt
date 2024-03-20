d2df0124af9c6c46525316f5508b0c08573fac8e
Phodal Huang <h@phodal.com>
2024-03-20 09:36:48 +0800

    feat(devins-lang): add duplicate agent declaration inspection #101
    This commit introduces a new LocalInspectionTool for the DevIns language, which detects and reports duplicate agent declarations. The inspection is implemented in `DevInsDuplicateAgentInspection` and is registered in the `.xml` file under the `localInspection` tag.

---
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/lints/DevInsDuplicateAgentInspection.kt
@@ -0,0 +1,33 @@
+package cc.unitmesh.devti.language.lints
+
+import cc.unitmesh.devti.language.psi.DevInTypes
+import cc.unitmesh.devti.language.psi.DevInUsed
+import cc.unitmesh.devti.language.psi.DevInVisitor
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.util.elementType
+
+class DevInsDuplicateAgentInspection : LocalInspectionTool() {
+    override fun getDisplayName() = "Duplicate agent calling"
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return DevInsDuplicateAgentVisitor(holder)
+    }
+
+    private class DevInsDuplicateAgentVisitor(val holder: ProblemsHolder) : DevInVisitor() {
+        private var agentIds: MutableSet<String> = mutableSetOf()
+
+        override fun visitUsed(o: DevInUsed) {
+            o.firstChild.let { next ->
+                if (next.nextSibling.elementType == DevInTypes.AGENT_ID) {
+                    if (agentIds.contains(next.text)) {
+                        holder.registerProblem(next, "Duplicate agent calling")
+                    } else {
+                        agentIds.add(next.text)
+                    }
+                }
+            }
+        }
+    }
+}
