24a5da1b2b28d538c2cfc04f418b81c02401e3c9
Phodal Huang <h@phodal.com>
2024-03-25 19:08:34 +0800

    feat(devins-android): init Android test service support

---
+++ exts/ext-android/src/main/kotlin/cc/unitmesh/android/provider/AndroidTestService.kt
@@ -0,0 +1,28 @@
+package cc.unitmesh.android.provider
+
+import cc.unitmesh.devti.context.ClassContext
+import cc.unitmesh.devti.provider.AutoTestService
+import cc.unitmesh.devti.provider.context.TestFileContext
+import com.intellij.execution.configurations.RunProfile
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import org.jetbrains.android.util.AndroidUtils
+import org.jetbrains.kotlin.psi.KtFile
+
+class AndroidTestService: AutoTestService() {
+    override fun isApplicable(element: PsiElement): Boolean = AndroidUtils.hasAndroidFacets(element.project)
+    override fun psiFileClass(project: Project): Class<out PsiElement> = KtFile::class.java
+
+    override fun runConfigurationClass(project: Project): Class<out RunProfile>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findOrCreateTestFile(sourceFile: PsiFile, project: Project, element: PsiElement): TestFileContext? {
+        TODO("Not yet implemented")
+    }
+
+    override fun lookupRelevantClass(project: Project, element: PsiElement): List<ClassContext> {
+        TODO("Not yet implemented")
+    }
+}
