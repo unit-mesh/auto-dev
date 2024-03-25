691fff55851f71ea439bd6511b109f3ce67bf4cc
Phodal Huang <h@phodal.com>
2024-03-25 11:39:39 +0800

    feat(devins-python): add support for creating Python run configurations #100

--- pycharm/src/main/kotlin/cc/unitmesh/python/provider/PythonAutoTestService.kt
+++ pycharm/src/main/kotlin/cc/unitmesh/python/provider/PythonAutoTestService.kt
@@ -3,23 +3,50 @@ package cc.unitmesh.python.provider
 import cc.unitmesh.devti.context.ClassContext
 import cc.unitmesh.devti.provider.AutoTestService
 import cc.unitmesh.devti.provider.context.TestFileContext
+import com.intellij.execution.RunManager
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.RunConfigurationProducer
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.PsiUtilBase
 import com.jetbrains.python.PythonLanguage
 import com.jetbrains.python.psi.PyClass
+import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.PyFunction
 import com.jetbrains.python.run.PythonRunConfiguration
+import com.jetbrains.python.run.PythonRunConfigurationProducer
 
 class PythonAutoTestService : AutoTestService() {
     override fun isApplicable(element: PsiElement): Boolean = element.language is PythonLanguage
 
     override fun runConfigurationClass(project: Project): Class<out RunProfile> = PythonRunConfiguration::class.java
+
+    override fun createConfiguration(project: Project, virtualFile: VirtualFile): RunConfiguration? {
+        val psiFile: PyFile = PsiManager.getInstance(project).findFile(virtualFile) as? PyFile ?: return null
+        val runManager = RunManager.getInstance(project)
+
+        val context = ConfigurationContext(psiFile)
+        val configProducer = RunConfigurationProducer.getInstance(
+            PythonRunConfigurationProducer::class.java
+        )
+        var settings = configProducer.findExistingConfiguration(context)
+
+        if (settings == null) {
+            val fromContext = configProducer.createConfigurationFromContext(context) ?: return null
+            settings = fromContext.configurationSettings
+            runManager.setTemporaryConfiguration(settings)
+        }
+        val configuration = settings.configuration as PythonRunConfiguration
+        return configuration
+    }
 
     fun getElementForTests(project: Project, editor: Editor): PsiElement? {
         val element = PsiUtilBase.getElementAtCaret(editor) ?: return null
