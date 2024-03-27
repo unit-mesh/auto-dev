ad1c4e8d53d697b23c8e96bccd91246eade6f8af
Phodal Huang <h@phodal.com>
2024-03-27 20:18:56 +0800

    refactor: ensure correct handling of cmakeLists.txt and test file creation.

--- pycharm/src/main/kotlin/cc/unitmesh/python/provider/PythonAutoTestService.kt
+++ pycharm/src/main/kotlin/cc/unitmesh/python/provider/PythonAutoTestService.kt
@@ -9,14 +9,21 @@ import com.intellij.execution.actions.RunConfigurationProducer
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.PsiUtilBase
+import com.intellij.util.concurrency.annotations.RequiresReadLock
+import com.intellij.util.concurrency.annotations.RequiresWriteLock
+import com.jetbrains.python.PythonLanguage
 import com.jetbrains.python.psi.PyClass
 import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.PyFunction
@@ -66,7 +73,43 @@ class PythonAutoTestService : AutoTestService() {
     }
 
     override fun findOrCreateTestFile(sourceFile: PsiFile, project: Project, element: PsiElement): TestFileContext? {
-        TODO("Not yet implemented")
+        val testFileName = getTestNameExample(sourceFile.virtualFile)
+        val testDir = getTestsDirectory(sourceFile.virtualFile, project)
+        val testFile = WriteAction.computeAndWait<VirtualFile?, Throwable> {
+            testDir.findOrCreateChildData(this, toTestFileName(testFileName, sourceFile.name))
+        } ?: return null
+
+        return TestFileContext(true, testFile, listOf(), "", PythonLanguage.INSTANCE)
+    }
+
+    @RequiresReadLock
+    private fun getTestNameExample(file: VirtualFile): String {
+        val children = file.children
+        for (child in children) {
+            val fileName = (child ?: continue).name
+            if (fileName.endsWith(".py") && !fileName.startsWith("_")) {
+                return fileName
+            }
+        }
+
+        return "test_example.py"
+    }
+
+    @RequiresWriteLock
+    fun getTestsDirectory(file: VirtualFile, project: Project): VirtualFile {
+        val baseDirectory: VirtualFile? = ProjectFileIndex.getInstance(project).getContentRootForFile(file)
+        if (baseDirectory == null) {
+            val parent = file.parent
+            return parent
+        }
+
+        val testDir = VfsUtil.createDirectoryIfMissing("tests") ?: baseDirectory
+        return testDir
+    }
+
+    private fun toTestFileName(testFileName: String, testNameExample: String): String {
+        if (testNameExample.startsWith("test_")) return "test_$testFileName.py"
+        return "${testFileName}_test.py"
     }
 
     override fun lookupRelevantClass(project: Project, element: PsiElement): List<ClassContext> {
