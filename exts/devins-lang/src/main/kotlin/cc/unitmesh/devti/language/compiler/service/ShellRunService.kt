3ce99a7799457ad57fadb50e5d659161f8bfffe8
Phodal Huang <h@phodal.com>
2024-03-27 08:51:16 +0800

    feat(devins-lang): introduce new ShellRunService to support running shell scripts. This service simplifies the execution of shell commands within the DevIns IDE, enhancing the user experience. #100

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/service/ShellRunService.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/service/ShellRunService.kt
@@ -2,32 +2,24 @@ package cc.unitmesh.devti.language.compiler.service
 
 import cc.unitmesh.devti.provider.RunService
 import com.intellij.execution.RunManager
-import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.intellij.sh.psi.ShFile
 import com.intellij.sh.run.ShConfigurationType
 import com.intellij.sh.run.ShRunConfiguration
 
 class ShellRunService : RunService {
     override fun runConfigurationClass(project: Project): Class<out RunProfile> = ShRunConfiguration::class.java
-
     override fun createConfiguration(project: Project, virtualFile: VirtualFile): RunConfiguration? {
+        val psiFile = PsiManager.getInstance(project).findFile(virtualFile) as? ShFile ?: return null
         val configurationSetting = RunManager.getInstance(project)
-            .createConfiguration(virtualFile.name, ShConfigurationType.getInstance())
+            .createConfiguration(psiFile.name, ShConfigurationType.getInstance())
 
         val configuration = configurationSetting.configuration as ShRunConfiguration
         configuration.scriptPath = virtualFile.path
         return configurationSetting.configuration
-    }
-
-    override fun createRunSettings(project: Project, virtualFile: VirtualFile): RunnerAndConfigurationSettings? {
-        val runManager = RunManager.getInstance(project)
-        this.createConfiguration(project, virtualFile)?.let {
-            return runManager.findSettings(it)
-        }
-
-        return null
     }
 }
