490f97d503aae54dee7df7b271954c671d27a0cb
Phodal Huang <h@phodal.com>
2024-03-28 23:15:41 +0800

    refactor(devins-lang): add support for browsing web content with new command `/browse`
    This commit introduces a new feature to the `devins-lang` extension, allowing users to browse web content using the new `/browse` command. The command is implemented in `BrowseInsCommand.kt`, which now includes a `runInEdt` block to ensure thread safety when parsing and retrieving the body of the web content.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/BrowseInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/BrowseInsCommand.kt
@@ -2,11 +2,18 @@ package cc.unitmesh.devti.language.compiler
 
 import cc.unitmesh.devti.language.agenttool.browse.BrowseTool
 import cc.unitmesh.devti.language.compiler.exec.InsCommand
+import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 
 class BrowseInsCommand(val myProject: Project, val prop: String) : InsCommand {
     override suspend fun execute(): String? {
-        val documentContent = BrowseTool.parse(prop)
-        return documentContent.body
+        var body: String? = null
+        runInEdt {
+            val parse = BrowseTool.parse(prop)
+            body = parse.body
+        }
+
+        return body
     }
 }
+
