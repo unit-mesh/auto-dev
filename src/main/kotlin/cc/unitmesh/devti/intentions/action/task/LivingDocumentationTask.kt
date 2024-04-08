47c800c8dbda38c5ee0bd94774e833b0eabce1e5
Jia Liu <jialiu@thoughtworks.com>
2024-04-08 17:27:25 +0800

    fix: should not send history when generate documentation

--- src/main/kotlin/cc/unitmesh/devti/intentions/action/task/LivingDocumentationTask.kt
+++ src/main/kotlin/cc/unitmesh/devti/intentions/action/task/LivingDocumentationTask.kt
@@ -35,7 +35,7 @@ class LivingDocumentationTask(
 
         logger.info("Prompt: $prompt")
 
-        val stream = LlmFactory().create(project).stream(prompt, "")
+        val stream = LlmFactory().create(project).stream(prompt, "", false)
 
         var result = ""
 
