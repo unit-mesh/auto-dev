dc960744e23b74d5fb31c7f3944a40fcb3c5919a
Phodal Huang <h@phodal.com>
2024-04-11 15:12:28 +0800

    fix(devins-lang):fix read access issue #144

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/psi/DevInFile.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/psi/DevInFile.kt
@@ -3,6 +3,7 @@ package cc.unitmesh.devti.language.psi
 import cc.unitmesh.devti.language.DevInFileType
 import cc.unitmesh.devti.language.DevInLanguage
 import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFileManager
@@ -23,9 +24,11 @@ class DevInFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, DevI
          * Create a temp DevInFile from a string.
          */
         fun fromString(project: Project, text: String): DevInFile {
-            val filename = DevInLanguage.displayName + "-${UUID.randomUUID()}." + DevInFileType.INSTANCE.defaultExtension
-            val devInFile = PsiFileFactory.getInstance(project)
-                .createFileFromText(filename, DevInLanguage, text) as DevInFile
+            val filename =
+                DevInLanguage.displayName + "-${UUID.randomUUID()}." + DevInFileType.INSTANCE.defaultExtension
+            val devInFile = runReadAction {
+                PsiFileFactory.getInstance(project).createFileFromText(filename, DevInLanguage, text) as DevInFile
+            }
 
             return devInFile
         }
