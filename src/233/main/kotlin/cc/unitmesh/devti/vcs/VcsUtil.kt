357143e05428ebb971ba021a5f713f7b48a2988b
Phodal Huang <h@phodal.com>
2024-05-11 13:54:28 +0800

    fix(VcsUtil): update imports for VcsDataKeys and CommitWorkflowUi

--- src/233/main/kotlin/cc/unitmesh/devti/vcs/VcsUtil.kt
+++ src/233/main/kotlin/cc/unitmesh/devti/vcs/VcsUtil.kt
@@ -2,8 +2,6 @@ package cc.unitmesh.devti.vcs
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.vcs.VcsDataKeys
-import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.CurrentContentRevision
 import com.intellij.vcs.commit.CommitWorkflowUi
 
 object VcsUtil {
