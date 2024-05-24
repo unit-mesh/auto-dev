8c23e51be9949688d6d9066173b31ec7164658e9
Phodal Huang <h@phodal.com>
2024-05-24 11:19:28 +0800

    chore: fix for error version issue

--- src/main/kotlin/cc/unitmesh/devti/update/AutoDevUpdateStartupActivity.kt
+++ src/main/kotlin/cc/unitmesh/devti/update/AutoDevUpdateStartupActivity.kt
@@ -5,32 +5,31 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import java.time.ZonedDateTime
 
-class AutoDevUpdateStartupActivity : ProjectActivity {
-    private val RC_CHANNEL: String = "https://plugins.jetbrains.com/plugins/rc/21520"
-
-    @RequiresBackgroundThread
-    override suspend fun execute(project: Project) {
-        if (ApplicationManager.getApplication().isUnitTestMode) return
-
-//        val pluginHosts = UpdateSettings.getInstance().storedPluginHosts
-//        if (!pluginHosts.contains(RC_CHANNEL)) {
-//            pluginHosts.add(RC_CHANNEL)
+//class AutoDevUpdateStartupActivity : ProjectActivity {
+//    private val RC_CHANNEL: String = "https://plugins.jetbrains.com/plugins/rc/21520"
+//
+//    @RequiresBackgroundThread
+//    override suspend fun execute(project: Project) {
+//        if (ApplicationManager.getApplication().isUnitTestMode) return
+//
+////        val pluginHosts = UpdateSettings.getInstance().storedPluginHosts
+////        if (!pluginHosts.contains(RC_CHANNEL)) {
+////            pluginHosts.add(RC_CHANNEL)
+////        }
+//
+//        val now = ZonedDateTime.now()
+//        val lastCheck: ZonedDateTime? = AutoDevSettingsState.lastCheckTime
+//        if (lastCheck != null && lastCheck.plusDays(1L).isAfter(now)) {
+//            return
 //        }
-
-        val now = ZonedDateTime.now()
-        val lastCheck: ZonedDateTime? = AutoDevSettingsState.lastCheckTime
-        if (lastCheck != null && lastCheck.plusDays(1L).isAfter(now)) {
-            return
-        }
-
-        AutoDevSettingsState.lastCheckTime = now
-        CheckUpdatesTask(project).queue()
-    }
-}
+//
+//        AutoDevSettingsState.lastCheckTime = now
+//        CheckUpdatesTask(project).queue()
+//    }
+//}
 
 class CheckUpdatesTask @JvmOverloads constructor(project: Project, private val notifyNoUpdate: Boolean = false) :
     Task.Backgroundable(project, "Check Update", true) {
