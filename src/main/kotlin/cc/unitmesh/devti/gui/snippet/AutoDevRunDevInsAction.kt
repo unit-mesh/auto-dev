1f92c36b247052c362947b20c096e56170720016
Phodal Huang <h@phodal.com>
2024-05-28 13:36:43 +0800

    feat(editor): replace findVirtualFile with FileDocumentManager in AutoDevRunDevInsAction
    The commit replaces the deprecated findVirtualFile method with FileDocumentManager's getFile method in AutoDevRunDevInsAction.kt. This change ensures compatibility with newer versions of the IntelliJ platform.

--- src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevRunDevInsAction.kt
+++ src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevRunDevInsAction.kt
@@ -3,17 +3,18 @@ package cc.unitmesh.devti.gui.snippet
 import cc.unitmesh.devti.provider.devins.LanguagePromptProcessor
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.psi.PsiManager
-import com.intellij.testFramework.utils.editor.findVirtualFile
 
 class AutoDevRunDevInsAction : DumbAwareAction() {
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 
     override fun update(e: AnActionEvent) {
         val editor = e.getData(com.intellij.openapi.actionSystem.PlatformDataKeys.EDITOR) ?: return
-        val file = editor.document.findVirtualFile() ?: return
         val project = e.project ?: return
+        val document = editor.document
+        val file = FileDocumentManager.getInstance().getFile(document) ?: return
 
         val language = PsiManager.getInstance(project).findFile(file)?.language?.id ?: return
         e.presentation.isEnabled = language == "DevIn" && LanguagePromptProcessor.instance(language).isNotEmpty()
