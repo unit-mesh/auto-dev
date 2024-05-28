b8a2c16484a9db45e0ac1895a52bb3f99167e238
Phodal Huang <h@phodal.com>
2024-05-28 17:43:43 +0800

    feat(custom-action): add batchFileRegex and batchFiles method #193
    Add a new configuration property `batchFileRegex` to support batch file matching in custom intentions. The `batchFiles` method has been implemented to retrieve a list of files based on the provided regex pattern, which can be used for batch operations. This feature enhances the custom action's capability to process multiple files in a batch mode.

--- src/main/kotlin/cc/unitmesh/devti/custom/action/CustomIntentionConfig.kt
+++ src/main/kotlin/cc/unitmesh/devti/custom/action/CustomIntentionConfig.kt
@@ -1,5 +1,8 @@
 package cc.unitmesh.devti.custom.action
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.VirtualFile
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -8,7 +11,23 @@ class CustomIntentionConfig {
     var autoInvoke: Boolean = false
     var matchRegex: String = ""
     var template: String = ""
-    val isBuiltIn: Boolean = false
     val priority: Int = 0
     var selectedRegex: String = ""
+
+    /**
+     * batchFileRegex represents a regular expression used for matching files in batch mode.
+     * When this regex is set, it will be used to determine which files should be included
+     * in batch operations, such as file processing or mass operations.
+     * The regex should be a valid regular expression string according to the regex
+     * capabilities of the host environment.
+     * It is expected that this variable will be set with an appropriate regex pattern
+     * before batch operations are initiated.
+     */
+    var batchFileRegex: String = ""
+
+    /// glob mode
+    fun batchFiles(project: Project): List<VirtualFile> {
+        val regex = Regex(batchFileRegex)
+        return project.guessProjectDir()?.children?.filter { regex.matches(it.name) } ?: emptyList()
+    }
 }
