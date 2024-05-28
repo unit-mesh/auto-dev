cdb5c51f995e45de438ec7a76a9dbe1c7a82b024
Phodal Huang <h@phodal.com>
2024-05-28 09:08:22 +0800

    refactor(JavaRefactoringTool): optimize file lookup and element identification #181
    Optimized the file lookup process in JavaRefactoringTool by changing the search scope from global to project-specific. Also, improved the identification of elements by adding a new property to distinguish between classes and methods. This refactoring enhances the efficiency and accuracy of the tool.

--- java/src/main/kotlin/cc/unitmesh/idea/provider/JavaRefactoringTool.kt
+++ java/src/main/kotlin/cc/unitmesh/idea/provider/JavaRefactoringTool.kt
@@ -1,13 +1,13 @@
 package cc.unitmesh.idea.provider
 
 import cc.unitmesh.devti.provider.RefactoringTool
+import cc.unitmesh.idea.spring.lookupPackageName
 import com.intellij.codeInsight.daemon.impl.quickfix.SafeDeleteFix
 import com.intellij.codeInspection.MoveToPackageFix
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.jvm.analysis.quickFix.RenameQuickFix
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -16,6 +16,7 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
 
 class JavaRefactoringTool : RefactoringTool {
     val project = ProjectManager.getInstance().openProjects.firstOrNull()
@@ -24,19 +25,18 @@ class JavaRefactoringTool : RefactoringTool {
         if (project == null) return null
 
         val elementInfo = getElementInfo(path)
-        val searchScope = GlobalSearchScope.allScope(project)
-        val javaFiles: Collection<VirtualFile> = FileTypeIndex.getFiles(JavaFileType.INSTANCE, searchScope)
+        val searchScope = ProjectScope.getProjectScope(project)
+        val javaFiles: List<PsiJavaFile> = FileTypeIndex.getFiles(JavaFileType.INSTANCE, searchScope)
+            .mapNotNull { PsiManager.getInstance(project).findFile(it) as? PsiJavaFile }
 
         val className = elementInfo.className
         val packageName = elementInfo.pkgName
 
-
-        val sourceFile: VirtualFile = javaFiles.firstOrNull {
-            it.name == "$className.java" && it.parent.name == packageName
+        val sourceFile = javaFiles.firstOrNull {
+            it.packageName == packageName && it.name == "$className.java"
         } ?: return null
 
-        val psiFile = PsiManager.getInstance(project).findFile(sourceFile) ?: return null
-        return psiFile
+        return sourceFile
     }
 
     override fun rename(sourceName: String, targetName: String): Boolean {
@@ -44,24 +44,33 @@ class JavaRefactoringTool : RefactoringTool {
             return false
         }
 
-        val elementInfo = getElementInfo(targetName)
+        val elementInfo = getElementInfo(sourceName)
         val psiFile: PsiFile? = null
 
         // find psi element by cannocial name which is sourceName
         val element = runReadAction {
-            if (elementInfo.isMethod) {
-                val className = elementInfo.className
-                val javaFile = this.lookupFile(sourceName) as? PsiJavaFile ?: return@runReadAction null
+            when {
+                elementInfo.isMethod -> {
+                    val className = elementInfo.className
+                    val javaFile = this.lookupFile(sourceName) as? PsiJavaFile ?: return@runReadAction null
 
-                val psiMethod: PsiMethod =
-                    javaFile.classes.firstOrNull { it.name == className }
-                        ?.methods?.firstOrNull { it.name == elementInfo.methodName }
-                        ?: return@runReadAction null
+                    val psiMethod: PsiMethod =
+                        javaFile.classes.firstOrNull { it.name == className }
+                            ?.methods?.firstOrNull { it.name == elementInfo.methodName }
+                            ?: return@runReadAction null
 
-                psiMethod
-            } else {
-                val sourceFile = LocalFileSystem.getInstance().findFileByPath(sourceName) ?: return@runReadAction null
-                PsiManager.getInstance(project).findFile(sourceFile)
+                    psiMethod
+                }
+
+                elementInfo.isClass -> {
+                    val javaFile = this.lookupFile(sourceName) as? PsiJavaFile ?: return@runReadAction null
+                    javaFile.classes.firstOrNull { it.name == elementInfo.className }
+                }
+
+                else -> {
+                    val javaFile = this.lookupFile(sourceName) as? PsiJavaFile ?: return@runReadAction null
+                    javaFile
+                }
             }
         } ?: return false
 
@@ -79,6 +88,7 @@ class JavaRefactoringTool : RefactoringTool {
     }
 
     data class ElementInfo(
+        val isClass: Boolean,
         val isMethod: Boolean,
         val methodName: String,
         val canonicalName: String,
@@ -86,18 +96,21 @@ class JavaRefactoringTool : RefactoringTool {
         val pkgName: String
     )
 
-    private fun getElementInfo(targetName: String): ElementInfo {
-        val isMethod = targetName.contains("#")
-        val methodName = targetName.substringAfter("#")
-        val canonicalName = targetName.substringBefore("#")
-        val className = canonicalName.substringAfterLast(".")
+    private fun getElementInfo(input: String): ElementInfo {
+        val isMethod = input.contains("#")
+        val methodName = input.substringAfter("#")
+        val canonicalName = input.substringBefore("#")
+        val maybeClassName = canonicalName.substringAfterLast(".")
         // the clasName should be Uppercase or it will be the package
+        var isClass = false
         var pkgName = canonicalName.substringBeforeLast(".")
-        if (className[0].isLowerCase()) {
-            pkgName = "$pkgName.$className"
+        if (maybeClassName[0].isLowerCase()) {
+            pkgName = "$pkgName.$maybeClassName"
+        } else {
+            isClass = true
         }
 
-        return ElementInfo(isMethod, methodName, canonicalName, className, pkgName)
+        return ElementInfo(isClass, isMethod, methodName, canonicalName, maybeClassName, pkgName)
     }
 
     override fun safeDelete(element: PsiElement): Boolean {
