cbdc2bbf825865d277b00fff09770cb92ff9249e
Phodal Huang <h@phodal.com>
2024-05-30 18:40:34 +0800

    refactor(httpclient): refactor package structure and file names #198
    - Refactored package structure and file names for clarity and consistency.
    - Updated references and imports accordingly.

--- exts/ext-http-client/src/main/kotlin/cc/unitmesh/httpclient/IntellijHttpClientExecutor.kt
+++ exts/ext-http-client/src/main/kotlin/cc/unitmesh/httpclient/IntellijHttpClientExecutor.kt
@@ -1,25 +1,29 @@
-package cc.unitmesh.devti.http
+package cc.unitmesh.httpclient
 
 import cc.unitmesh.devti.provider.http.HttpClientProvider
 import com.intellij.execution.Executor
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.actions.ConfigurationContext
-import com.intellij.httpClient.http.request.HttpRequestPsiFile
 import com.intellij.httpClient.http.request.run.HttpRequestExecutorExtensionFactory
 import com.intellij.httpClient.http.request.run.HttpRequestRunConfigurationExecutor
+import com.intellij.httpClient.http.request.run.config.HttpRequestRunConfiguration
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 
 class IntellijHttpClientExecutor : HttpClientProvider {
     override fun execute(project: Project, virtualFile: VirtualFile, text: String) {
-        val psiFile: HttpRequestPsiFile =
-            PsiManager.getInstance(project).findFile(virtualFile) as? HttpRequestPsiFile ?: return
+        val psiFile: PsiFile = PsiManager.getInstance(project).findFile(virtualFile) ?: return
 
-        val runner: RunnerAndConfigurationSettings = ConfigurationContext(psiFile).configurationsFromContext?.firstOrNull {
-            val configuration = it.configuration as? RunnerAndConfigurationSettings
-            configuration?.configuration?.name == "HttpRequest"
-        }?.configurationSettings ?: return
+        val runner: RunnerAndConfigurationSettings = ConfigurationContext(psiFile)
+            .configurationsFromContext
+            ?.firstOrNull()
+            ?.configurationSettings ?: return
+
+//        runner.configuration?.apply {
+//            this as HttpRequestRunConfiguration
+//        }
 
         val executor: Executor = HttpRequestExecutorExtensionFactory.getRunExtension().executor ?: return
         HttpRequestRunConfigurationExecutor.getInstance().execute(
