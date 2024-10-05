1f41457cb05be3c6fbe839609b8620bc289c6886
Phodal Huang <h@phodal.com>
2024-10-05 13:21:49 +0800

    chore: fix terminal Compatibility issue

--- exts/ext-terminal/src/241/main/kotlin/cc/unitmesh/terminal/TerminalUtil.kt
+++ exts/ext-terminal/src/241/main/kotlin/cc/unitmesh/terminal/TerminalUtil.kt
@@ -2,46 +2,27 @@ package cc.unitmesh.terminal
 
 import cc.unitmesh.terminal.ShellCommandSuggestAction.Companion.suggestCommand
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataProvider
-import com.intellij.openapi.application.runInEdt
-import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.terminal.ui.TerminalWidget
-import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.content.Content
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
-import org.jetbrains.plugins.terminal.exp.TerminalPromptController
 
 object TerminalUtil {
     fun sendMsg(project: Project, data: String, e: AnActionEvent) {
         val content = getContent(project) ?: return
         val findWidgetByContent = TerminalToolWindowManager.findWidgetByContent(content) ?: return
-        val controller: TerminalPromptController? = lookupTerminalPromptControllerByView(findWidgetByContent)
-        if (controller == null) {
-            trySendMsgInOld(project, data, content)
-            return
-        }
-
-        val sb = StringBuilder()
-        suggestCommand(data, project, { string ->
-            sb.append(string)
-            null
-        }, {
-            runInEdt {
-                CopyPasteManager.copyTextToClipboard(sb.toString())
-                controller.performPaste(e.dataContext)
-            }
-        })
+//        val controller = lookupTerminalPromptControllerByView(findWidgetByContent)
+        trySendMsgInOld(project, data, content)
     }
 
-    private fun lookupTerminalPromptControllerByView(findWidgetByContent: TerminalWidget): TerminalPromptController? {
-        val terminalView = (findWidgetByContent.component as? Wrapper)?.targetComponent ?: return null
-        if (terminalView is DataProvider) {
-            val controller = terminalView.getData(TerminalPromptController.KEY.name)
-            return (controller as? TerminalPromptController)
-        }
+    private fun lookupTerminalPromptControllerByView(findWidgetByContent: TerminalWidget): Unit? {
+//        val terminalView = (findWidgetByContent.component as? Wrapper)?.targetComponent ?: return null
+//        if (terminalView is DataProvider) {
+//            val controller = terminalView.getData(TerminalPromptController.KEY.name)
+//            return (controller as? TerminalPromptController)
+//        }
 
         return null
     }
