cc27aac6b13d935ac8180b56c5f671419f8674bf
Phodal Huang <h@phodal.com>
2025-01-11 13:44:05 +0800

    chore(dependencies): comment out unused dependencies and update plugin versions
    - Comment out `cc.unitmesh:cocoa-core` and `cc.unitmesh:document` dependencies as they are no longer used.
    - Update `pythonPlugin` and `goPlugin` versions to the latest available.

--- build.gradle.kts
+++ build.gradle.kts
@@ -434,9 +434,8 @@ project(":core") {
         // follow: https://onnxruntime.ai/docs/get-started/with-java.html
 //        implementation("com.microsoft.onnxruntime:onnxruntime:1.18.0")
 //        implementation("ai.djl.huggingface:tokenizers:0.29.0")
-
-        implementation("cc.unitmesh:cocoa-core:1.0.0")
-        implementation("cc.unitmesh:document:1.0.0")
+//        implementation("cc.unitmesh:cocoa-core:1.0.0")
+//        implementation("cc.unitmesh:document:1.0.0")
 
         // kanban
         implementation("org.kohsuke:github-api:1.314")
