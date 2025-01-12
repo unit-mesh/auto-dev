57c0c78c90ebe85e2aad8532de618bb06ed973be
Phodal Huang <h@phodal.com>
2025-01-12 14:04:27 +0800

    refactor(imports): update ChatRole import path
    Update the import path for `ChatRole` to reflect its new location in the `message` package.

--- core/src/main/kotlin/com/intellij/temporary/gui/block/CodeBlockView.kt
+++ core/src/main/kotlin/com/intellij/temporary/gui/block/CodeBlockView.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.temporary.gui.block
 
-import cc.unitmesh.devti.gui.chat.ChatRole
+import cc.unitmesh.devti.gui.chat.message.ChatRole
 import cc.unitmesh.devti.util.parser.CodeFence
 import com.intellij.lang.Language
 import com.intellij.openapi.Disposable
