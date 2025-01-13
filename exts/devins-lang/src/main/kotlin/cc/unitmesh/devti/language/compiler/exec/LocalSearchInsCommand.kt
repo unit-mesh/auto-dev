6b7844ecd53c3c4bf5d6e69621eaed8b6caa2a54
Phodal Huang <h@phodal.com>
2025-01-13 20:53:39 +0800

    refactor(compiler): simplify search logic and remove unused code #257
    - Remove unused imports and redundant search methods.
    - Simplify `search` function by removing hardcoded overlap value and making it configurable.
    - Improve code readability and maintainability by cleaning up unnecessary

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/LocalSearchInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/LocalSearchInsCommand.kt
@@ -1,15 +1,9 @@
 package cc.unitmesh.devti.language.compiler.exec
 
 import cc.unitmesh.devti.language.utils.canBeAdded
-import com.intellij.find.FindManager
-import com.intellij.lang.Language
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.PsiSearchHelper
-import com.intellij.util.CommonProcessors
 
 
 /**
@@ -26,16 +20,14 @@ import com.intellij.util.CommonProcessors
  *
  */
 class LocalSearchInsCommand(val myProject: Project, private val scope: String, val text: String?) : InsCommand {
-    private val searchScope = GlobalSearchScope.projectScope(myProject)
-
     override suspend fun execute(): String {
-        var text = (text ?: scope).trim()
+        val text = (text ?: scope).trim()
         /// check text length if less then 3 return alert slowly
         if (text.length < 3) {
             throw IllegalArgumentException("Text length should be more than 5")
         }
 
-        val textSearch = search(myProject, text)
+        val textSearch = search(myProject, text, 5)
         return textSearch.map { (file, lines) ->
             val filePath = file.path
             val linesWithContext = lines.joinToString("\n")
@@ -44,15 +36,24 @@ class LocalSearchInsCommand(val myProject: Project, private val scope: String, v
     }
 
     /**
-     * Search in file get before 5 and 5 after text lines
+     * Searches for occurrences of a specified keyword within the content of files in the project.
+     * For each occurrence, it retrieves a specified number of lines before and after the matched line,
+     * providing context around the keyword. The results are grouped by the file in which the keyword was found.
+     *
+     * @param project The project in which to search for the keyword. This is used to access the project's file index.
+     * @param keyword The keyword to search for within the files. The search is case-sensitive.
+     * @param overlap The number of lines to retrieve before and after each matched line. This determines the context size around the keyword.
+     * @return A map where each key is a `VirtualFile` containing the keyword, and the value is a list of strings representing
+     *         the lines of context around the keyword in that file. The context includes the matched line and the specified
+     *         number of lines before and after it.
      */
-    fun search(project: Project, keyword: String): Map<VirtualFile, List<String>> {
+    private fun search(project: Project, keyword: String, overlap: Int): Map<VirtualFile, List<String>> {
         val result = mutableMapOf<VirtualFile, List<String>>()
 
         ProjectFileIndex.getInstance(project).iterateContent { file ->
-            if (!file.canBeAdded() || !ProjectFileIndex.getInstance(project)
-                    .isInContent(file)
-            ) return@iterateContent true
+            if (!file.canBeAdded() || !ProjectFileIndex.getInstance(project).isInContent(file)) {
+                return@iterateContent true
+            }
 
             val content = file.contentsToByteArray().toString(Charsets.UTF_8).lines()
             val matchedIndices = content.withIndex()
@@ -61,8 +62,8 @@ class LocalSearchInsCommand(val myProject: Project, private val scope: String, v
 
             if (matchedIndices.isNotEmpty()) {
                 val linesWithContext = matchedIndices.flatMap { index ->
-                    val start = (index - 5).coerceAtLeast(0)
-                    val end = (index + 5).coerceAtMost(content.size - 1)
+                    val start = (index - overlap).coerceAtLeast(0)
+                    val end = (index + overlap).coerceAtMost(content.size - 1)
                     content.subList(start, end + 1)
                 }.distinct()
 
@@ -71,50 +72,5 @@ class LocalSearchInsCommand(val myProject: Project, private val scope: String, v
             true
         }
         return result
-    }
-
-
-    /**
-     * Provides low-level search and find usages services for a project, like finding references
-     * to an element, finding overriding / inheriting elements, finding to do items and so on.
-     */
-    fun textSearch(project: Project, language: Language, key: String): List<PsiFile> {
-        val searchHelper = PsiSearchHelper.getInstance(project)
-
-        val files: Set<PsiFile> = HashSet()
-        val psiFileProcessor = CommonProcessors.CollectProcessor(files)
-
-        searchHelper.processAllFilesWithWord(key, searchScope, psiFileProcessor, true)
-        println("processAllFilesWithWord: $files")
-        searchHelper.processAllFilesWithWordInText(key, searchScope, psiFileProcessor, true)
-        println("processAllFilesWithWordInText: $files")
-        searchHelper.processAllFilesWithWordInLiterals(key, searchScope, psiFileProcessor)
-        println("processAllFilesWithWordInLiterals: $files")
-
-        return files.toList()
-    }
-
-    /**
-     * [FindManager] Allows to invoke and control Find, Replace and Find Usages operations in files,
-     * Get the 5 lines before keyword lines and 5 lines after keyword lines
-     * And merge all string if had intersect
-     */
-    fun searchInFile(project: Project?, keyword: String?, virtualFile: VirtualFile): String {
-        val findModel = FindManager.getInstance(project).findInFileModel
-        findModel.stringToFind = keyword!!
-        findModel.isCaseSensitive = false
-        findModel.isWholeWordsOnly = false
-
-        val findManager = FindManager.getInstance(project)
-        val findResult = findManager.findString(keyword, 0, findModel, virtualFile)
-
-        return findResult.toString()
-    }
-
-    /**
-     * [com.jetbrains.python.psi.search.PySearchUtilBase] {@link PySearchUtilBase#excludeSdkTestsScope}
-     */
-    fun scope() {
-
     }
 }
