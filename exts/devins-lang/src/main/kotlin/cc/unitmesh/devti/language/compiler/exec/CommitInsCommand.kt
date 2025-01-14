b1a6c96a0007bb136e47f48826362920f75ac464
Phodal Huang <h@phodal.com>
2025-01-14 22:05:35 +0800

    refactor(compiler): remove CodeFence parsing from commands #257
    - Remove unnecessary CodeFence parsing in WriteInsCommand and CommitInsCommand.
    - Simplify CommitInsCommand to directly use commitMsg parameter.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
@@ -1,15 +1,12 @@
 package cc.unitmesh.devti.language.compiler.exec
 
 import cc.unitmesh.devti.language.git.GitUtil
-import cc.unitmesh.devti.util.parser.CodeFence
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.LocalChangeList
 
-class CommitInsCommand(val myProject: Project, val code: String) : InsCommand {
+class CommitInsCommand(val myProject: Project, val commitMsg: String) : InsCommand {
     override suspend fun execute(): String {
-        val commitMsg = CodeFence.parse(code).text
-
         val changeListManager = ChangeListManager.getInstance(myProject)
         changeListManager.changeLists.forEach {
             val list: LocalChangeList = changeListManager.getChangeList(it.id) ?: return@forEach
--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/WriteInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/WriteInsCommand.kt
@@ -22,8 +22,6 @@ class WriteInsCommand(val myProject: Project, val argument: String, val content:
     private val pathSeparator = "/"
 
     override suspend fun execute(): String? {
-        val content = CodeFence.parse(content).text
-
         val range: LineInfo? = LineInfo.fromString(used.text)
         val filepath = argument.split("#")[0]
         val projectDir = myProject.guessProjectDir() ?: return "$DEVINS_ERROR: Project directory not found"
