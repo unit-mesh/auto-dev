ce7314c45182e39bca1217a87febd1db6cdbfab7
Phodal Huang <h@phodal.com>
2025-01-15 20:32:35 +0800

    feat(python): add framework detection logic
    Add logic to detect Python frameworks (Django, Flask, FastAPI) and return context items. Annotate `collect` method with `@RequiresBackgroundThread`.

--- pycharm/src/main/kotlin/cc/unitmesh/python/PythonFrameworkContextProvider.kt
+++ pycharm/src/main/kotlin/cc/unitmesh/python/PythonFrameworkContextProvider.kt
@@ -4,6 +4,7 @@ import cc.unitmesh.devti.provider.context.ChatContextItem
 import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
 import com.intellij.openapi.project.Project
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.jetbrains.python.PythonLanguage
 
 class PythonFrameworkContextProvider : ChatContextProvider {
@@ -12,8 +13,9 @@ class PythonFrameworkContextProvider : ChatContextProvider {
         return creationContext.element?.language is PythonLanguage
     }
 
+    @RequiresBackgroundThread
     override suspend fun collect(project: Project, creationContext: ChatCreationContext): List<ChatContextItem> {
-//        val configures: List<PyPackage> = project.modules.asSequence()
+//        val packages: List<PyPackage> = project.modules.asSequence()
 //            .mapNotNull {
 //                val pair = PyProjectSdkConfigurationExtension.findForModule(it)
 //                pair?.second?.createAndAddSdkForInspection(it)
@@ -23,8 +25,17 @@ class PythonFrameworkContextProvider : ChatContextProvider {
 //            }
 //            .flatten().toList()
 //
-//        configures.forEach {
-//            println(it)
+//        /// check has Django, Flask or FastAPI and return the context
+//        when {
+//            packages.any { it.name == "django" } -> {
+//                return listOf(ChatContextItem(PythonFrameworkContextProvider::class, "This project uses Django"))
+//            }
+//            packages.any { it.name == "flask" } -> {
+//                return listOf(ChatContextItem(PythonFrameworkContextProvider::class, "This project uses Flask"))
+//            }
+//            packages.any { it.name == "fastapi" } -> {
+//                return listOf(ChatContextItem(PythonFrameworkContextProvider::class, "This project uses FastAPI"))
+//            }
 //        }
 
         return listOf()
