d22c4af57cf1dfd6f17af536c308631951cfef0d
Phodal Huang <h@phodal.com>
2025-01-18 22:04:44 +0800

    refactor(database): remove unused imports and methods
    - Removed unused imports related to SQL utilities and context building.
    - Deleted the `collectByElement` method as it was no longer used.

--- exts/ext-database/src/main/kotlin/cc/unitmesh/database/provider/DatabaseSchemaContextProvider.kt
+++ exts/ext-database/src/main/kotlin/cc/unitmesh/database/provider/DatabaseSchemaContextProvider.kt
@@ -1,17 +1,10 @@
 package cc.unitmesh.database.provider
 
-import cc.unitmesh.database.util.SqlContextBuilder
 import cc.unitmesh.devti.provider.context.ChatContextItem
 import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
-import com.intellij.database.util.DasUtil
-import com.intellij.database.util.DbImplUtilCore
 import com.intellij.database.util.DbUtil
 import com.intellij.openapi.project.Project
-import com.intellij.sql.dialects.SqlLanguageDialect
-import com.intellij.sql.psi.SqlFile
-import com.intellij.sql.psi.SqlLanguage
-import com.intellij.sql.psi.impl.SqlImplUtil
 
 class DatabaseSchemaContextProvider : ChatContextProvider {
     override fun isApplicable(project: Project, creationContext: ChatCreationContext): Boolean {
@@ -30,26 +23,6 @@ class DatabaseSchemaContextProvider : ChatContextProvider {
         }.toList()
 
         return infos
-    }
-
-    private fun collectByElement(creationContext: ChatCreationContext): List<ChatContextItem> {
-        val file = creationContext.element?.containingFile as? SqlFile ?: return emptyList()
-        val ds = SqlImplUtil.getDataSources(file).firstOrNull() ?: return emptyList()
-
-        val currentNamespace = SqlContextBuilder.getCurrentNamespace(file) ?: return emptyList()
-        val schema = SqlContextBuilder.getSchema(ds, currentNamespace) ?: return emptyList()
-
-        val displayName: String = file.language.displayName
-        val displayCode: String = creationContext.element?.text ?: ""
-        val schemaDescription: String? = SqlContextBuilder.formatSchema(schema)
-
-        val text = if (schemaDescription == null) {
-            "```$displayName\n\nConsider the following database schema:\n\n$displayCode\n\nNo schema description available\n```"
-        } else {
-            "```$displayName\n\nConsider the following database schema:\n$schemaDescription\n```"
-        }
-
-        return listOf(ChatContextItem(DatabaseSchemaContextProvider::class, text))
     }
 }
 
