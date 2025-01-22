7ec4912c3e020dbafc786a56b86ec097a7b1887e
Phodal Huang <h@phodal.com>
2025-01-22 16:03:59 +0800

    refactor(WebViewWindow): improve browser initialization and handling #265
    - Refactor browser initialization to use `JBCefApp` and `JBCefClient`.
    - Add support for off-screen rendering on Linux.
    - Enable dev tools menu item and add load handler for main frame events.
    - Replace `browser` with `myBrowser` for consistency and clarity.

--- core/src/main/kotlin/cc/unitmesh/devti/agent/view/WebViewWindow.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/agent/view/WebViewWindow.kt
@@ -1,7 +1,14 @@
 package cc.unitmesh.devti.agent.view
 
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.ui.JBColor
+import com.intellij.ui.jcef.JBCefApp
 import com.intellij.ui.jcef.JBCefBrowser
+import com.intellij.ui.jcef.JBCefBrowserBase
+import com.intellij.ui.jcef.JBCefJSQuery
+import org.cef.browser.CefBrowser
+import org.cef.browser.CefFrame
+import org.cef.handler.CefLoadHandlerAdapter
 import java.awt.Component
 
 /**
@@ -12,22 +19,38 @@ import java.awt.Component
  */
 class WebViewWindow {
     // official doc: https://plugins.jetbrains.com/docs/intellij/jcef.html#executing-javascript
-    private val browser: JBCefBrowser
+    private val ourCefClient = JBCefApp.getInstance().createClient()
+    private val myBrowser: JBCefBrowser = try {
+        JBCefBrowser.createBuilder()
+            .setClient(ourCefClient)
+            .setOffScreenRendering(SystemInfo.isLinux)
+            .setEnableOpenDevToolsMenuItem(true)
+            .build()
+    } catch (e: Exception) {
+        JBCefBrowser()
+    }
+    private val myViewerStateJSQuery: JBCefJSQuery = JBCefJSQuery.create(myBrowser as JBCefBrowserBase)
 
     init {
-        browser = try {
-            JBCefBrowser.createBuilder()
-                .build()
-        } catch (e: Exception) {
-            JBCefBrowser()
+        myBrowser.component.background = JBColor.WHITE
+
+        myViewerStateJSQuery.addHandler { s: String ->
+            JBCefJSQuery.Response(null)
         }
 
-        browser.component.background = JBColor.WHITE
+        var myLoadHandler = object : CefLoadHandlerAdapter() {
+            override fun onLoadEnd(browser: CefBrowser, frame: CefFrame, httpStatusCode: Int) {
+                if (frame.isMain) {
+                    // todo add some event maybe ?
+                }
+            }
+        }
+        ourCefClient.addLoadHandler(myLoadHandler, myBrowser.cefBrowser)
     }
 
-    val component: Component = browser.component
+    val component: Component = myBrowser.component
 
     fun loadHtml(html: String) {
-        browser.loadHTML(html)
+        myBrowser.loadHTML(html)
     }
 }
