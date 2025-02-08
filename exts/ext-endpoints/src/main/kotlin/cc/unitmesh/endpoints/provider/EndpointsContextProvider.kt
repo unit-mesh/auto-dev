7c604728416ccd92a64838e90057b95eca6e5e0c
Phodal Huang <h@phodal.com>
2025-02-08 10:45:42 +0800

    feat(endpoints): update provider to use new API and filter available providers #287

--- exts/ext-endpoints/src/main/kotlin/cc/unitmesh/endpoints/provider/EndpointsContextProvider.kt
+++ exts/ext-endpoints/src/main/kotlin/cc/unitmesh/endpoints/provider/EndpointsContextProvider.kt
@@ -3,21 +3,32 @@ package cc.unitmesh.endpoints.provider
 import cc.unitmesh.devti.provider.context.ChatContextItem
 import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
-import com.intellij.microservices.endpoints.EndpointsProjectModel
+import com.intellij.microservices.endpoints.EndpointsProvider
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 
 class EndpointsContextProvider : ChatContextProvider {
     override fun isApplicable(project: Project, creationContext: ChatCreationContext): Boolean {
-        // todo: update to new api
-        return EndpointsProjectModel.EP_NAME.extensionList.isNotEmpty()
+        return runReadAction { EndpointsProvider.getAvailableProviders(project).toList() }.isNotEmpty()
     }
 
     override suspend fun collect(
         project: Project,
         creationContext: ChatCreationContext
     ): List<ChatContextItem> {
-        val model = EndpointsProjectModel.EP_NAME.extensionList.firstOrNull()
-        if (model == null) return emptyList()
+        val model = runReadAction { EndpointsProvider.getAvailableProviders(project).toList() }
+        if (model.isEmpty()) return emptyList()
+
+        val availableProviders = model.filter { it.getStatus(project) == EndpointsProvider.Status.HAS_ENDPOINTS }
+
+        if (availableProviders.isNotEmpty()) {
+            val infos = availableProviders.mapNotNull {
+                val text = "This project has endpoints from ${it.javaClass.simpleName}"
+                return@mapNotNull ChatContextItem(EndpointsContextProvider::class, text)
+            }.toList()
+
+            return infos
+        }
 
         return emptyList()
     }
