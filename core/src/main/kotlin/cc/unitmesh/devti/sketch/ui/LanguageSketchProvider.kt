05728284acbe1a843c6eadf2593605831f88bbdf
Phodal Huang <h@phodal.com>
2025-02-16 20:20:45 +0800

    refactor(sketch): simplify MermaidSketch by extending FileEditorSketch
    Refactored MermaidSketch to extend FileEditorSketch, consolidating common editor functionality and reducing boilerplate code. This change improves maintainability and aligns with the new abstraction.

--- core/src/main/kotlin/cc/unitmesh/devti/sketch/ui/LanguageSketchProvider.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/sketch/ui/LanguageSketchProvider.kt
@@ -1,11 +1,48 @@
 package cc.unitmesh.devti.sketch.ui
 
+import com.intellij.lang.Language
 import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.fileEditor.TextEditorWithPreview
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import javax.swing.JComponent
+import kotlin.jvm.javaClass
 
 
-interface ExtensionLangSketch: LangSketch {
+interface ExtensionLangSketch : LangSketch {
     fun getExtensionName(): String
+}
+
+abstract class FileEditorSketch(val project: Project, val virtualFile: VirtualFile, val editorProviderId: String) :
+    ExtensionLangSketch {
+    val editor = getEditorProvider().createEditor(project, virtualFile) as TextEditorWithPreview
+
+    open val mainPanel = editor.component
+
+    fun getEditorProvider(): FileEditorProvider =
+        FileEditorProvider.EP_FILE_EDITOR_PROVIDER.extensionList.firstOrNull {
+            it.javaClass.simpleName == editorProviderId
+        } ?: TextEditorProvider.getInstance()
+
+    override fun getViewText(): String = virtualFile.readText()
+
+    override fun updateViewText(text: String, complete: Boolean) {
+        val document = editor.editor.document
+        document.setText(text)
+    }
+
+    fun VirtualFile.readText(): String {
+        return VfsUtilCore.loadText(this)
+    }
+
+    override fun getComponent(): JComponent = mainPanel
+
+    override fun updateLanguage(language: Language?, originLanguage: String?) {}
+
+    override fun dispose() {}
 }
 
 interface LanguageSketchProvider {
