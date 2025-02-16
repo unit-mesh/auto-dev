cffc439b3d3f115ec89e17d652e897728c65e063
Phodal Huang <h@phodal.com>
2025-02-16 20:49:55 +0800

    refactor(sketch): simplify OpenAPISketch and FileEditorSketch
    Removed redundant implementations in OpenAPISketch by extending FileEditorSketch. Updated FileEditorSketch to use FileEditor instead of TextEditorWithPreview and added documentation for clarity.

--- core/src/main/kotlin/cc/unitmesh/devti/sketch/ui/LanguageSketchProvider.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/sketch/ui/LanguageSketchProvider.kt
@@ -2,36 +2,42 @@ package cc.unitmesh.devti.sketch.ui
 
 import com.intellij.lang.Language
 import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorProvider
-import com.intellij.openapi.fileEditor.TextEditorWithPreview
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import javax.swing.JComponent
-import kotlin.jvm.javaClass
 
 
 interface ExtensionLangSketch : LangSketch {
     fun getExtensionName(): String
 }
 
-abstract class FileEditorSketch(val project: Project, val virtualFile: VirtualFile, val editorProviderId: String) :
+/**
+ * @param withPreviewEditorId means a editor extends from [com.intellij.openapi.fileEditor.TextEditorWithPreview]
+ */
+abstract class FileEditorSketch(val project: Project, val virtualFile: VirtualFile, val withPreviewEditorId: String) :
     ExtensionLangSketch {
-    val editor = getEditorProvider().createEditor(project, virtualFile) as TextEditorWithPreview
+    val editor: FileEditor = getEditorProvider().createEditor(project, virtualFile)
 
     open val mainPanel = editor.component
 
     fun getEditorProvider(): FileEditorProvider =
         FileEditorProvider.EP_FILE_EDITOR_PROVIDER.extensionList.firstOrNull {
-            it.javaClass.simpleName == editorProviderId
+            it.javaClass.simpleName == withPreviewEditorId
         } ?: TextEditorProvider.getInstance()
 
     override fun getViewText(): String = virtualFile.readText()
 
     override fun updateViewText(text: String, complete: Boolean) {
-        val document = editor.editor.document
-        document.setText(text)
+//        if (editor as? TextEditorWithPreview != null) {
+//            val document = editor.editor.document
+//            document.setText(text)
+//        } else {
+//            // done nothing
+//        }
     }
 
     fun VirtualFile.readText(): String {
