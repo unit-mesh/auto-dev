8ad4cfa2c7e9c3dfb50841a90052ddedee63cb3e
Phodal Huang <h@phodal.com>
2025-02-18 10:58:20 +0800

    refactor(vue): move and rename VueRelatedClassProvider files
    - Deleted test file `VueRelatedClassProviderTest.kt`.
    - Moved and renamed `VueRelatedClassProvider.kt` and `cc.unitmesh.vue.xml` to the `src/233` directory.
    - Updated package and implementation class references accordingly.

--- exts/ext-vue/src/233/main/kotlin/provider/VueRelatedClassProvider.kt
+++ exts/ext-vue/src/233/main/kotlin/provider/VueRelatedClassProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package cc.unitmesh.vue.provider
+package provider
 
 import cc.unitmesh.devti.provider.RelatedClassesProvider
 import com.intellij.javascript.nodejs.PackageJsonData
