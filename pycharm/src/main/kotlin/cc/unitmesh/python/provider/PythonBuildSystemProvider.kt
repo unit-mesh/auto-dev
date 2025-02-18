5ffcfa46a96d44ba462707892349cc59bf3985e2
Phodal Huang <h@phodal.com>
2025-02-18 21:49:34 +0800

    feat(python): add Python build system provider #308

---
+++ pycharm/src/main/kotlin/cc/unitmesh/python/provider/PythonBuildSystemProvider.kt
@@ -0,0 +1,29 @@
+package cc.unitmesh.python.provider
+
+import cc.unitmesh.devti.provider.BuildSystemProvider
+import cc.unitmesh.devti.provider.DevPackage
+import cc.unitmesh.devti.template.context.DockerfileContext
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.packaging.PyRequirement
+import com.jetbrains.python.packaging.PyRequirementParser
+
+class PythonBuildSystemProvider : BuildSystemProvider() {
+    override fun collect(project: Project): DockerfileContext? = null
+
+    override fun collectDependencies(project: Project, psiFile: PsiFile): List<DevPackage> {
+        if (psiFile.language.id != "Python") {
+            return emptyList()
+        }
+
+        val reqs: List<PyRequirement> = PyRequirementParser.fromFile(psiFile.virtualFile)
+
+        return reqs.map {
+            DevPackage(
+                type = "Python",
+                name = it.name,
+                version = it.versionSpecs.firstOrNull()?.version ?: ""
+            )
+        }
+    }
+}
