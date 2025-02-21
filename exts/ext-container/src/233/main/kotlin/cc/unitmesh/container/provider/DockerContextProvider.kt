aee75a51de59cacb5f8cd2d40a90242ec6aed29f
Phodal Huang <h@phodal.com>
2025-02-21 22:56:12 +0800

    chore(docker): integrate Docker support in project context provider #306
    - Add Docker dependency in `autodev-core.xml`.
    - Create `docker.xml` for IDE-specific Docker contributions.
    - Update `DockerContextProvider` to handle Docker file context retrieval and error logging.

--- exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/provider/DockerContextProvider.kt
+++ exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/provider/DockerContextProvider.kt
@@ -4,7 +4,6 @@ import cc.unitmesh.devti.provider.context.ChatContextItem
 import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
 import com.intellij.docker.DockerFileSearch
-import com.intellij.docker.dockerFile.DockerPsiFile
 import com.intellij.docker.dockerFile.parser.psi.DockerFileFromCommand
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
@@ -19,23 +18,25 @@ class DockerContextProvider : ChatContextProvider {
         project: Project,
         creationContext: ChatCreationContext
     ): List<ChatContextItem> {
+
+        val dockerFiles = DockerFileSearch.getInstance().getDockerFiles(project).mapNotNull {
+            PsiManager.getInstance(project).findFile(it)
+        }
+
+        if (dockerFiles.isEmpty()) return emptyList()
+
+        var context = "This project use Docker."
+
         try {
-            val dockerFiles = DockerFileSearch.getInstance().getDockerFiles(project).mapNotNull {
-                PsiManager.getInstance(project).findFile(it) as? DockerPsiFile
-            }
-
-            if (dockerFiles.isEmpty()) return emptyList()
-
             val virtualFile = dockerFiles.firstOrNull()?.virtualFile
-            val defaultContent = "This project use Docker to run in server, path: ${virtualFile?.path}"
-            val defaultContext = listOf(ChatContextItem(DockerContextProvider::class, defaultContent))
+            context = "This project use Docker, path: ${virtualFile?.path}"
 
             val fromCommands = dockerFiles.mapNotNull {
                 PsiTreeUtil.getChildrenOfType(it, DockerFileFromCommand::class.java)?.toList()
             }.flatten()
 
             if (fromCommands.isEmpty()) {
-                return defaultContext
+                return listOf(ChatContextItem(DockerContextProvider::class, context))
             }
 
             val text = "This project use Docker to run in server. Here is related info:\n" +
@@ -44,7 +45,7 @@ class DockerContextProvider : ChatContextProvider {
             return listOf(ChatContextItem(DockerContextProvider::class, text))
         } catch (e: Exception) {
             logger<DockerContextProvider>().warn("Failed to collect Docker context", e)
-            return listOf(ChatContextItem(DockerContextProvider::class, "This project use Docker."))
+            return listOf(ChatContextItem(DockerContextProvider::class, context))
         }
     }
 }
