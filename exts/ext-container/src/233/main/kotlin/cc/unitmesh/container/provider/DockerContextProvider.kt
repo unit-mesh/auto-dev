2649ffd5123845e34d9a6bf02a3fb06d184ed4bb
Phodal Huang <h@phodal.com>
2025-02-21 23:11:09 +0800

    feat(docker): enhance DockerContextProvider with regex and runReadAction #306
    - Add fromRegex to extract 'FROM' commands from Docker files.
    - Use runReadAction to safely access Docker file content.
    - Update context generation logic to handle errors gracefully and improve output.

--- exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/provider/DockerContextProvider.kt
+++ exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/provider/DockerContextProvider.kt
@@ -3,14 +3,18 @@ package cc.unitmesh.container.provider
 import cc.unitmesh.devti.provider.context.ChatContextItem
 import cc.unitmesh.devti.provider.context.ChatContextProvider
 import cc.unitmesh.devti.provider.context.ChatCreationContext
+import cc.unitmesh.devti.sketch.ui.patch.readText
 import com.intellij.docker.DockerFileSearch
 import com.intellij.docker.dockerFile.parser.psi.DockerFileFromCommand
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil
 
 class DockerContextProvider : ChatContextProvider {
+    private val fromRegex = Regex("FROM\\s(.*)")
+
     override fun isApplicable(project: Project, creationContext: ChatCreationContext): Boolean =
         DockerFileSearch.getInstance().getDockerFiles(project).isNotEmpty()
 
@@ -18,7 +22,6 @@ class DockerContextProvider : ChatContextProvider {
         project: Project,
         creationContext: ChatCreationContext
     ): List<ChatContextItem> {
-
         val dockerFiles = DockerFileSearch.getInstance().getDockerFiles(project).mapNotNull {
             PsiManager.getInstance(project).findFile(it)
         }
@@ -27,25 +30,33 @@ class DockerContextProvider : ChatContextProvider {
 
         var context = "This project use Docker."
 
-        try {
-            val virtualFile = dockerFiles.firstOrNull()?.virtualFile
-            context = "This project use Docker, path: ${virtualFile?.path}"
+        val virtualFile = dockerFiles.firstOrNull()?.virtualFile
+            ?: return listOf(ChatContextItem(DockerContextProvider::class, context))
 
+        context = "This project use Docker, path: ${virtualFile.path}"
+
+        var additionalCtx = ""
+        try {
             val fromCommands = dockerFiles.mapNotNull {
                 PsiTreeUtil.getChildrenOfType(it, DockerFileFromCommand::class.java)?.toList()
             }.flatten()
 
-            if (fromCommands.isEmpty()) {
-                return listOf(ChatContextItem(DockerContextProvider::class, context))
-            }
-
-            val text = "This project use Docker to run in server. Here is related info:\n" +
-                    fromCommands.joinToString("\n") { it.text }
-
-            return listOf(ChatContextItem(DockerContextProvider::class, text))
+            if (fromCommands.isEmpty()) return listOf(ChatContextItem(DockerContextProvider::class, context))
+            additionalCtx = fromCommands.joinToString("\n") { it.text }
         } catch (e: Exception) {
             logger<DockerContextProvider>().warn("Failed to collect Docker context", e)
-            return listOf(ChatContextItem(DockerContextProvider::class, context))
+            val fromMatch = fromRegex.find(virtualFile.readText() ?: "")
+
+            if (fromMatch != null) {
+                additionalCtx = fromMatch.groupValues[1]
+            }
+
+            if (additionalCtx.isEmpty()) {
+                return listOf(ChatContextItem(DockerContextProvider::class, context))
+            }
         }
+
+        val text = "This project use Docker to run in server. Here is related info:\n$additionalCtx"
+        return listOf(ChatContextItem(DockerContextProvider::class, text))
     }
 }
