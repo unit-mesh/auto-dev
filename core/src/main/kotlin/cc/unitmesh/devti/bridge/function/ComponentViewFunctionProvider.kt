a8f2284d2113bafdd1434886de9e73d94922bdb8
Phodal Huang <h@phodal.com>
2025-02-26 10:04:36 +0800

    refactor(bridge): move ComponentViewFunctionProvider to function package #319
    Relocated `ComponentViewFunctionProvider` to the `bridge.function` package and updated the corresponding XML configuration to reflect the new location.

--- core/src/main/kotlin/cc/unitmesh/devti/bridge/function/ComponentViewFunctionProvider.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/bridge/function/ComponentViewFunctionProvider.kt
@@ -1,22 +1,18 @@
-package cc.unitmesh.devti.bridge
+package cc.unitmesh.devti.bridge.function
 
+import cc.unitmesh.devti.bridge.ArchViewCommand
 import cc.unitmesh.devti.bridge.provider.UiComponentProvider
+import cc.unitmesh.devti.bridge.tools.UiComponent
 import cc.unitmesh.devti.provider.toolchain.ToolchainFunctionProvider
 import com.intellij.openapi.project.Project
 
 class ComponentViewFunctionProvider : ToolchainFunctionProvider {
-    override fun isApplicable(project: Project, funcName: String): Boolean {
-        return funcName == ArchViewCommand.ComponentView.name
-    }
+    override fun isApplicable(project: Project, funcName: String) = funcName == ArchViewCommand.ComponentView.name
 
     override fun execute(
         project: Project,
         funcName: String,
         args: List<Any>,
         allVariables: Map<String, Any?>
-    ): Any {
-        return UiComponentProvider.collect(project).joinToString("\n") {
-            it.format()
-        }
-    }
+    ): String = UiComponentProvider.Companion.collect(project).joinToString("\n", transform = UiComponent::format)
 }
