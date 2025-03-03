abad8e3018f365d978c7d66d2a2844b1ad726828
Phodal Huang <h@phodal.com>
2025-03-03 23:11:19 +0800

    refactor(ProjectFileUtil): make file search asynchronous
    Replace synchronous file search with an asynchronous task using ProgressManager and CompletableFuture to improve performance and responsiveness.

--- core/src/main/kotlin/cc/unitmesh/devti/util/ProjectFileUtil.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/util/ProjectFileUtil.kt
@@ -3,6 +3,9 @@ package cc.unitmesh.devti.util
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.roots.ProjectFileIndex
@@ -11,9 +14,11 @@ import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.vcs.changes.VcsIgnoreManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.ProjectScope
+import java.util.concurrent.CompletableFuture
 
 // https://github.com/JetBrains/intellij-community/blob/master/platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ProjectFileIndexImpl.java#L32
 fun isInProject(virtualFile: VirtualFile, project: Project): Boolean {
@@ -26,7 +31,19 @@ fun Project.isInProject(virtualFile: VirtualFile): Boolean {
 
 fun Project.findFile(filename: String): VirtualFile? {
     ApplicationManager.getApplication().assertReadAccessAllowed()
-    return FilenameIndex.getVirtualFilesByName(filename, ProjectScope.getProjectScope(this)).firstOrNull()
+    val future = CompletableFuture<VirtualFile?>()
+
+    val task = object : Task.Backgroundable(this, "Searching for file", false) {
+        override fun run(indicator: com.intellij.openapi.progress.ProgressIndicator) {
+            val file  = FilenameIndex.getVirtualFilesByName(filename, ProjectScope.getProjectScope(this@findFile)).firstOrNull()
+            future.complete(file)
+        }
+    }
+
+    ProgressManager.getInstance()
+        .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
+
+    return future.get()
 }
 
 fun Project.findFileByPath(path: String): VirtualFile? {
