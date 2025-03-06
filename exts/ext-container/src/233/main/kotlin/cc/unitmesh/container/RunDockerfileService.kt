b72674a3c1e2b034b1c63f3da3954e253e4f368a
Phodal Huang <h@phodal.com>
2025-03-06 21:03:58 +0800

    refactor(docker): remove unused Docker-related imports and code
    Remove unused DockerCloudConfiguration, DockerCloudType, DockerServerRuntimesManager, and RemoteServerImpl imports and related code in RunDockerfileService. Simplify the service by removing unnecessary Docker connection logic.

--- exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/RunDockerfileService.kt
+++ exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/RunDockerfileService.kt
@@ -1,9 +1,6 @@
 package cc.unitmesh.container
 
 import cc.unitmesh.devti.provider.RunService
-import com.intellij.docker.DockerCloudConfiguration
-import com.intellij.docker.DockerCloudType
-import com.intellij.docker.DockerServerRuntimesManager
 import com.intellij.docker.dockerFile.DockerLanguage
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.openapi.application.ApplicationManager
@@ -12,8 +9,6 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
-import com.intellij.remoteServer.impl.configuration.RemoteServerImpl
-import kotlinx.coroutines.future.await
 import kotlinx.coroutines.runBlocking
 
 class RunDockerfileService : RunService {
@@ -22,25 +17,14 @@ class RunDockerfileService : RunService {
 
     override fun runConfigurationClass(project: Project): Class<out RunProfile>? = null
 
-    fun defaultDockerConnection() =
-        RemoteServerImpl(
-            "DockerConnection",
-            DockerCloudType.getInstance(),
-            DockerCloudConfiguration.Companion.createDefault()
-        )
-
     override fun runFile(project: Project, virtualFile: VirtualFile, psiElement: PsiElement?, isFromToolAction: Boolean)
             : String? {
 
         ApplicationManager.getApplication().invokeAndWait {
             runBlocking {
-                val runtime = DockerServerRuntimesManager.Companion.getInstance(project)
-                    .getOrCreateConnection(defaultDockerConnection())
-                    .await()
 
             }
         }
-
 
         return null
     }
