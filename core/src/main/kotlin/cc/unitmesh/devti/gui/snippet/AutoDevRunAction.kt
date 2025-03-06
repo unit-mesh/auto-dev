3c0560c9c38c83d0bcd5bc2d2e4171af5309e94d
Phodal Huang <h@phodal.com>
2025-03-06 22:42:55 +0800

    feat(run-action): add Dockerfile support and i18n for run action #308
    - Added support for creating Dockerfiles when running scratch files.
    - Integrated i18n for the "Run this file" action text.

--- core/src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevRunAction.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/gui/snippet/AutoDevRunAction.kt
@@ -1,17 +1,25 @@
 package cc.unitmesh.devti.gui.snippet
 
+import cc.unitmesh.devti.AutoDevBundle
 import cc.unitmesh.devti.AutoDevNotifications
 import cc.unitmesh.devti.provider.RunService
 import com.intellij.ide.scratch.ScratchRootType
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.util.NlsSafe
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import java.io.File
+import java.io.IOException
 
-class AutoDevRunAction : DumbAwareAction("Run this file") {
+
+class AutoDevRunAction : DumbAwareAction(AutoDevBundle.message("autodev.run.action")) {
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 
     override fun update(e: AnActionEvent) {
@@ -34,11 +42,15 @@ class AutoDevRunAction : DumbAwareAction("Run this file") {
 
         val document = editor.document
         val file = FileDocumentManager.getInstance().getFile(document) ?: return
-        val psiFile = PsiManager.getInstance(project).findFile(file)
+        var originPsiFile = PsiManager.getInstance(project).findFile(file)
             ?: return
 
-        val scratchFile = ScratchRootType.getInstance()
-            .createScratchFile(project, file.name, psiFile.language, document.text)
+        var scratchFile: VirtualFile? = ScratchRootType.getInstance()
+            .createScratchFile(project, file.name, originPsiFile.language, document.text)
+
+        if (scratchFile?.extension == "Dockerfile") {
+            scratchFile = createDockerFile(project, document.text) ?: scratchFile
+        }
 
         if (scratchFile == null) {
             AutoDevNotifications.warn(project, "Cannot create scratch file")
@@ -49,6 +61,9 @@ class AutoDevRunAction : DumbAwareAction("Run this file") {
             File(scratchFile.path).setExecutable(true)
         }
 
+        var psiFile = PsiManager.getInstance(project).findFile(scratchFile)
+            ?: return
+
         try {
             RunService.provider(project, file)
                 ?.runFile(project, scratchFile, psiFile, isFromToolAction = true)
@@ -56,6 +71,28 @@ class AutoDevRunAction : DumbAwareAction("Run this file") {
                 ?: AutoDevNotifications.notify(project, "No run service found for ${file.name}")
         } catch (e: Exception) {
             AutoDevNotifications.notify(project, "Run Failed: ${e.message}")
+        }
+    }
+
+    private fun createDockerFile(project: Project, text: @NlsSafe String): VirtualFile? {
+        val projectDir = project.guessProjectDir()
+        if (projectDir == null) {
+            return null
+        }
+
+        return runWriteAction {
+            try {
+                // 在项目根目录创建名为 dockerfile 的文件
+                var dockerfile = projectDir.findChild("Dockerfile")
+                if (dockerfile == null) {
+                    dockerfile = projectDir.createChildData(null, "Dockerfile")
+                }
+
+                dockerfile.setBinaryContent(text.toByteArray())
+                return@runWriteAction dockerfile
+            } catch (e: IOException) {
+                return@runWriteAction null
+            }
         }
     }
 }
