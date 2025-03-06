b87552c6a1aca8a756f2fdf8d5cc8f11fbe55e55
Phodal Huang <h@phodal.com>
2025-03-06 13:57:31 +0800

    refactor(compiler): extract file creation logic into separate method
    Extracted the file creation logic from the main method into a new `writeToFile` function to improve code readability and maintainability. Also updated the build command in the template files to use `./gradlew` instead of `/run:build`.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/WriteInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/WriteInsCommand.kt
@@ -6,6 +6,7 @@ import cc.unitmesh.devti.language.compiler.error.DEVINS_ERROR
 import cc.unitmesh.devti.language.compiler.model.LineInfo
 import cc.unitmesh.devti.language.psi.DevInUsed
 import cc.unitmesh.devti.language.utils.lookupFile
+import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.command.WriteCommandAction
@@ -32,45 +33,7 @@ class WriteInsCommand(val myProject: Project, val argument: String, val content:
 
         val virtualFile = runReadAction { myProject.lookupFile(filepath) }
         if (virtualFile == null) {
-            // filepath maybe just a file name, so we need to create parent directory
-            val hasChildPath = filepath.contains(pathSeparator)
-            if (hasChildPath) {
-                val parentPath = filepath.substringBeforeLast(pathSeparator)
-                var parentDir = projectDir.findChild(parentPath)
-                if (parentDir == null) {
-                    // parentDir maybe multiple level, so we need to create all parent directory
-                    val parentDirs = parentPath.split(pathSeparator)
-                    parentDir = projectDir
-                    for (dir in parentDirs) {
-                        if (dir.isEmpty()) continue
-
-                        //check child folder if exist? if not, create it
-                        if (parentDir?.findChild(dir) == null) {
-                            parentDir = runWriteAction { parentDir?.createChildDirectory(this, dir) }
-                        } else {
-                            parentDir = parentDir?.findChild(dir)
-                        }
-                    }
-
-                    if (parentDir == null) {
-                        return "$DEVINS_ERROR: Create Directory failed: $parentPath"
-                    }
-                }
-
-                return runWriteAction {
-                    createNewContent(parentDir, filepath, content)
-                        ?: return@runWriteAction "$DEVINS_ERROR: Create File failed: $argument"
-
-                    return@runWriteAction "Create file: $argument"
-                }
-            } else {
-                return runWriteAction {
-                    createNewContent(projectDir, filepath, content)
-                        ?: return@runWriteAction "$DEVINS_ERROR: Create File failed: $argument"
-
-                    return@runWriteAction "Create file: $argument"
-                }
-            }
+            return writeToFile(filepath, projectDir)
         }
 
         val psiFile = PsiManager.getInstance(myProject).findFile(virtualFile)
@@ -78,6 +41,48 @@ class WriteInsCommand(val myProject: Project, val argument: String, val content:
 
         FileEditorManager.getInstance(myProject).openFile(virtualFile, true)
         return executeInsert(psiFile, range, content)
+    }
+
+    private fun writeToFile(filepath: String, projectDir: VirtualFile): String {
+        // filepath maybe just a file name, so we need to create parent directory
+        val hasChildPath = filepath.contains(pathSeparator)
+        if (hasChildPath) {
+            val parentPath = filepath.substringBeforeLast(pathSeparator)
+            var parentDir = projectDir.findChild(parentPath)
+            if (parentDir == null) {
+                // parentDir maybe multiple level, so we need to create all parent directory
+                val parentDirs = parentPath.split(pathSeparator)
+                parentDir = projectDir
+                for (dir in parentDirs) {
+                    if (dir.isEmpty()) continue
+
+                    //check child folder if exist? if not, create it
+                    if (parentDir?.findChild(dir) == null) {
+                        parentDir = runWriteAction { parentDir?.createChildDirectory(this, dir) }
+                    } else {
+                        parentDir = parentDir?.findChild(dir)
+                    }
+                }
+
+                if (parentDir == null) {
+                    return "$DEVINS_ERROR: Create Directory failed: $parentPath"
+                }
+            }
+
+            return runWriteAction {
+                createNewContent(parentDir, filepath, content)
+                    ?: return@runWriteAction "$DEVINS_ERROR: Create File failed: $argument"
+
+                return@runWriteAction "Create file: $argument"
+            }
+        } else {
+            return runWriteAction {
+                createNewContent(projectDir, filepath, content)
+                    ?: return@runWriteAction "$DEVINS_ERROR: Create File failed: $argument"
+
+                return@runWriteAction "Create file: $argument"
+            }
+        }
     }
 
     private fun createNewContent(parentDir: VirtualFile, filepath: String, content: String): Document? {
