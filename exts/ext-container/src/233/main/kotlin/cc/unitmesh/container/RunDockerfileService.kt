dd0adfc2d3b56e8d27d5c17fa65c1157c5801f5a
Phodal Huang <h@phodal.com>
2025-03-06 21:12:27 +0800

    feat(docker): add Docker connection and runtime support #306
    Introduce `defaultDockerConnection` method and integrate Docker runtime management using `DockerServerRuntimesManager`. This enables handling Docker configurations and connections within the service.

--- exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/RunDockerfileService.kt
+++ exts/ext-container/src/233/main/kotlin/cc/unitmesh/container/RunDockerfileService.kt
@@ -1,6 +1,9 @@
 package cc.unitmesh.container
 
 import cc.unitmesh.devti.provider.RunService
+import com.intellij.docker.DockerCloudConfiguration
+import com.intellij.docker.DockerCloudType
+import com.intellij.docker.DockerServerRuntimesManager
 import com.intellij.docker.dockerFile.DockerLanguage
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.openapi.application.ApplicationManager
@@ -9,22 +12,40 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
+import com.intellij.remoteServer.ServerType
+import com.intellij.remoteServer.configuration.RemoteServer
+import kotlinx.coroutines.future.await
 import kotlinx.coroutines.runBlocking
 
 class RunDockerfileService : RunService {
     override fun isApplicable(project: Project, file: VirtualFile): Boolean = file.name == "Dockerfile" ||
-            file.isValid && runReadAction { PsiManager.getInstance(project).findFile(file)?.language == DockerLanguage.INSTANCE }
+            file.isValid && runReadAction {
+        PsiManager.getInstance(project).findFile(file)?.language == DockerLanguage.INSTANCE
+    }
 
     override fun runConfigurationClass(project: Project): Class<out RunProfile>? = null
+
+    fun defaultDockerConnection() = object : RemoteServer<DockerCloudConfiguration> {
+        override fun getName(): String = "DockerConnection"
+        override fun getType(): ServerType<DockerCloudConfiguration?> = DockerCloudType.getInstance()
+        override fun getConfiguration(): DockerCloudConfiguration = DockerCloudConfiguration.createDefault()
+
+        override fun setName(name: String?) {}
+    }
 
     override fun runFile(project: Project, virtualFile: VirtualFile, psiElement: PsiElement?, isFromToolAction: Boolean)
             : String? {
 
         ApplicationManager.getApplication().invokeAndWait {
             runBlocking {
-
+                val instance = DockerServerRuntimesManager.getInstance(project)
+                val dockerConnection = defaultDockerConnection()
+                val runtime = instance
+                    .getOrCreateConnection(dockerConnection)
+                    .await()
             }
         }
+
 
         return null
     }
