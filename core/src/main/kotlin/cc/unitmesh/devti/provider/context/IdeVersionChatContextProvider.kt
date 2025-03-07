bc7f72424709c3f2e9a77515c346a586eea3c7e0
Phodal Huang <h@phodal.com>
2025-03-07 10:43:11 +0800

    feat(chat): add IDE version context provider
    Introduce IdeVersionChatContextProvider to collect and provide IDE version information as part of the chat context. This enhances context awareness in chat interactions.

---
+++ core/src/main/kotlin/cc/unitmesh/devti/provider/context/IdeVersionChatContextProvider.kt
@@ -0,0 +1,17 @@
+package cc.unitmesh.devti.provider.context
+
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.project.Project
+
+class IdeVersionChatContextProvider : ChatContextProvider {
+    override fun isApplicable(project: Project, creationContext: ChatCreationContext) = true
+
+    override fun collect(project: Project, creationContext: ChatCreationContext): List<ChatContextItem> {
+        val productName = ApplicationNamesInfo.getInstance().fullProductName
+        val ideVersion = ApplicationInfo.getInstance().fullVersion
+        val ideInfo = "Here is current user's IDE: $productName $ideVersion"
+
+        return listOf(ChatContextItem(IdeVersionChatContextProvider::class, ideInfo))
+    }
+}
