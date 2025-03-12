994b7a75c48b20ed8e8c26d3b977b7a7e784c20d
Phodal Huang <h@phodal.com>
2025-03-12 14:32:07 +0800

    refactor(bridge): move context processing to background task
    Refactor `setup` method in `BridgeToolWindow` to run context processing in a background task using `ProgressManager`. This improves UI responsiveness by offloading the work to a background thread.

--- core/src/main/kotlin/cc/unitmesh/devti/bridge/BridgeToolWindow.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/bridge/BridgeToolWindow.kt
@@ -5,6 +5,10 @@ import cc.unitmesh.devti.sketch.SketchInputListener
 import cc.unitmesh.devti.sketch.SketchToolWindow
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
 
 class BridgeToolWindow(val myProject: Project, val myEditor: Editor?, private val showInput: Boolean = false) :
@@ -16,10 +20,17 @@ class BridgeToolWindow(val myProject: Project, val myEditor: Editor?, private va
         override fun getInitPrompt(): String = systemPrompt
 
         override suspend fun setup() {
-            val customContext = BridgeRunContext.create(project, null, "")
-            systemPrompt = templateRender.renderTemplate(template, customContext)
             invokeLater {
-                toolWindow.addSystemPrompt(systemPrompt)
+                val task = object : Task.Backgroundable(project, "Processing context", false) {
+                    override fun run(indicator: ProgressIndicator) {
+                        val customContext = BridgeRunContext.create(project, null, "")
+                        systemPrompt = templateRender.renderTemplate(template, customContext)
+                        toolWindow.addSystemPrompt(systemPrompt)
+                    }
+                }
+
+                ProgressManager.getInstance()
+                    .runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
             }
         }
     }
