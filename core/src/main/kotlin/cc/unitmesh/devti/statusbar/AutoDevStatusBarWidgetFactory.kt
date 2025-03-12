9fae3715ac0f56269f37901a1e5ad3a7eb8ebc21
Phodal Huang <h@phodal.com>
2025-03-12 09:59:58 +0800

    refactor(statusbar): rename AIAssistant to AutoDevAIAssistant and simplify methods

--- core/src/main/kotlin/cc/unitmesh/devti/statusbar/AutoDevStatusBarWidgetFactory.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/statusbar/AutoDevStatusBarWidgetFactory.kt
@@ -9,26 +9,16 @@ import com.intellij.openapi.wm.StatusBarWidgetFactory
 import org.jetbrains.annotations.NonNls
 
 class AutoDevStatusBarWidgetFactory : StatusBarWidgetFactory {
+    override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
 
-    override fun canBeEnabledOn(statusBar: StatusBar): Boolean {
-        return true
-    }
-
-    override fun isAvailable(project: Project): Boolean {
-        return true
-    }
+    override fun isAvailable(project: Project): Boolean = true
 
     override fun getId(): @NonNls String = "AutoDev"
 
-    override fun getDisplayName(): @NlsContexts.ConfigurableName String {
-        return AutoDevBundle.message("autodev.statusbar.name")
-    }
+    override fun getDisplayName(): @NlsContexts.ConfigurableName String =
+        AutoDevBundle.message("autodev.statusbar.name")
 
-    override fun createWidget(project: Project): StatusBarWidget {
-        return AutoDevStatusBarWidget(project)
-    }
+    override fun createWidget(project: Project): StatusBarWidget = AutoDevStatusBarWidget(project)
 
-    override fun disposeWidget(widget: StatusBarWidget) {
-        widget.dispose()
-    }
+    override fun disposeWidget(widget: StatusBarWidget) = widget.dispose()
 }
