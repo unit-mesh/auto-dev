03eaf7cce1ce71e7385a2f9c5ff28ead53f4cf64
Phodal Huang <h@phodal.com>
2025-03-12 16:18:55 +0800

    refactor(observer): extract test result handling logic #259
    - Extracted test result handling logic into a separate `sendResult` method.
    - Removed unused imports and redundant logging.
    - Updated search scope usage to `GlobalSearchScope`.

--- core/src/main/kotlin/cc/unitmesh/devti/observer/TestAgentObserver.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/TestAgentObserver.kt
@@ -1,6 +1,5 @@
 package cc.unitmesh.devti.observer
 
-import cc.unitmesh.devti.gui.AutoDevToolWindowFactory
 import cc.unitmesh.devti.gui.chat.message.ChatActionType
 import cc.unitmesh.devti.gui.sendToChatWindow
 import cc.unitmesh.devti.provider.observer.AgentObserver
@@ -11,54 +10,48 @@ import com.intellij.execution.testframework.sm.runner.SMTestProxy
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.application.runReadAction
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.NlsSafe
-import com.intellij.psi.search.ProjectScope.getProjectScope
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
 import com.intellij.util.messages.MessageBusConnection
 
 class TestAgentObserver : AgentObserver, Disposable {
     private var connection: MessageBusConnection? = null
     override fun onRegister(project: Project) {
         connection = project.messageBus.connect()
-        val searchScope = getProjectScope(project)
         connection?.subscribe(TEST_STATUS, object : SMTRunnerEventsAdapter() {
-            override fun onSuiteFinished(suite: SMTestProxy, nodeId: String?) {
-                logger<AutoDevToolWindowFactory>().info(suite.toString())
-            }
-
             override fun onTestFailed(test: SMTestProxy) {
-                val sourceCode = test.getLocation(project, searchScope)
-                runInEdt {
-                    sendToChatWindow(project, ChatActionType.CHAT) { contentPanel, _ ->
-                        val psiElement = sourceCode?.psiElement
-                        val language = psiElement?.language?.displayName ?: ""
-                        val filepath = psiElement?.containingFile?.virtualFile?.relativePath(project) ?: ""
-                        val code = runReadAction<@NlsSafe String> { psiElement?.text ?: "" }
-                        contentPanel.setInput(
-                            """Help me fix follow test issue:
-                           | ErrorMessage:
-                           |```
-                           |${test.errorMessage}
-                           |```
-                           |stacktrace details: 
-                           |${test.stacktrace}
-                           |
-                           |// filepath: $filepath
-                           |origin code:
-                           |```$language
-                           |$code
-                           |```
-                           |""".trimMargin()
-                        )
-                    }
-                }
-            }
-
-            override fun onTestFinished(test: SMTestProxy, nodeId: String?) {
-                logger<TestAgentObserver>().info(nodeId)
+                sendResult(test, project, ProjectScope.getProjectScope(project))
             }
         })
+    }
+
+    private fun sendResult(test: SMTestProxy, project: Project, searchScope: GlobalSearchScope) {
+        val sourceCode = test.getLocation(project, searchScope)
+        runInEdt {
+            sendToChatWindow(project, ChatActionType.CHAT) { contentPanel, _ ->
+                val psiElement = sourceCode?.psiElement
+                val language = psiElement?.language?.displayName ?: ""
+                val filepath = psiElement?.containingFile?.virtualFile?.relativePath(project) ?: ""
+                val code = runReadAction<String> { psiElement?.text ?: "" }
+                contentPanel.setInput(
+                    """Help me fix follow test issue:
+                               | ErrorMessage:
+                               |```
+                               |${test.errorMessage}
+                               |```
+                               |stacktrace details: 
+                               |${test.stacktrace}
+                               |
+                               |// filepath: $filepath
+                               |origin code:
+                               |```$language
+                               |$code
+                               |```
+                               |""".trimMargin()
+                )
+            }
+        }
     }
 
     override fun dispose() {
