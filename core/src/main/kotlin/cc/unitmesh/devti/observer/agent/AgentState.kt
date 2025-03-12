33cea27278861c440a9630a9df87028bd387dc85
Phodal Huang <h@phodal.com>
2025-03-12 18:02:15 +0800

    feat(observer): add AgentState and AgentStateService #259
    Introduce AgentState data class to manage agent state, including conversation ID, change list, messages, and used tools. Add AgentStateService as a singleton service to hold the current state

---
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentState.kt
@@ -0,0 +1,43 @@
+package cc.unitmesh.devti.observer.agent
+
+import cc.unitmesh.devti.agent.tool.AgentTool
+import cc.unitmesh.devti.llms.custom.Message
+import com.intellij.openapi.components.Service
+import com.intellij.util.diff.Diff.Change
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import java.util.UUID
+
+@Serializable
+data class AgentState(
+    val conversationId: String = UUID.randomUUID().toString(),
+    @Contextual
+    val changeList: List<Change> = emptyList(),
+
+    val messages: List<Message> = emptyList(),
+
+    val usedTools: List<AgentTool> = emptyList(),
+)
+
+
+@Service
+class AgentStateService {
+    var state: AgentState = AgentState()
+
+    /**
+     * Call some LLM to compress it or use some other method to compress the history
+     */
+    fun compressHistory(messages: List<Message>): List<Message> {
+        return messages
+    }
+}
+
+interface AgentProcessor {
+    fun process()
+}
+
+class HistoryMessageProcessor: AgentProcessor {
+    override fun process() {
+        TODO("Not yet implemented")
+    }
+}
