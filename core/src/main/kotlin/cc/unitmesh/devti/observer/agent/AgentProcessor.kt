64199c9a532ad0157c12a509fd4d35fd21cd52d5
Phodal Huang <h@phodal.com>
2025-03-13 14:29:26 +0800

    refactor(agent): move AgentStateService to separate file #259
    Extracted AgentStateService from AgentState.kt into its own file for better code organization.
    Also moved AgentProcessor interface and HistoryMessageProcessor class to a new file, AgentProcessor.kt.

---
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentProcessor.kt
@@ -0,0 +1,11 @@
+package cc.unitmesh.devti.observer.agent
+
+interface AgentProcessor {
+    fun process()
+}
+
+class HistoryMessageProcessor : AgentProcessor {
+    override fun process() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentStateService.kt
@@ -0,0 +1,40 @@
+package cc.unitmesh.devti.observer.agent
+
+import cc.unitmesh.devti.agent.tool.AgentTool
+import cc.unitmesh.devti.devin.dataprovider.BuiltinCommand
+import cc.unitmesh.devti.llms.custom.Message
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.logger
+
+@Service(Service.Level.PROJECT)
+class AgentStateService {
+    var state: AgentState = AgentState()
+
+    fun resetState() {
+        state = AgentState()
+    }
+
+    fun addTools(tools: List<BuiltinCommand>) {
+        state.usedTools = tools.map {
+            AgentTool(it.commandName, it.description, "")
+        }
+
+        logger<AgentStateService>().info("Called agent tools:\n ${state.usedTools.joinToString("\n")}")
+    }
+
+    fun addChanges(fileName: String) {
+        // todo changeList.add()
+    }
+
+    fun resetMessages() {
+        state.messages = emptyList()
+    }
+
+    /**
+     * Call some LLM to compress it or use some other method to compress the history
+     */
+    fun preprocessMessages(messages: List<Message>): List<Message> {
+        state.messages = messages
+        return messages
+    }
+}
