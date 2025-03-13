590dbf45dfcceb2ff9eef4a72b92c0d9910137ef
Phodal Huang <h@phodal.com>
2025-03-13 23:17:17 +0800

    refactor(observer): replace ChangeListListener with ChangeListAdapter #259
    - Updated ChangeListObserver to use ChangeListAdapter instead of ChangeListListener for better handling of change list events.
    - Re-enabled ChangeListObserver in autodev-core.xml configuration.

--- core/src/main/kotlin/cc/unitmesh/devti/observer/ChangeListObserver.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/ChangeListObserver.kt
@@ -3,18 +3,26 @@ package cc.unitmesh.devti.observer
 import cc.unitmesh.devti.provider.observer.AgentObserver
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.changes.ChangeListListener
+import com.intellij.openapi.vcs.changes.ChangeList
+import com.intellij.openapi.vcs.changes.ChangeListAdapter
+import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.util.messages.MessageBusConnection
 
 class ChangeListObserver : AgentObserver, Disposable {
     private var connection: MessageBusConnection? = null
 
     override fun onRegister(project: Project) {
-        connection = project.messageBus.connect()
-        connection?.subscribe(ChangeListListener.TOPIC, object : ChangeListListener {
-
-        })
+        ChangeListManager.getInstance(project).addChangeListListener(object : ChangeListAdapter() {
+            override fun defaultListChanged(
+                oldDefaultList: ChangeList?,
+                newDefaultList: ChangeList?,
+                automatic: Boolean
+            ) {
+                super.defaultListChanged(oldDefaultList, newDefaultList, automatic)
+            }
+        }, this)
     }
+
 
     override fun dispose() {
         connection?.disconnect()
