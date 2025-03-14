821d4e31d65f268e20625a07c5058adccfd26a47
Phodal Huang <h@phodal.com>
2025-03-14 10:25:59 +0800

    feat(agent): enhance AgentState with additional properties
    - Add originIntention: to store the user's initial question- Add environment: to log environment variables, potentially related to ChatContextProvider

--- core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentState.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentState.kt
@@ -6,9 +6,22 @@ import com.intellij.util.diff.Diff.Change
 import java.util.UUID
 
 data class AgentState(
+    /**
+     * First question of user
+     */
+    var originIntention: String = "",
+
     var conversationId: String = UUID.randomUUID().toString(),
+
     var changeList: List<Change> = emptyList(),
+
     var messages: List<Message> = emptyList(),
+
     var usedTools: List<AgentTool> = emptyList(),
+
+    /**
+     * Logging environment variables, maybe related to  [cc.unitmesh.devti.provider.context.ChatContextProvider]
+     */
+    var environment: Map<String, String> = emptyMap()
 )
 
