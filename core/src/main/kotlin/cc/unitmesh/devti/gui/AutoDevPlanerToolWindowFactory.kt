e33c82b7942e62c45c994d289fa59ce4868f7982
Phodal Huang <h@phodal.com>
2025-03-16 17:27:54 +0800

    feat(toolWindow): add AutoDevPlaner tool window #223
    Add a new tool window `AutoDevPlaner` with left anchor, positioned after the Structure window. Includes a factory class `AutoDevPlanerToolWindowFactory` to manage the window's content and state.

---
+++ core/src/main/kotlin/cc/unitmesh/devti/gui/AutoDevPlanerToolWindowFactory.kt
@@ -0,0 +1,57 @@
+package cc.unitmesh.devti.gui
+
+import cc.unitmesh.devti.observer.plan.PlanBoard
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.SimpleToolWindowPanel
+import com.intellij.openapi.ui.Splittable
+import com.intellij.openapi.util.NlsActions
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener
+import java.util.concurrent.atomic.AtomicBoolean
+
+class AutoDevPlanerToolWindowFactory : ToolWindowFactory, ToolWindowManagerListener, DumbAware {
+    private val orientation = AtomicBoolean(true)
+
+    override fun createToolWindowContent(
+        project: Project,
+        toolWindow: ToolWindow
+    ) {
+        val panel = AutoDevPlanerTooWindow(project)
+        val manager = toolWindow.contentManager
+        manager.addContent(manager.factory.createContent(panel, null, false).apply { isCloseable = false })
+        project.messageBus.connect(manager).subscribe(ToolWindowManagerListener.TOPIC, this)
+    }
+
+    override fun stateChanged(manager: ToolWindowManager) {
+        val window = manager.getToolWindow(PlANNER_ID) ?: return
+        if (window.isDisposed) return
+        val vertical = !window.anchor.isHorizontal
+        if (vertical != orientation.getAndSet(vertical)) {
+            for (content in window.contentManager.contents) {
+                val splittable = content?.component as? Splittable
+                splittable?.orientation = vertical
+            }
+        }
+    }
+
+    companion object {
+        private val PlANNER_ID = "AutoDevPlaner"
+    }
+}
+
+class AutoDevPlanerTooWindow(val project: Project) : SimpleToolWindowPanel(true, true), Disposable {
+    override fun getName(): @NlsActions.ActionText String? = "AutoDev Planer"
+    val planBoard: PlanBoard = project.getService(PlanBoard::class.java)
+
+    init {
+        add(planBoard.planSketch)
+    }
+
+    override fun dispose() {
+
+    }
+}
