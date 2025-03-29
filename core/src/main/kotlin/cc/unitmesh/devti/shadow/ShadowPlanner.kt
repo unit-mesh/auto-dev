932850c81f8570548244ac081a100e46b4067a5e
Phodal Huang <h@phodal.com>
2025-03-29 14:27:00 +0800

    feat(core): add ShadowPlanner for generating development plans #352
    - Implement ShadowPlanner object with methods for enhancing prompts and generating plans
    - Add functionality to collect system prompts using sketch templates
    - Integrate with LlmFactory for language model generation
    - Utilize AutoDevCoroutineScope for asynchronous plan generation

---
+++ core/src/main/kotlin/cc/unitmesh/devti/shadow/ShadowPlanner.kt
@@ -0,0 +1,54 @@
+package cc.unitmesh.devti.shadow
+
+import cc.unitmesh.devti.llms.LlmFactory
+import cc.unitmesh.devti.llms.cancelHandler
+import cc.unitmesh.devti.provider.devins.LanguageProcessor
+import cc.unitmesh.devti.sketch.SketchRunContext
+import cc.unitmesh.devti.template.GENIUS_CODE
+import cc.unitmesh.devti.template.TemplateRender
+import cc.unitmesh.devti.util.AutoDevCoroutineScope
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.launch
+
+object ShadowPlanner {
+    /**
+     * Call llm to make prompt better
+     */
+    fun enhancePrompt(prompt: String): String {
+        return prompt.replace("{{", "{{{").replace("}}", "}}}")
+    }
+
+    suspend fun generatePlan(project: Project, input: String, handleCancel: ((String) -> Unit)?) {
+        var llmProvider = LlmFactory.create(project)
+
+        AutoDevCoroutineScope.workerScope(project).launch {
+            val devInProcessor = LanguageProcessor.devin()
+            val compiledInput = devInProcessor?.compile(project, input) ?: input
+
+            val input = compiledInput.toString().trim()
+            if (input.isEmpty()) {
+                return@launch
+            }
+
+            val systemPrompt = collectSystemPrompt(project)
+            val flow = llmProvider.stream(input, systemPrompt, keepHistory = true, true)
+            val suggestion = StringBuilder()
+
+            flow.cancelHandler { handleCancel }.cancellable().collect { char ->
+                suggestion.append(char)
+            }
+        }
+    }
+
+    /**
+     * call sketch and can also can plan model to directly generate code
+     */
+    suspend fun collectSystemPrompt(project: Project): String {
+        val templateRender = TemplateRender(GENIUS_CODE)
+        val template = templateRender.getTemplate("sketch.vm")
+        val customContext = SketchRunContext.create(project, null, "")
+        var systemPrompt = templateRender.renderTemplate(template, customContext)
+        return systemPrompt
+    }
+}
