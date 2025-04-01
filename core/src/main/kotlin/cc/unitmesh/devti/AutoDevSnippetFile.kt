fb289aba110ecac820d18980491e00701b799496
Phodal Huang <h@phodal.com>
2025-04-01 13:40:35 +0800

    refactor(devti): remove unused AutoDevUnloadListener and update related files - Remove AutoDevUnloadListener class - Remove applicationListeners from autodev-core.xml - Update AutoDevSnippetFile object

--- core/src/main/kotlin/cc/unitmesh/devti/AutoDevSnippetFile.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/AutoDevSnippetFile.kt
@@ -3,17 +3,14 @@ package cc.unitmesh.devti
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightVirtualFile
 
-const val AUTODEV_SNIPPET_NAME = "autodev-snippet-"
-
 object AutoDevSnippetFile {
+    const val AUTODEV_SNIPPET_NAME = "autodev-snippet-"
+
     fun isSnippet(file: VirtualFile): Boolean {
         if (file !is LightVirtualFile) return false
         return file.name.startsWith("autodev-snippet-") || file.name.startsWith("DevIn-") || file.name.startsWith("devIn-")
     }
 
-    /**
-     * extension should not contain dot
-     */
     fun naming(extension: String): String {
         val time = System.currentTimeMillis()
         return "$AUTODEV_SNIPPET_NAME$time.$extension"
