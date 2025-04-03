cbb6147e2b76fc9dbaf4525be8662633fce7b96f
Phodal Huang <h@phodal.com>
2025-04-03 17:15:54 +0800

    refactor(gui): improve file layout in WorkspacePanel #358
    - Replace FlowLayout with custom WrapLayout to support line wrapping- Adjust panel layout to display files at the top
    - Remove unused file reference addition to input

--- core/src/main/kotlin/cc/unitmesh/devti/gui/chat/ui/WorkspacePanel.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/gui/chat/ui/WorkspacePanel.kt
@@ -8,8 +8,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.JBUI
-import java.awt.BorderLayout
-import java.awt.FlowLayout
+import java.awt.*
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.BorderFactory
@@ -20,13 +19,13 @@ class WorkspacePanel(
     private val input: AutoDevInput
 ) : JPanel(BorderLayout()) {
     private val workspaceFiles = mutableListOf<VirtualFile>()
-    private val filesPanel = JPanel(FlowLayout(FlowLayout.LEFT, 2, 2))
+    private val filesPanel = JPanel(WrapLayout(FlowLayout.LEFT, 2, 2))
     
     init {
         border = JBUI.Borders.empty()
 
         val addButton = JBLabel(AllIcons.General.Add)
-        addButton.cursor = java.awt.Cursor(java.awt.Cursor.HAND_CURSOR)
+        addButton.cursor = Cursor(Cursor.HAND_CURSOR)
         addButton.toolTipText = "Add files to workspace"
         addButton.border = JBUI.Borders.empty(2, 4)
         addButton.background = JBColor(0xEDF4FE, 0x313741)
@@ -40,7 +39,7 @@ class WorkspacePanel(
         filesPanel.isOpaque = false
         filesPanel.add(addButton)
         
-        add(filesPanel, BorderLayout.CENTER)
+        add(filesPanel, BorderLayout.NORTH)
         isOpaque = false
     }
     
@@ -61,7 +60,6 @@ class WorkspacePanel(
             workspaceFiles.add(file)
             updateFilesPanel()
 
-            // Add file reference to the input (will be visible on submit)
             val relativePath = try {
                 project.basePath?.let { basePath ->
                     file.path.substringAfter(basePath).removePrefix("/")
@@ -78,7 +76,7 @@ class WorkspacePanel(
         filesPanel.removeAll()
         
         val addButton = JBLabel(AllIcons.General.Add)
-        addButton.cursor = java.awt.Cursor(java.awt.Cursor.HAND_CURSOR)
+        addButton.cursor = Cursor(Cursor.HAND_CURSOR)
         addButton.toolTipText = "Add files to workspace"
         addButton.border = JBUI.Borders.empty(2, 4)
         addButton.background = JBColor(0xEDF4FE, 0x313741)
@@ -129,7 +127,7 @@ class FileItemPanel(
         val fileLabel = JBLabel(file.name, icon, JBLabel.LEFT)
         
         val removeLabel = JBLabel(AllIcons.Actions.Close)
-        removeLabel.cursor = java.awt.Cursor(java.awt.Cursor.HAND_CURSOR)
+        removeLabel.cursor = Cursor(Cursor.HAND_CURSOR)
         removeLabel.addMouseListener(object : MouseAdapter() {
             override fun mouseClicked(e: MouseEvent) {
                 onRemove()
@@ -143,3 +141,89 @@ class FileItemPanel(
     }
 }
 
+/**
+ * FlowLayout subclass that fully supports wrapping of components.
+ */
+class WrapLayout : FlowLayout {
+    constructor() : super()
+    constructor(align: Int) : super(align)
+    constructor(align: Int, hgap: Int, vgap: Int) : super(align, hgap, vgap)
+
+    /**
+     * Returns the preferred dimensions for this layout given the components
+     * in the specified target container.
+     * @param target the container that needs to be laid out
+     * @return the preferred dimensions to lay out the subcomponents of the specified container
+     */
+    override fun preferredLayoutSize(target: Container): Dimension {
+        return layoutSize(target, true)
+    }
+
+    /**
+     * Returns the minimum dimensions needed to layout the components
+     * contained in the specified target container.
+     * @param target the container that needs to be laid out
+     * @return the minimum dimensions to lay out the subcomponents of the specified container
+     */
+    override fun minimumLayoutSize(target: Container): Dimension {
+        return layoutSize(target, false)
+    }
+
+    /**
+     * Calculate the dimensions needed to layout the components in the target container
+     * @param target the target container
+     * @param preferred true for preferred size, false for minimum size
+     * @return the dimensions needed for layout
+     */
+    private fun layoutSize(target: Container, preferred: Boolean): Dimension {
+        synchronized(target.treeLock) {
+            // Each row must fit within the target container width
+            var targetWidth = target.width
+            
+            if (targetWidth == 0) {
+                targetWidth = Int.MAX_VALUE
+            }
+
+            val hgap = this.hgap
+            val vgap = this.vgap
+            val insets = target.insets
+            val horizontalInsetsAndGap = insets.left + insets.right + (hgap * 2)
+            val maxWidth = targetWidth - horizontalInsetsAndGap
+
+            // Fit components into the calculated width
+            var dim = Dimension(0, 0)
+            var rowWidth = 0
+            var rowHeight = 0
+
+            val count = target.componentCount
+            for (i in 0 until count) {
+                val m = target.getComponent(i)
+                if (m.isVisible) {
+                    val d = if (preferred) m.preferredSize else m.minimumSize
+                    
+                    // If this component doesn't fit in the current row, start a new row
+                    if (rowWidth + d.width > maxWidth && rowWidth > 0) {
+                        dim.width = maxWidth.coerceAtLeast(rowWidth)
+                        dim.height += rowHeight + vgap
+                        rowWidth = 0
+                        rowHeight = 0
+                    }
+
+                    // Add component to current row
+                    rowWidth += d.width + hgap
+                    rowHeight = rowHeight.coerceAtLeast(d.height)
+                }
+            }
+
+            // Add last row dimensions
+            dim.width = maxWidth.coerceAtLeast(rowWidth)
+            dim.height += rowHeight + vgap
+
+            // Account for container's insets
+            dim.width += horizontalInsetsAndGap
+            dim.height += insets.top + insets.bottom + vgap * 2
+
+            return dim
+        }
+    }
+}
