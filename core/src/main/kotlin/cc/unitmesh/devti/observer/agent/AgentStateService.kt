e19324fcd365313075b7965bd244fb3bfd0f833e
Phodal Huang <h@phodal.com>
2025-04-03 10:06:40 +0800

    refactor(devti): extract patch conversion logic into separate utility
    - Move createChange and getAbsolutePath functions to PatchConverter object
    - Update import statements and remove unnecessary code from AgentStateService
    - Improve code organization and readability by separating concerns

--- core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentStateService.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/observer/agent/AgentStateService.kt
@@ -8,28 +8,13 @@ import cc.unitmesh.devti.observer.plan.AgentTaskEntry
 import cc.unitmesh.devti.observer.plan.MarkdownPlanParser
 import cc.unitmesh.devti.observer.plan.PlanUpdateListener
 import cc.unitmesh.devti.settings.AutoDevSettingsState
+import cc.unitmesh.devti.util.PatchConverter
 import cc.unitmesh.devti.util.parser.MarkdownCodeHelper
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diff.impl.patch.TextFilePatch
-import com.intellij.openapi.diff.impl.patch.apply.GenericPatchApplier
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.ThrowableComputable
-import com.intellij.openapi.vcs.FilePath
-import com.intellij.openapi.vcs.FileStatus
-import com.intellij.openapi.vcs.VcsBundle
-import com.intellij.openapi.vcs.VcsException
-import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.ContentRevision
-import com.intellij.openapi.vcs.changes.CurrentContentRevision
-import com.intellij.openapi.vcs.changes.TextRevisionNumber
-import com.intellij.openapi.vcs.history.VcsRevisionNumber
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.vcsUtil.VcsUtil
-import org.jetbrains.annotations.NonNls
 import java.io.File
 import java.io.IOException
 
@@ -52,7 +37,7 @@ class AgentStateService(val project: Project) {
     fun addToChange(patch: TextFilePatch) {
         val baseDir = File(project.basePath!!)
         if (patch.afterName != null) {
-            val newChangePath = getAbsolutePath(baseDir, patch.afterName).canonicalPath
+            val newChangePath = PatchConverter.getAbsolutePath(baseDir, patch.afterName).canonicalPath
             state.changes.removeIf {
                 val afterRevision = it.afterRevision
                 afterRevision != null && File(afterRevision.file.path).canonicalPath == newChangePath
@@ -60,7 +45,7 @@ class AgentStateService(val project: Project) {
         }
 
         try {
-            val change = createChange(patch)
+            val change = PatchConverter.createChange(project, patch)
             state.changes.add(change)
 
             ApplicationManager.getApplication().messageBus
@@ -69,92 +54,6 @@ class AgentStateService(val project: Project) {
         } catch (e: IOException) {
             logger<AgentStateService>().info(e)
         }
-    }
-
-    private fun createChange(patch: TextFilePatch): Change {
-        val baseDir = File(project.basePath!!)
-        val beforePath = patch.beforeName
-        val afterPath = patch.afterName
-
-        val fileStatus = when {
-            patch.isNewFile -> {
-                FileStatus.ADDED
-            }
-            patch.isDeletedFile -> {
-                FileStatus.DELETED
-            }
-            else -> {
-                FileStatus.MODIFIED
-            }
-        }
-
-        val beforeFilePath = VcsUtil.getFilePath(getAbsolutePath(baseDir, beforePath), false)
-        val afterFilePath = VcsUtil.getFilePath(getAbsolutePath(baseDir, afterPath), false)
-
-        var beforeRevision: ContentRevision? = null
-        if (fileStatus !== FileStatus.ADDED) {
-            beforeRevision = object : CurrentContentRevision(beforeFilePath) {
-                override fun getRevisionNumber(): VcsRevisionNumber {
-                    return TextRevisionNumber(VcsBundle.message("local.version.title"))
-                }
-            }
-        }
-
-        var afterRevision: ContentRevision? = null
-        if (fileStatus !== FileStatus.DELETED) {
-            afterRevision = object : CurrentContentRevision(beforeFilePath) {
-                override fun getRevisionNumber(): VcsRevisionNumber =
-                    TextRevisionNumber(VcsBundle.message("local.version.title"))
-
-                override fun getVirtualFile(): VirtualFile? = afterFilePath.virtualFile
-                override fun getFile(): FilePath = afterFilePath
-                override fun getContent(): @NonNls String? {
-                    when {
-                        patch.isNewFile -> {
-                            return patch.singleHunkPatchText
-                        }
-                        patch.isDeletedFile -> {
-                            return null
-                        }
-                        else -> {
-                            val localContent: String = loadLocalContent()
-                            val appliedPatch = GenericPatchApplier.apply(localContent, patch.hunks)
-                            /// sometimes llm will return a wrong patch which the content is not correct
-                            if (appliedPatch != null) {
-                                return appliedPatch.patchedText
-                            }
-
-                            return patch.singleHunkPatchText
-                        }
-                    }
-                }
-
-                @Throws(VcsException::class)
-                private fun loadLocalContent(): String {
-                    return ReadAction.compute<String?, VcsException?>(ThrowableComputable {
-                        val file: VirtualFile? = beforeFilePath.virtualFile
-                        if (file == null) return@ThrowableComputable null
-                        val doc = FileDocumentManager.getInstance().getDocument(file)
-                        if (doc == null) return@ThrowableComputable null
-                        doc.text
-                    })
-                }
-            }
-        }
-
-        return Change(beforeRevision, afterRevision, fileStatus)
-    }
-
-    private fun getAbsolutePath(baseDir: File, relativePath: String): File {
-        var file: File?
-        try {
-            file = File(baseDir, relativePath).getCanonicalFile()
-        } catch (e: IOException) {
-            logger<AgentStateService>().info(e)
-            file = File(baseDir, relativePath)
-        }
-
-        return file
     }
 
     fun buildOriginIntention(): String? {
---
+++ core/src/main/kotlin/cc/unitmesh/devti/util/PatchConverter.kt
@@ -0,0 +1,117 @@
+package cc.unitmesh.devti.util
+
+import cc.unitmesh.devti.observer.agent.AgentStateService
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diff.impl.patch.TextFilePatch
+import com.intellij.openapi.diff.impl.patch.apply.GenericPatchApplier
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.FileStatus
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ContentRevision
+import com.intellij.openapi.vcs.changes.CurrentContentRevision
+import com.intellij.openapi.vcs.changes.TextRevisionNumber
+import com.intellij.openapi.vcs.history.VcsRevisionNumber
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcsUtil.VcsUtil
+import org.jetbrains.annotations.NonNls
+import java.io.File
+import java.io.IOException
+
+object PatchConverter {
+    fun getAbsolutePath(baseDir: File, relativePath: String): File {
+        var file: File?
+        try {
+            file = File(baseDir, relativePath).getCanonicalFile()
+        } catch (e: IOException) {
+            logger<PatchConverter>().info(e)
+            file = File(baseDir, relativePath)
+        }
+
+        return file
+    }
+
+    fun createChange(project: Project, patch: TextFilePatch): Change {
+        val baseDir = File(project.basePath!!)
+        val beforePath = patch.beforeName
+        val afterPath = patch.afterName
+
+        val fileStatus = when {
+            patch.isNewFile -> {
+                FileStatus.ADDED
+            }
+
+            patch.isDeletedFile -> {
+                FileStatus.DELETED
+            }
+
+            else -> {
+                FileStatus.MODIFIED
+            }
+        }
+
+        val beforeFilePath = VcsUtil.getFilePath(getAbsolutePath(baseDir, beforePath), false)
+        val afterFilePath = VcsUtil.getFilePath(getAbsolutePath(baseDir, afterPath), false)
+
+        var beforeRevision: ContentRevision? = null
+        if (fileStatus !== FileStatus.ADDED) {
+            beforeRevision = object : CurrentContentRevision(beforeFilePath) {
+                override fun getRevisionNumber(): VcsRevisionNumber {
+                    return TextRevisionNumber(VcsBundle.message("local.version.title"))
+                }
+            }
+        }
+
+        var afterRevision: ContentRevision? = null
+        if (fileStatus !== FileStatus.DELETED) {
+            afterRevision = object : CurrentContentRevision(beforeFilePath) {
+                override fun getRevisionNumber(): VcsRevisionNumber =
+                    TextRevisionNumber(VcsBundle.message("local.version.title"))
+
+                override fun getVirtualFile(): VirtualFile? = afterFilePath.virtualFile
+                override fun getFile(): FilePath = afterFilePath
+                override fun getContent(): @NonNls String? {
+                    when {
+                        patch.isNewFile -> {
+                            return patch.singleHunkPatchText
+                        }
+
+                        patch.isDeletedFile -> {
+                            return null
+                        }
+
+                        else -> {
+                            val localContent: String = loadLocalContent()
+                            val appliedPatch = GenericPatchApplier.apply(localContent, patch.hunks)
+                            /// sometimes llm will return a wrong patch which the content is not correct
+                            if (appliedPatch != null) {
+                                return appliedPatch.patchedText
+                            }
+
+                            return patch.singleHunkPatchText
+                        }
+                    }
+                }
+
+                @Throws(VcsException::class)
+                private fun loadLocalContent(): String {
+                    return ReadAction.compute<String?, VcsException?>(ThrowableComputable {
+                        val file: VirtualFile? = beforeFilePath.virtualFile
+                        if (file == null) return@ThrowableComputable null
+                        val doc = FileDocumentManager.getInstance().getDocument(file)
+                        if (doc == null) return@ThrowableComputable null
+                        doc.text
+                    })
+                }
+            }
+        }
+
+        return Change(beforeRevision, afterRevision, fileStatus)
+    }
+
+}
