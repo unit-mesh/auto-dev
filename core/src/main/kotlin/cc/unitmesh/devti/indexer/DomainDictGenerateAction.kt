93c23d925fc9077bb3959eb6ecbc5eeadf6bee32
Phodal Huang <h@phodal.com>
2025-04-09 20:39:18 +0800

    feat(core): implement domain dictionary generation feature #358
    - Add LangDictProvider interface and implementation for Java language
    - Create DomainDictGenerateAction to generate domain dictionary- Update autodev-core.xml to include new extension points and observers
    - Rename indexer template file from .devin to .vm

--- core/src/main/kotlin/cc/unitmesh/devti/indexer/DomainDictGenerateAction.kt
+++ core/src/main/kotlin/cc/unitmesh/devti/indexer/DomainDictGenerateAction.kt
@@ -1,16 +1,65 @@
 package cc.unitmesh.devti.indexer
 
+import cc.unitmesh.devti.AutoDevIcons
+import cc.unitmesh.devti.indexer.provider.LangDictProvider
+import cc.unitmesh.devti.llms.LlmFactory
+import cc.unitmesh.devti.settings.coder.coderSetting
 import cc.unitmesh.devti.settings.locale.LanguageChangedCallback.presentationText
+import cc.unitmesh.devti.statusbar.AutoDevStatus
+import cc.unitmesh.devti.statusbar.AutoDevStatusService
+import cc.unitmesh.devti.template.GENIUS_CODE
+import cc.unitmesh.devti.template.TemplateRender
+import cc.unitmesh.devti.template.context.TemplateContext
+import cc.unitmesh.devti.util.AutoDevCoroutineScope
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.guessProjectDir
+import kotlinx.coroutines.launch
 
-class DomainDictGenerateAction: AnAction() {
-    init{
+class DomainDictGenerateAction : AnAction() {
+    init {
         presentationText("indexer.generate.domain", templatePresentation)
     }
 
     override fun actionPerformed(event: AnActionEvent) {
-        /// todo: add load source content file and call llm to gerneate to csv
-        TODO("Not yet implemented")
+        val project = event.project ?: return
+        AutoDevCoroutineScope.scope(project).launch {
+            val names = LangDictProvider.all(project)
+
+            val baseDir = project.coderSetting.state.teamPromptsDir
+            val templateRender = TemplateRender(GENIUS_CODE)
+            val template = templateRender.getTemplate("indexer.vm")
+            val context = DomainDictGenerateContext(names.joinToString(", "))
+            val prompt = templateRender.renderTemplate(template, context)
+
+            try {
+                this@DomainDictGenerateAction.templatePresentation.icon = AutoDevIcons.InProgress
+                this@DomainDictGenerateAction.templatePresentation.isEnabled = false
+                val result = StringBuilder()
+                AutoDevStatusService.notifyApplication(AutoDevStatus.InProgress)
+                LlmFactory.create(project).stream(prompt, "").collect {
+                    result.append(it)
+                }
+
+                val dict = result.toString()
+                val file = project.guessProjectDir()!!.toNioPath().resolve(baseDir).resolve("domain.csv").toFile()
+                if (!file.exists()) {
+                    file.createNewFile()
+                }
+
+                AutoDevStatusService.notifyApplication(AutoDevStatus.Done)
+                file.writeText(dict)
+            } catch (e: Exception) {
+                AutoDevStatusService.notifyApplication(AutoDevStatus.Error)
+                e.printStackTrace()
+            } finally {
+                this@DomainDictGenerateAction.templatePresentation.icon = AutoDevIcons.AI_COPILOT
+                this@DomainDictGenerateAction.templatePresentation.isEnabled = true
+            }
+        }
     }
 }
+
+data class DomainDictGenerateContext(
+    val code: String
+) : TemplateContext
