4d06fe589f5d989d7538f3d084be17413d61ee9e
Phodal Huang <h@phodal.com>
2025-04-24 17:13:39 +0800

    fix(CrawlProcessorTest): update test URL to remove trailing slash for accurate parsing refactor(autodev-core): correct PostProcessor interface path for consistency

--- exts/devins-lang/src/test/kotlin/cc/unitmesh/devti/language/compiler/execute/CrawlProcessorTest.kt
+++ exts/devins-lang/src/test/kotlin/cc/unitmesh/devti/language/compiler/execute/CrawlProcessorTest.kt
@@ -5,7 +5,7 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class CrawlProcessorTest : BasePlatformTestCase() {
     fun testShouldParseLink() {
-        val urls = arrayOf("https://ide.unitmesh.cc/local-agent/")
+        val urls = arrayOf("https://ide.unitmesh.cc/local-agent")
         val results = CrawlProcessor.execute(urls)
         assertEquals(results.size, 1)
     }
