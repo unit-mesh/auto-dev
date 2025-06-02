38c0a1e6f72a220fd7f87bced201bd873a205066
Phodal Huang <h@phodal.com>
2025-06-02 10:59:27 +0800

    refactor(commands): enhance builtin command descriptions
    Expand command descriptions with detailed usage guidance, input/output specifications, and best practices. Improve developer experience by providing comprehensive context for when and how to use each command effectively.

--- exts/devins-lang/src/233/main/kotlin/cc/unitmesh/devti/language/git/GitUtil.kt
+++ exts/devins-lang/src/233/main/kotlin/cc/unitmesh/devti/language/git/GitUtil.kt
@@ -1,15 +1,18 @@
 package cc.unitmesh.devti.language.git
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.changes.LocalChangeList
 import com.intellij.vcs.commit.ChangeListCommitState
 import com.intellij.vcs.commit.LocalChangesCommitter
+import com.intellij.vcs.commit.ShowNotificationCommitResultHandler
 
 object GitUtil {
     fun doCommit(myProject: Project, list: LocalChangeList, commitMsg: String) {
         val commitState = ChangeListCommitState(list, list.changes.toList(), commitMsg)
         val committer = LocalChangesCommitter(myProject, commitState, CommitContext())
-        committer.runCommit("Commit", false)
+        committer.addResultHandler(ShowNotificationCommitResultHandler(committer))
+        committer.runCommit(VcsBundle.message("commit.changes"), true)
     }
 }
