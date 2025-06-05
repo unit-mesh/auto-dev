47c8ff6f3a9c7c2a5ed3f2f8f481c42f41915713
Phodal Huang <h@phodal.com>
2025-06-05 14:20:36 +0800

    docs(vcs): add shelving action references to CommitInsCommand #408
    Add references to IntelliJ's VCS shelving API classes that are related to the commit functionality, providing better context for future development.

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
@@ -10,11 +10,17 @@ import com.intellij.openapi.vcs.changes.LocalChangeList
 class CommitInsCommand(val myProject: Project, val commitMsg: String) : InsCommand {
     override val commandName: BuiltinCommand = BuiltinCommand.COMMIT
 
+    /**
+     * [com.intellij.openapi.vcs.changes.shelf.ShelveChangesAction] to trigger the action
+     *
+     * [com.intellij.openapi.vcs.changes.shelf.ShelveChangesCommitExecutor]
+     *
+     * [com.intellij.openapi.vcs.changes.shelf.ShelvedChangesViewManager] to manage the shelve changes
+     */
     override suspend fun execute(): String {
         val changeListManager = ChangeListManager.getInstance(myProject)
         val changeList: LocalChangeList = changeListManager.defaultChangeList
         GitUtil.doCommit(myProject, changeList, commitMsg)
-
         return "Commited for $changeList"
     }
 }
