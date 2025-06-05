e024c2b4e49fedf859d5dacd9d226130747647ea
Phodal Huang <h@phodal.com>
2025-06-05 14:31:17 +0800

    feat(commit): add support for shelving changes based on AutoSketchMode #408

--- exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
+++ exts/devins-lang/src/main/kotlin/cc/unitmesh/devti/language/compiler/exec/CommitInsCommand.kt
@@ -3,6 +3,12 @@ package cc.unitmesh.devti.language.compiler.exec
 import cc.unitmesh.devti.command.InsCommand
 import cc.unitmesh.devti.command.dataprovider.BuiltinCommand
 import cc.unitmesh.devti.language.git.GitUtil
+import cc.unitmesh.devti.sketch.AutoSketchMode
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.LocalChangeList
@@ -15,12 +21,35 @@ class CommitInsCommand(val myProject: Project, val commitMsg: String) : InsComma
      *
      * [com.intellij.openapi.vcs.changes.shelf.ShelveChangesCommitExecutor]
      *
-     * [com.intellij.openapi.vcs.changes.shelf.ShelvedChangesViewManager] to manage the shelve changes
+     * [com.intellij.openapi.vcs.changes.shelf.ShelvedChangesViewManager] to manage the shelf changes
      */
     override suspend fun execute(): String {
-        val changeListManager = ChangeListManager.getInstance(myProject)
-        val changeList: LocalChangeList = changeListManager.defaultChangeList
-        GitUtil.doCommit(myProject, changeList, commitMsg)
-        return "Commited for $changeList"
+        if (AutoSketchMode.getInstance(myProject).isEnable) {
+            invokeShelveChangesAction()
+            return "Commit by UI will depends by user selection"
+        } else {
+            val changeListManager = ChangeListManager.getInstance(myProject)
+            val changeList: LocalChangeList = changeListManager.defaultChangeList
+            GitUtil.doCommit(myProject, changeList, commitMsg)
+            return "Commited for $changeList"
+        }
+    }
+
+    private fun invokeShelveChangesAction() {
+        val actionManager = ActionManager.getInstance()
+        val shelveAction = actionManager.getAction("ChangesView.Shelve")
+
+        if (shelveAction != null) {
+            ApplicationManager.getApplication().invokeLater({
+                val dataContext = DataManager.getInstance().getDataContext()
+                val event = AnActionEvent.createFromAnAction(
+                    shelveAction,
+                    null,
+                    "",
+                    dataContext
+                )
+                shelveAction.actionPerformed(event)
+            }, ModalityState.NON_MODAL)
+        }
     }
 }
