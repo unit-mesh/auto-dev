8a1e08a3028dd0a514aa0618b66b2f96a9b6d85f
Phodal Huang <h@phodal.com>
2025-11-11 20:28:09 +0800

    feat(js): export platform API for JS interop #453
    Add JsPlatform object and jsPlatformInfo function to expose platform-specific information and utilities to JavaScript consumers.

---
+++ mpp-core/src/jsMain/kotlin/cc/unitmesh/agent/PlatformExports.kt
@@ -0,0 +1,37 @@
+package cc.unitmesh.agent
+
+import kotlin.js.JsExport
+import kotlin.js.JsName
+
+/**
+ * JS Exports for Platform API
+ * Provides access to platform-specific information and utilities
+ */
+
+@JsExport
+@JsName("JsPlatform")
+object JsPlatform {
+    val name: String get() = Platform.name
+    val isJvm: Boolean get() = Platform.isJvm
+    val isJs: Boolean get() = Platform.isJs
+    val isWasm: Boolean get() = Platform.isWasm
+    val isAndroid: Boolean get() = Platform.isAndroid
+    val isIOS: Boolean get() = Platform.isIOS
+    
+    fun getOSName(): String = Platform.getOSName()
+    fun getDefaultShell(): String = Platform.getDefaultShell()
+    fun getCurrentTimestamp(): String = Platform.getCurrentTimestamp()
+    fun getOSInfo(): String = Platform.getOSInfo()
+    fun getOSVersion(): String = Platform.getOSVersion()
+    fun getUserHomeDir(): String = Platform.getUserHomeDir()
+    fun getLogDir(): String = Platform.getLogDir()
+}
+
+/**
+ * Get platform information as a simple string
+ */
+@JsExport
+@JsName("getPlatformInfo")
+fun jsPlatformInfo(): String {
+    return getPlatformInfo()
+}
