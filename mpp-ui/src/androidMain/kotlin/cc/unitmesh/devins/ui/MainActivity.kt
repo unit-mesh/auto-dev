0e8532838a25af179fea6e2050ffe6c33d44187f
Phodal Huang <h@phodal.com>
2025-11-13 12:22:41 +0800

    feat(ui): use PlatformAutoDevApp in MainActivity
    Replace AutoDevApp with PlatformAutoDevApp to enable platform-specific UI handling in the Android entrypoint.

--- mpp-ui/src/androidMain/kotlin/cc/unitmesh/devins/ui/MainActivity.kt
+++ mpp-ui/src/androidMain/kotlin/cc/unitmesh/devins/ui/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import cc.unitmesh.devins.db.DatabaseDriverFactory
 import cc.unitmesh.devins.ui.compose.AutoDevApp
+import cc.unitmesh.devins.ui.compose.PlatformAutoDevApp
 import cc.unitmesh.devins.ui.config.ConfigManager
 import cc.unitmesh.devins.ui.platform.AndroidActivityProvider
 
 /**
  * AutoDev 移动应用 - Android 版本
- * 
+ *
  * 默认使用本地模式，支持本地和远程两种 Agent 模式
  * 用户可以在应用内通过 UI 切换模式，配置会保存到 ~/.autodev/config.yaml
  */
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             // 使用 AutoDevApp，支持本地和远程模式切换
-            AutoDevApp()
+            PlatformAutoDevApp()
         }
     }
 }
