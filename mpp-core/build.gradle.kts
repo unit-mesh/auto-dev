4f48614e6fb8c8046623e9211ffd3791c5f366f5
Phodal Huang <h@phodal.com>
2025-11-28 14:06:02 +0800

    feat(core): replace linter with codegraph dependency
    Switch dependency from mpp-linter to mpp-codegraph in commonMain.

--- mpp-core/build.gradle.kts
+++ mpp-core/build.gradle.kts
@@ -118,7 +118,8 @@ kotlin {
         commonMain {
             dependencies {
                 // MPP Linter dependency
-//                implementation(project(":mpp-linter"))
+                implementation(project(":mpp-codegraph"))
+
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
                 implementation("com.charleskorn.kaml:kaml:0.61.0")
